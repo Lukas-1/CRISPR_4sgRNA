4e7877067f0c8d5948cdf5f1843e893307128761
Lukas-1 <lukas.frick@gmail.com>
2019-12-31 00:26:27 +0100

    Some small adjusments

--- 1) R scripts/1) R functions/09) Constants and settings.R
+++ 1) R scripts/1) R functions/09) Constants and settings.R
@@ -10,3 +10,31 @@ preferred_AF_max_column <- "all22_SNP_AF_max_Kaviar"
 SNP_frequency_cutoff    <- 0.001 # 0.1%
 
 
+SNP_column_names <- c("sgRNA_SNP_IDs_vcf",     "sgRNA_SNP_AFs_1kGenomes", "sgRNA_SNP_AF_max_1kGenomes", "sgRNA_SNP_AF_sum_1kGenomes",
+                      "sgRNA_SNP_AFs_TOPMED",  "sgRNA_SNP_AF_max_TOPMED", "sgRNA_SNP_AF_sum_TOPMED",
+                      "sgRNA_SNP_AFs_Kaviar",  "sgRNA_SNP_AF_max_Kaviar", "sgRNA_SNP_AF_sum_Kaviar",
+                      "sgRNA_SNP_IDs_1kG_ph1", "sgRNA_SNP_AFs_1kG_ph1",   "sgRNA_SNP_AF_max_1kG_ph1", "sgRNA_SNP_AF_sum_1kG_ph1",
+                      "sgRNA_SNP_IDs_1kG_ph3", "sgRNA_SNP_AFs_1kG_ph3",   "sgRNA_SNP_AF_max_1kG_ph3", "sgRNA_SNP_AF_sum_1kG_ph3",
+                      "sgRNA_SNP_IDs_gnomAD",  "sgRNA_SNP_AFs_gnomAD",    "sgRNA_SNP_AF_max_gnomAD",  "sgRNA_SNP_AF_sum_gnomAD",
+
+                      "PAM_SNP_IDs_vcf",     "PAM_SNP_AFs_1kGenomes", "PAM_SNP_AF_max_1kGenomes", "PAM_SNP_AF_sum_1kGenomes",
+                      "PAM_SNP_AFs_TOPMED",  "PAM_SNP_AF_max_TOPMED", "PAM_SNP_AF_sum_TOPMED",
+                      "PAM_SNP_AFs_Kaviar",  "PAM_SNP_AF_max_Kaviar", "PAM_SNP_AF_sum_Kaviar",
+                      "PAM_SNP_IDs_1kG_ph1", "PAM_SNP_AFs_1kG_ph1",   "PAM_SNP_AF_max_1kG_ph1", "PAM_SNP_AF_sum_1kG_ph1",
+                      "PAM_SNP_IDs_1kG_ph3", "PAM_SNP_AFs_1kG_ph3",   "PAM_SNP_AF_max_1kG_ph3", "PAM_SNP_AF_sum_1kG_ph3",
+                      "PAM_SNP_IDs_gnomAD",  "PAM_SNP_AFs_gnomAD",    "PAM_SNP_AF_max_gnomAD",  "PAM_SNP_AF_sum_gnomAD",
+
+                      "all23_SNP_IDs_vcf",     "all23_SNP_AFs_1kGenomes", "all23_SNP_AF_max_1kGenomes", "all23_SNP_AF_sum_1kGenomes",
+                      "all23_SNP_AFs_TOPMED",  "all23_SNP_AF_max_TOPMED", "all23_SNP_AF_sum_TOPMED",
+                      "all23_SNP_AFs_Kaviar",  "all23_SNP_AF_max_Kaviar", "all23_SNP_AF_sum_Kaviar",
+                      "all23_SNP_IDs_1kG_ph1", "all23_SNP_AFs_1kG_ph1",   "all23_SNP_AF_max_1kG_ph1", "all23_SNP_AF_sum_1kG_ph1",
+                      "all23_SNP_IDs_1kG_ph3", "all23_SNP_AFs_1kG_ph3",   "all23_SNP_AF_max_1kG_ph3", "all23_SNP_AF_sum_1kG_ph3",
+                      "all23_SNP_IDs_gnomAD",  "all23_SNP_AFs_gnomAD",    "all23_SNP_AF_max_gnomAD",  "all23_SNP_AF_sum_gnomAD",
+
+                      "all22_SNP_IDs_vcf",     "all22_SNP_AF_max_1kGenomes", "all22_SNP_AF_max_TOPMED", "all22_SNP_AF_max_Kaviar",
+                      "all22_SNP_IDs_1kG_ph1", "all22_SNP_AF_max_1kG_ph1",
+                      "all22_SNP_IDs_1kG_ph3", "all22_SNP_AF_max_1kG_ph3",
+                      "all22_SNP_IDs_gnomAD",  "all22_SNP_AF_max_gnomAD"
+                      )
+
+
--- 1) R scripts/4) Build CRISPRko library/14) Export libraries and produce plots.R
+++ 1) R scripts/4) Build CRISPRko library/14) Export libraries and produce plots.R
@@ -46,32 +46,6 @@ source_abbreviations_vec <- c(
 
 # Re-arrange the columns --------------------------------------------------
 
-SNP_column_names <- c("sgRNA_SNP_IDs_vcf",     "sgRNA_SNP_AFs_1kGenomes", "sgRNA_SNP_AF_max_1kGenomes", "sgRNA_SNP_AF_sum_1kGenomes",
-                      "sgRNA_SNP_AFs_TOPMED",  "sgRNA_SNP_AF_max_TOPMED", "sgRNA_SNP_AF_sum_TOPMED",
-                      "sgRNA_SNP_AFs_Kaviar",  "sgRNA_SNP_AF_max_Kaviar", "sgRNA_SNP_AF_sum_Kaviar",
-                      "sgRNA_SNP_IDs_1kG_ph1", "sgRNA_SNP_AFs_1kG_ph1",   "sgRNA_SNP_AF_max_1kG_ph1", "sgRNA_SNP_AF_sum_1kG_ph1",
-                      "sgRNA_SNP_IDs_1kG_ph3", "sgRNA_SNP_AFs_1kG_ph3",   "sgRNA_SNP_AF_max_1kG_ph3", "sgRNA_SNP_AF_sum_1kG_ph3",
-                      "sgRNA_SNP_IDs_gnomAD",  "sgRNA_SNP_AFs_gnomAD",    "sgRNA_SNP_AF_max_gnomAD",  "sgRNA_SNP_AF_sum_gnomAD",
-
-                      "PAM_SNP_IDs_vcf",     "PAM_SNP_AFs_1kGenomes", "PAM_SNP_AF_max_1kGenomes", "PAM_SNP_AF_sum_1kGenomes",
-                      "PAM_SNP_AFs_TOPMED",  "PAM_SNP_AF_sum_TOPMED", "PAM_SNP_AF_max_TOPMED",
-                      "PAM_SNP_AFs_Kaviar",  "PAM_SNP_AF_sum_Kaviar", "PAM_SNP_AF_max_Kaviar",
-                      "PAM_SNP_IDs_1kG_ph1", "PAM_SNP_AFs_1kG_ph1",   "PAM_SNP_AF_max_1kG_ph1", "PAM_SNP_AF_sum_1kG_ph1",
-                      "PAM_SNP_IDs_1kG_ph3", "PAM_SNP_AFs_1kG_ph3",   "PAM_SNP_AF_max_1kG_ph3", "PAM_SNP_AF_sum_1kG_ph3",
-                      "PAM_SNP_IDs_gnomAD",  "PAM_SNP_AFs_gnomAD",    "PAM_SNP_AF_max_gnomAD",  "PAM_SNP_AF_sum_gnomAD",
-
-                      "all23_SNP_IDs_vcf",     "all23_SNP_AFs_1kGenomes", "all23_SNP_AF_max_1kGenomes", "all23_SNP_AF_sum_1kGenomes",
-                      "all23_SNP_AFs_TOPMED",  "all23_SNP_AF_sum_TOPMED", "all23_SNP_AF_max_TOPMED",
-                      "all23_SNP_AFs_Kaviar",  "all23_SNP_AF_sum_Kaviar", "all23_SNP_AF_max_Kaviar",
-                      "all23_SNP_IDs_1kG_ph1", "all23_SNP_AFs_1kG_ph1",   "all23_SNP_AF_max_1kG_ph1", "all23_SNP_AF_sum_1kG_ph1",
-                      "all23_SNP_IDs_1kG_ph3", "all23_SNP_AFs_1kG_ph3",   "all23_SNP_AF_max_1kG_ph3", "all23_SNP_AF_sum_1kG_ph3",
-                      "all23_SNP_IDs_gnomAD",  "all23_SNP_AFs_gnomAD",    "all23_SNP_AF_max_gnomAD",  "all23_SNP_AF_sum_gnomAD",
-
-                      "all22_SNP_AF_max_1kGenomes", "all22_SNP_AF_max_TOPMED",  "all22_SNP_AF_max_Kaviar",
-                      "all22_SNP_AF_max_1kG_ph1",   "all22_SNP_AF_max_1kG_ph3", "all22_SNP_AF_max_gnomAD"
-                      )
-
-
 rearranged_column_names <- c(
   "Combined_ID", "Entrez_ID", "Gene_symbol", "Original_entrez", "Original_symbol",
 
@@ -139,7 +113,7 @@ omit_columns <- c("Combined_ID",
                   )
 
 omit_SNP_columns <- grep("SNP", colnames(merged_CRISPRko_df), value = TRUE)
-omit_SNP_columns <- omit_SNP_columns[!(omit_SNP_columns %in% c(preferred_rsID_column, preferred_AF_max_column))]
+omit_SNP_columns <- setdiff(omit_SNP_columns, c(preferred_rsID_column, preferred_AF_max_column))
 full_omit_columns <- c(omit_columns, omit_SNP_columns)
 
 
