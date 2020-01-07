2ba28f6ea75f39ca0a44e442c60e1832e609404b
Lukas-1 <lukas.frick@gmail.com>
2020-01-07 23:05:03 +0100

    Avoid some code duplication

--- 1) R scripts/2) Prepare for compiling CRISPR libraries/01) Extract gene annotation data from the org.Hs.eg.db Bioconductor database.R
+++ 1) R scripts/2) Prepare for compiling CRISPR libraries/01) Extract gene annotation data from the org.Hs.eg.db Bioconductor database.R
@@ -16,32 +16,26 @@ general_RData_directory <- file.path(CRISPR_root_directory, "3) RData files", "1
 
 
 
+# Define functions --------------------------------------------------------
+
+BimapToList <- function(Bimap_object) {
+  as.list(Bimap_object[mappedkeys(Bimap_object)])
+}
+
+
+
 
 # Define gene annotation lookup lists -------------------------------------
 
-my_x <- org.Hs.egSYMBOL
-entrez_to_symbol_list <- as.list(my_x[mappedkeys(my_x)])
+entrez_to_symbol_list     <- BimapToList(org.Hs.egSYMBOL)
+entrez_to_genename_list   <- BimapToList(org.Hs.egGENENAME)
+entrez_to_chromosome_list <- BimapToList(org.Hs.egCHR)
 
-my_x <- org.Hs.egSYMBOL2EG
-symbol_to_entrez_list <- as.list(my_x[mappedkeys(my_x)])
+symbol_to_entrez_list     <- BimapToList(org.Hs.egSYMBOL2EG)
+alias_to_entrez_list      <- BimapToList(org.Hs.egALIAS2EG)
 
-my_x <- org.Hs.egGENENAME
-entrez_to_genename_list <- as.list(my_x[mappedkeys(my_x)])
-
-my_x <- org.Hs.egCHR
-entrez_to_chromosome_list <- as.list(my_x[mappedkeys(my_x)])
-
-my_x <- org.Hs.egALIAS2EG
-alias_to_entrez_list <- as.list(my_x[mappedkeys(my_x)])
-
-my_x <- org.Hs.egENSEMBL2EG
-ensembl_to_entrez_list <- as.list(my_x[mappedkeys(my_x)])
-
-my_x <- org.Hs.egENSEMBL
-entrez_to_ensembl_list <- as.list(my_x[mappedkeys(my_x)])
-
-rm(my_x)
-
+ensembl_to_entrez_list    <- BimapToList(org.Hs.egENSEMBL2EG)
+entrez_to_ensembl_list    <- BimapToList(org.Hs.egENSEMBL)
 
 
 
@@ -51,5 +45,8 @@ rm(my_x)
 save(list = grep("^(entrez_to_.+_list|.+_to_entrez_list)$", ls(), value = TRUE),
      file = file.path(general_RData_directory, "01) Extract gene annotation data from the org.Hs.eg.db Bioconductor database.RData")
      )
+
+
+
 
 
