af14c77fa6f5d0f7e00469cf6cb32dc7cc1e9c12
Lukas-1 <lukas.frick@gmail.com>
2020-01-18 10:36:36 +0100

    Implemented tied ranks for sgRNAs, using 'data.table::frank'!

--- 1) R scripts/1) R functions/10) Ranking sgRNAs.R
+++ 1) R scripts/1) R functions/10) Ranking sgRNAs.R
@@ -3,6 +3,8 @@
 
 # Import packages and source code -----------------------------------------
 
+library("data.table")
+
 general_functions_directory <- "~/CRISPR/1) R scripts/1) R functions"
 source(file.path(general_functions_directory, "02) Translating between Entrez IDs and gene symbols.R")) # For GetMinEntrez
 source(file.path(general_functions_directory, "09) Constants and settings.R")) # For GetMinEntrez
@@ -80,32 +82,32 @@ RankDf <- function(CRISPR_sub_df, allow_5pG_MM = FALSE) {
 
   is_CRISPRko <- "Exon_number_GPP" %in% names(CRISPR_sub_df)
 
-  my_order <- order(CRISPR_sub_df[, "Is_control"] == "No",
-                    !(grepl("TTTT", CRISPR_sub_df[, "sgRNA_sequence"], ignore.case = TRUE)),
-                    !(is.na(CRISPR_sub_df[, "Start"])),
-                    substr(CRISPR_sub_df[, "PAM"], 2, 3) == "GG",
-                    if (SNP_data_present) !((CRISPR_sub_df[, preferred_AF_max_column] > SNP_frequency_cutoff) %in% TRUE)                   else NA_vec,
-                    !(is.na(CRISPR_sub_df[, "GuideScan_specificity"])),
-                    if ("CRISPOR_4MM_specificity" %in% names(CRISPR_sub_df)) !(is.na(CRISPR_sub_df[, "CRISPOR_4MM_specificity"]))          else NA_vec,
+  list_for_ranking <- list(CRISPR_sub_df[, "Is_control"] == "No",
+                           !(grepl("TTTT", CRISPR_sub_df[, "sgRNA_sequence"], ignore.case = TRUE)),
+                           !(is.na(CRISPR_sub_df[, "Start"])),
+                           substr(CRISPR_sub_df[, "PAM"], 2, 3) == "GG",
+                           if (SNP_data_present) !((CRISPR_sub_df[, preferred_AF_max_column] > SNP_frequency_cutoff) %in% TRUE)                   else NA_vec,
+                           !(is.na(CRISPR_sub_df[, "GuideScan_specificity"])),
+                           if ("CRISPOR_4MM_specificity" %in% names(CRISPR_sub_df)) !(is.na(CRISPR_sub_df[, "CRISPOR_4MM_specificity"]))          else NA_vec,
 
-                    exactly_one_vec, # prefers exactly one match
-                    ifelse(exactly_one_vec, TRUE, CRISPR_sub_df[, "Num_0MM"] != 0), # ensures that Num_0MM >= 2 is preferred over Num_0MM == 0
-                    ifelse(CRISPR_sub_df[, "Num_0MM"] >= 2, -(CRISPR_sub_df[, "Num_0MM"]), NA_integer_),
-                    if (allow_5pG_MM) ifelse(exactly_one_vec, 1, -(CRISPR_sub_df[, "Num_5G_MM"]))                                          else NA_vec, # penalize multiple 5' G-mismatched locations
-                    if (allow_5pG_MM || !("Num_5G_MM" %in% names(CRISPR_sub_df))) -(CRISPR_sub_df[, "Num_1MM"]) else -(rowSums(CRISPR_sub_df[, c("Num_5G_MM", "Num_1MM")])),
+                           exactly_one_vec, # prefers exactly one match
+                           ifelse(exactly_one_vec, TRUE, CRISPR_sub_df[, "Num_0MM"] != 0), # ensures that Num_0MM >= 2 is preferred over Num_0MM == 0
+                           ifelse(CRISPR_sub_df[, "Num_0MM"] >= 2, -(CRISPR_sub_df[, "Num_0MM"]), NA_integer_),
+                           if (allow_5pG_MM) ifelse(exactly_one_vec, 1, -(CRISPR_sub_df[, "Num_5G_MM"]))                                          else NA_vec, # penalize multiple 5' G-mismatched locations
+                           if (allow_5pG_MM || !("Num_5G_MM" %in% names(CRISPR_sub_df))) -(CRISPR_sub_df[, "Num_1MM"]) else -(rowSums(CRISPR_sub_df[, c("Num_5G_MM", "Num_1MM")])),
 
-                    if (is_CRISPRko && ("CRISPOR_Graf_status" %in% names(CRISPR_sub_df))) (CRISPR_sub_df[, "CRISPOR_Graf_status"] %in% "GrafOK") else NA_vec,
+                           if (is_CRISPRko && ("CRISPOR_Graf_status" %in% names(CRISPR_sub_df))) (CRISPR_sub_df[, "CRISPOR_Graf_status"] %in% "GrafOK") else NA_vec,
 
-                    CRISPR_sub_df[, "GuideScan_specificity"],
-                    if ("CRISPOR_4MM_specificity" %in% names(CRISPR_sub_df)) CRISPR_sub_df[, "CRISPOR_4MM_specificity"]                    else NA_vec,
-                    CRISPR_sub_df[, "GuideScan_efficiency"],
-                    if ("CRISPOR_Doench_efficacy" %in% names(CRISPR_sub_df)) CRISPR_sub_df[, "CRISPOR_Doench_efficacy"]                    else NA_vec,
-                    if ("hCRISPRa_v2_rank" %in% names(CRISPR_sub_df)) -(suppressWarnings(as.integer(CRISPR_sub_df[, "hCRISPRa_v2_rank"]))) else NA_vec,
-                    if ("Calabrese_rank" %in% names(CRISPR_sub_df)) -(match(CRISPR_sub_df[, "Calabrese_rank"], c("1/2/3", "4/5/6")))       else NA_vec,
-                    -(CRISPR_sub_df[, "GPP_rank"]),
-                    decreasing = TRUE
-                    )
-  my_rank <- match(seq_along(my_order), my_order)
+                           CRISPR_sub_df[, "GuideScan_specificity"],
+                           if ("CRISPOR_4MM_specificity" %in% names(CRISPR_sub_df)) CRISPR_sub_df[, "CRISPOR_4MM_specificity"]                    else NA_vec,
+                           CRISPR_sub_df[, "GuideScan_efficiency"],
+                           if ("CRISPOR_Doench_efficacy" %in% names(CRISPR_sub_df)) CRISPR_sub_df[, "CRISPOR_Doench_efficacy"]                    else NA_vec,
+                           if ("hCRISPRa_v2_rank" %in% names(CRISPR_sub_df)) -(suppressWarnings(as.integer(CRISPR_sub_df[, "hCRISPRa_v2_rank"]))) else NA_vec,
+                           if ("Calabrese_rank" %in% names(CRISPR_sub_df)) -(match(CRISPR_sub_df[, "Calabrese_rank"], c("1/2/3", "4/5/6")))       else NA_vec,
+                           -(CRISPR_sub_df[, "GPP_rank"])
+                           )
+  assign("delete_list_for_ranking", list_for_ranking, envir = globalenv())
+  my_rank <- data.table::frank(x = lapply(list_for_ranking, "*", -1L), ties.method = "average")
   return(my_rank)
 }
 
