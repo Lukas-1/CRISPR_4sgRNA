24f78802064281504646fe7cbf6b8d6e0a72b16f
Lukas-1 <lukas.frick@gmail.com>
2020-01-18 20:36:06 +0100

    Moved some code

--- 1) R scripts/1) R functions/19) Using CRISPOR.R
+++ 1) R scripts/1) R functions/19) Using CRISPOR.R
@@ -100,27 +100,6 @@ MakeFASTADf <- function(CRISPR_df, combined_IDs) {
 
 
 
-CombineDfChunks <- function(df_list, max_num_per_chunk = 12000L) {
-  current_sum <- nrow(df_list[[1]])
-  current_chunk <- 1L
-  chunk_vec <- c(1L, rep(NA_integer_, length(df_list) - 1))
-  for (i in seq(from = 2, to = length(df_list), by = 1)) {
-    current_nrow <- nrow(df_list[[i]])
-    if ((current_sum + current_nrow) > max_num_per_chunk) {
-      current_chunk <- current_chunk + 1L
-      current_sum <- current_nrow
-    } else {
-      current_sum <- current_sum + current_nrow
-    }
-    chunk_vec[[i]] <- current_chunk
-  }
-  result_df_list <- tapply(df_list, chunk_vec, function(x) do.call(rbind.data.frame, c(x, list(stringsAsFactors = FALSE, make.row.names = FALSE))))
-  names(result_df_list) <- tapply(sub("_TF", "", names(df_list)), chunk_vec, paste0, collapse = "")
-  return(result_df_list)
-}
-
-
-
 MakeFASTAvec <- function(use_FASTA_df) {
   full_sequences_vec <- unique(paste0(use_FASTA_df[, "sgRNA_sequence"], use_FASTA_df[, "PAM"]))
   FASTA_list <- lapply(seq_along(full_sequences_vec), function(x) c(paste0(">seq", x), full_sequences_vec[[x]], ""))
--- 1) R scripts/1) R functions/21) Splitting sgRNAs into chunks for parallel analysis.R
+++ 1) R scripts/1) R functions/21) Splitting sgRNAs into chunks for parallel analysis.R
@@ -13,3 +13,23 @@ AppendIDsWithoutEntrezs <- function(entrez_IDs_list, CRISPR_df) {
                                     )
   return(IDs_list)
 }
+
+
+CombineDfChunks <- function(df_list, max_num_per_chunk = 12000L) {
+  current_sum <- nrow(df_list[[1]])
+  current_chunk <- 1L
+  chunk_vec <- c(1L, rep(NA_integer_, length(df_list) - 1))
+  for (i in seq(from = 2, to = length(df_list), by = 1)) {
+    current_nrow <- nrow(df_list[[i]])
+    if ((current_sum + current_nrow) > max_num_per_chunk) {
+      current_chunk <- current_chunk + 1L
+      current_sum <- current_nrow
+    } else {
+      current_sum <- current_sum + current_nrow
+    }
+    chunk_vec[[i]] <- current_chunk
+  }
+  result_df_list <- tapply(df_list, chunk_vec, function(x) do.call(rbind.data.frame, c(x, list(stringsAsFactors = FALSE, make.row.names = FALSE))))
+  names(result_df_list) <- tapply(sub("_TF", "", names(df_list)), chunk_vec, paste0, collapse = "")
+  return(result_df_list)
+}
