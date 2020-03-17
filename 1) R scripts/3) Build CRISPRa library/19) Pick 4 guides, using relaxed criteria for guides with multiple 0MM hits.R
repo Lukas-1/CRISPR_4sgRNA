95bca5a87b7ee112086e96f88b7249c5a5a68a74
Lukas-1 <lukas.frick@gmail.com>
2020-03-17 11:10:11 +0100

    Incorporate SNPs for guides with multiple 0MM locations, and re-pick the top 4 guides only for genes that are affected by relaxed locations

--- 1) R scripts/3) Build CRISPRa library/19) Pick 4 guides, using relaxed criteria for guides with multiple 0MM hits.R
+++ 1) R scripts/3) Build CRISPRa library/19) Pick 4 guides, using relaxed criteria for guides with multiple 0MM hits.R
@@ -5,6 +5,7 @@
 # Import packages and source code -----------------------------------------
 
 general_functions_directory <- "~/CRISPR/1) R scripts/1) R functions"
+source(file.path(general_functions_directory, "08) Checking for overlaps with genetic polymorphisms.R"))
 source(file.path(general_functions_directory, "10) Ranking sgRNAs.R"))
 source(file.path(general_functions_directory, "11) Merging data from multiple sources to annotate CRISPR libraries.R"))
 source(file.path(general_functions_directory, "13) Separating sgRNAs that target distinct TSSs.R"))
@@ -18,6 +19,7 @@ source(file.path(general_functions_directory, "15) Finding non-overlapping sgRNA
 
 CRISPR_root_directory   <- "~/CRISPR"
 RData_directory         <- file.path(CRISPR_root_directory, "3) RData files")
+general_RData_directory <- file.path(RData_directory, "1) General")
 CRISPRa_RData_directory <- file.path(RData_directory, "2) CRISPRa")
 
 
@@ -26,6 +28,9 @@ CRISPRa_RData_directory <- file.path(RData_directory, "2) CRISPRa")
 
 # Load data ---------------------------------------------------------------
 
+load(file.path(general_RData_directory, "05) Compile data on genetic polymorphisms.RData"))
+load(file.path(CRISPRa_RData_directory, "18) Pick 4 guides per TSS.RData"))
+strict_CRISPRa_df <- merged_replaced_CRISPRa_df
 load(file.path(CRISPRa_RData_directory, "17) Integrate the output from CRISPOR.RData"))
 
 
@@ -40,31 +45,38 @@ lax_CRISPRa_df <- FindBest0MMLocations(merged_replaced_CRISPRa_df)
 
 
 
+# Identify genes affected by relaxed locations ----------------------------
+
+are_identical <- mapply(identical, lax_CRISPRa_df[["Start"]], lax_CRISPRa_df[["Start_lax"]])
+changed_IDs <- lax_CRISPRa_df[["Combined_ID"]][!(are_identical)]
+
+
+
+
 
 # Re-order and rename columns ---------------------------------------------
 
-location_columns <- c("Chromosome", "Strand", "Start", "End")
-columns_vec <- colnames(lax_CRISPRa_df)
-
-are_strict_columns <- columns_vec %in% location_columns
-are_relaxed_columns <- columns_vec %in% paste0(location_columns, "_lax")
-
-columns_vec[are_strict_columns] <- paste0(location_columns, "_lax")
-columns_vec[are_relaxed_columns] <- location_columns
-
-lax_CRISPRa_df <- lax_CRISPRa_df[, columns_vec]
-
-are_strict_columns <- columns_vec %in% location_columns
-are_relaxed_columns <- columns_vec %in% paste0(location_columns, "_lax")
-
-colnames(lax_CRISPRa_df)[are_strict_columns] <- paste0(location_columns, "_strict")
-colnames(lax_CRISPRa_df)[are_relaxed_columns] <- location_columns
+lax_CRISPRa_df <- ReorderLaxColumns(lax_CRISPRa_df)
 
 drop_columns <- c("TSS_number", "Allocated_TSS", "Num_TSSs", "TSS_ID", "AltTSS_ID")
-
 lax_CRISPRa_df <- lax_CRISPRa_df[, !(colnames(lax_CRISPRa_df) %in% drop_columns)]
-colnames(lax_CRISPRa_df) <- sub("_lax$", "", colnames(lax_CRISPRa_df))
 
+
+
+
+
+# Re-assign cut locations -------------------------------------------------
+
+lax_CRISPRa_df[["Cut_location"]] <- GetCutLocations(lax_CRISPRa_df)
+
+
+
+
+
+# Find SNPs that overlap with sgRNAs, based on relaxed locations ----------
+
+all_columns <- colnames(lax_CRISPRa_df)
+lax_CRISPRa_df <- SNPDataForCRISPRdf(lax_CRISPRa_df[, grep("SNP", colnames(lax_CRISPRa_df), fixed = TRUE, invert = TRUE)])[, all_columns]
 
 
 
@@ -72,10 +84,7 @@ colnames(lax_CRISPRa_df) <- sub("_lax$", "", colnames(lax_CRISPRa_df))
 
 # Re-assign TSS based on locations assigned using relaxed criteria --------
 
-lax_CRISPRa_df[["Cut_location"]] <- GetCutLocations(lax_CRISPRa_df)
 lax_CRISPRa_df[["Distance_from_TSS"]] <- GetDistanceFromTSS(lax_CRISPRa_df)
-
-
 lax_CRISPRa_df <- AllocateTSSforAllGenes(lax_CRISPRa_df, parallel_mode = TRUE, tolerate_divergent_chromosomes = TRUE)
 
 
@@ -94,11 +103,31 @@ lax_CRISPRa_df <- RankCRISPRDf(lax_CRISPRa_df, ID_column = "AltTSS_ID")
 
 # Find non-overlapping guides, based on relaxed location criteria ---------
 
-lax_CRISPRa_df <- PrioritizeNonOverlapping(lax_CRISPRa_df, ID_column = "AltTSS_ID", parallel_mode = TRUE,
-                                           tolerate_divergent_chromosomes = TRUE
-                                           )
+lax_CRISPRa_changed_df <- PrioritizeNonOverlapping(lax_CRISPRa_df[lax_CRISPRa_df[["Combined_ID"]] %in% changed_IDs, ],
+                                                   ID_column = "AltTSS_ID", parallel_mode = TRUE,
+                                                   tolerate_divergent_chromosomes = TRUE
+                                                   )
 
-# delete_CRISPR_sub_df[, c("Combined_ID", "Source", "Num_0MM", "Locations_0MM", "Entrez_chromosome", "Chromosome", "Strand", "Start", "End")]
+
+
+
+
+# Merge the changed and unchanged sgRNAs ----------------------------------
+
+position_columns <- c("Chromosome", "Strand", "Start", "End")
+for (column_name in position_columns) {
+  strict_CRISPRa_df[[paste0(column_name, "_strict")]] <- strict_CRISPRa_df[[column_name]]
+}
+
+lax_CRISPRa_df <- rbind.data.frame(
+  lax_CRISPRa_changed_df,
+  strict_CRISPRa_df[!(strict_CRISPRa_df[["Combined_ID"]] %in% changed_IDs), colnames(lax_CRISPRa_changed_df)],
+  stringsAsFactors = FALSE,
+  make.row.names = FALSE
+)
+
+lax_CRISPRa_df <- lax_CRISPRa_df[order(match(lax_CRISPRa_df[["Combined_ID"]], strict_CRISPRa_df[["Combined_ID"]])), ]
+rownames(lax_CRISPRa_df) <- NULL
 
 
 
--- 1) R scripts/4) Build CRISPRko library/12) Pick 4 guides, using relaxed criteria for guides with multiple 0MM hits.R
+++ 1) R scripts/4) Build CRISPRko library/12) Pick 4 guides, using relaxed criteria for guides with multiple 0MM hits.R
@@ -6,6 +6,7 @@
 
 general_functions_directory <- "~/CRISPR/1) R scripts/1) R functions"
 source(file.path(general_functions_directory, "07) Annotating mapped sequences with additional information.R")) # For the 'human_genes_GRanges' object
+source(file.path(general_functions_directory, "08) Checking for overlaps with genetic polymorphisms.R"))
 source(file.path(general_functions_directory, "10) Ranking sgRNAs.R"))
 source(file.path(general_functions_directory, "11) Merging data from multiple sources to annotate CRISPR libraries.R"))
 source(file.path(general_functions_directory, "15) Finding non-overlapping sgRNAs.R"))
@@ -18,6 +19,7 @@ source(file.path(general_functions_directory, "15) Finding non-overlapping sgRNA
 
 CRISPR_root_directory    <- "~/CRISPR"
 RData_directory          <- file.path(CRISPR_root_directory, "3) RData files")
+general_RData_directory  <- file.path(RData_directory, "1) General")
 CRISPRko_RData_directory <- file.path(RData_directory, "3) CRISPRko")
 
 
@@ -25,6 +27,9 @@ CRISPRko_RData_directory <- file.path(RData_directory, "3) CRISPRko")
 
 # Load data ---------------------------------------------------------------
 
+load(file.path(general_RData_directory, "05) Compile data on genetic polymorphisms.RData"))
+load(file.path(CRISPRko_RData_directory, "11) Pick 4 guides per gene.RData"))
+strict_CRISPRko_df <- merged_CRISPRko_df
 load(file.path(CRISPRko_RData_directory, "10) Find overlaps between CRISPRko sgRNA sequences and genetic polymorphisms.RData"))
 
 
@@ -39,27 +44,18 @@ lax_CRISPRko_df <- FindBest0MMLocations(merged_CRISPRko_df)
 
 
 
+# Identify genes affected by relaxed locations ----------------------------
+
+are_identical <- mapply(identical, lax_CRISPRko_df[["Start"]], lax_CRISPRko_df[["Start_lax"]])
+changed_IDs <- lax_CRISPRko_df[["Combined_ID"]][!(are_identical)]
+
+
+
+
+
 # Re-order and rename columns ---------------------------------------------
 
-location_columns <- c("Chromosome", "Strand", "Start", "End")
-columns_vec <- colnames(lax_CRISPRko_df)
-
-are_strict_columns <- columns_vec %in% location_columns
-are_relaxed_columns <- columns_vec %in% paste0(location_columns, "_lax")
-
-columns_vec[are_strict_columns] <- paste0(location_columns, "_lax")
-columns_vec[are_relaxed_columns] <- location_columns
-
-lax_CRISPRko_df <- lax_CRISPRko_df[, columns_vec]
-
-are_strict_columns <- columns_vec %in% location_columns
-are_relaxed_columns <- columns_vec %in% paste0(location_columns, "_lax")
-
-colnames(lax_CRISPRko_df)[are_strict_columns] <- paste0(location_columns, "_strict")
-colnames(lax_CRISPRko_df)[are_relaxed_columns] <- location_columns
-
-colnames(lax_CRISPRko_df) <- sub("_lax$", "", colnames(lax_CRISPRko_df))
-
+lax_CRISPRko_df <- ReorderLaxColumns(lax_CRISPRko_df)
 
 
 
@@ -69,6 +65,14 @@ colnames(lax_CRISPRko_df) <- sub("_lax$", "", colnames(lax_CRISPRko_df))
 
 lax_CRISPRko_df[["Cut_location"]] <- GetCutLocations(lax_CRISPRko_df)
 
+
+
+
+
+# Find SNPs that overlap with sgRNAs, based on relaxed locations ----------
+
+all_columns <- colnames(lax_CRISPRko_df)
+lax_CRISPRko_df <- SNPDataForCRISPRdf(lax_CRISPRko_df[, grep("SNP", colnames(lax_CRISPRko_df), fixed = TRUE, invert = TRUE)])[, all_columns]
 
 
 
@@ -85,11 +89,33 @@ lax_CRISPRko_df <- RankCRISPRDf(lax_CRISPRko_df, ID_column = "Combined_ID")
 
 # Find non-overlapping guides, based on relaxed location criteria ---------
 
-lax_CRISPRko_df <- PrioritizeNonOverlapping(lax_CRISPRko_df,
-                                            ID_column = "Combined_ID",
-                                            parallel_mode = TRUE,
-                                            tolerate_divergent_chromosomes = TRUE
-                                            )
+lax_CRISPRko_changed_df <- PrioritizeNonOverlapping(lax_CRISPRko_df[lax_CRISPRko_df[["Combined_ID"]] %in% changed_IDs, ],
+                                                    ID_column = "Combined_ID",
+                                                    parallel_mode = TRUE,
+                                                    tolerate_divergent_chromosomes = TRUE
+                                                    )
+
+
+
+
+
+# Merge the changed and unchanged sgRNAs ----------------------------------
+
+position_columns <- c("Chromosome", "Strand", "Start", "End")
+for (column_name in position_columns) {
+  strict_CRISPRko_df[[paste0(column_name, "_strict")]] <- strict_CRISPRko_df[[column_name]]
+}
+
+lax_CRISPRko_df <- rbind.data.frame(
+  lax_CRISPRko_changed_df,
+  strict_CRISPRko_df[!(strict_CRISPRko_df[["Combined_ID"]] %in% changed_IDs), colnames(lax_CRISPRko_df)],
+  stringsAsFactors = FALSE,
+  make.row.names = FALSE
+)
+
+lax_CRISPRko_df <- lax_CRISPRko_df[order(match(lax_CRISPRko_df[["Combined_ID"]], strict_CRISPRko_df[["Combined_ID"]])), ]
+rownames(lax_CRISPRko_df) <- NULL
+
 
 
 
