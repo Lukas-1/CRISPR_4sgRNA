e7915bca1a363bda99b4c8176c7de7b02a07daa9
Lukas_1 <lukas.frick@gmail.com>
2020-03-29 19:34:01 +0200

    Add a section of code for testing (that is commented out)

--- 1) R scripts/4) Build CRISPRko library/11) Pick 4 guides per gene.R
+++ 1) R scripts/4) Build CRISPRko library/11) Pick 4 guides per gene.R
@@ -24,7 +24,6 @@ CRISPRko_RData_directory <- file.path(RData_directory, "3) CRISPRko")
 
 # Load data ---------------------------------------------------------------
 
-load(file.path(general_RData_directory, "05) Compile data on genetic polymorphisms.RData"))
 load(file.path(CRISPRko_RData_directory, "10) Find overlaps between CRISPRko sgRNA sequences and genetic polymorphisms.RData"))
 
 
@@ -57,6 +56,68 @@ save(list = "merged_CRISPRko_df",
                       "11) Pick 4 guides per gene.RData"
                       )
      )
+
+
+
+
+
+
+
+
+# selected_combined_IDs <- unique(merged_CRISPRko_df[["Combined_ID"]])[1:1000]
+# library("microbenchmark")
+# microbenchmark(
+#   selected_IDs_old_df <- PrioritizeNonOverlapping(merged_CRISPRko_df[merged_CRISPRko_df[["Combined_ID"]] %in% selected_combined_IDs, ],
+#                                                   ID_column = "Combined_ID", parallel_mode = FALSE
+#                                                   ),
+#   selected_IDs_new_df <- PrioritizeNonOverlapping(merged_CRISPRko_df[merged_CRISPRko_df[["Combined_ID"]] %in% selected_combined_IDs, ],
+#                                                   ID_column = "Combined_ID", parallel_mode = FALSE, max_overlaps = Inf
+#                                                   ),
+#   times = 1
+# )
+#
+#
+# identical(selected_IDs_old_df[4305:4310, colnames(selected_IDs_old_df) != "Overlaps_tolerance"],
+#           selected_IDs_new_df[4305:4310, colnames(selected_IDs_new_df) != "Overlaps_tolerance"]
+#           )
+#
+#
+#
+# show_columns <- c("Entrez_ID", "Gene_symbol", "Original_symbol", "Symbol_overlapping_0MM",
+#                   "sgRNA_sequence",
+#                   "Entrez_chromosome", "Chromosome", "Strand",
+#                   "CRISPOR_3MM_specificity", "CRISPOR_Num_0MM", "CRISPOR_Num_1MM",
+#                   "CRISPOR_Num_2MM", "CRISPOR_Num_3MM", "CRISPOR_Num_4MM",
+#                   "Rank", "Best_combination_rank", "Spacing",# "Overlaps_tolerance",
+#                   "Num_overlaps", "Original_rank"
+#                   )
+#
+# identical(selected_IDs_old_df[, !(colnames(selected_IDs_old_df) %in% c("Overlaps_tolerance", "Best_combination_rank"))],
+#           selected_IDs_new_df[, !(colnames(selected_IDs_old_df) %in% c("Overlaps_tolerance", "Best_combination_rank"))]
+#           )
+#
+#
+#
+# cbind(selected_IDs_old_df[4305:4310, 159],
+#   selected_IDs_new_df[4305:4310, 159]
+# )
+#
+#
+#
+#
+#
+# microbenchmark(
+#   selected_IDs_old_df <- SortCombinations(merged_CRISPRko_df[merged_CRISPRko_df[, "Gene_symbol"] %in% "A1BG", ]),
+#   selected_IDs_new_df <- SortCombinations(merged_CRISPRko_df[merged_CRISPRko_df[, "Gene_symbol"] %in% "A1BG", ], max_overlaps = Inf),
+#   times = 1
+# )
+#
+#
+# microbenchmark(
+#   selected_IDs_old_df <- PrioritizeNonOverlapping(merged_CRISPRko_df[merged_CRISPRko_df[["Combined_ID"]] %in% selected_combined_IDs, ]),
+#   selected_IDs_new_df <- PrioritizeNonOverlapping(merged_CRISPRko_df[merged_CRISPRko_df[["Combined_ID"]] %in% selected_combined_IDs, ], max_overlaps = 99),
+#   times = 3
+# )
 
 
 
