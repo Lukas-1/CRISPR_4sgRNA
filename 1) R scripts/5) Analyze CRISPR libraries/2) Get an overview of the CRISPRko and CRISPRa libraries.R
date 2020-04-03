9421fe826c676f285827c3292ea0b018366cd72e
Lukas_1 <lukas.frick@gmail.com>
2020-04-03 10:26:35 +0200

    Split the script for getting an overview over the GPP sgRNA designer into two parts

--- 1) R scripts/5) Analyze CRISPR libraries/2) Get an overview of the CRISPRko and CRISPRa libraries.R
+++ 1) R scripts/5) Analyze CRISPR libraries/2) Get an overview of the CRISPRko and CRISPRa libraries.R
@@ -7,15 +7,14 @@
 general_functions_directory <- "~/CRISPR/1) R scripts/1) R functions"
 source(file.path(general_functions_directory, "01) Retrieving annotation data for a gene.R"))
 source(file.path(general_functions_directory, "02) Translating between Entrez IDs and gene symbols.R"))
-source(file.path(general_functions_directory, "14) Checking for identical subsequences.R"))
-source(file.path(general_functions_directory, "20) Randomly allocating sgRNAs to plate layouts.R"))
+source(file.path(general_functions_directory, "14) Checking for identical subsequences.R")) # For CheckThatFactorIsInOrder
+source(file.path(general_functions_directory, "20) Randomly allocating sgRNAs to plate layouts.R")) # For CRISPRaAreTop4Mat
 
 
 
 # Define folder paths -----------------------------------------------------
 
 CRISPR_root_directory    <- "~/CRISPR"
-CRISPR_input_directory   <- file.path(CRISPR_root_directory, "2) Input data")
 RData_directory          <- file.path(CRISPR_root_directory, "3) RData files")
 general_RData_directory  <- file.path(RData_directory, "1) General")
 CRISPRa_RData_directory  <- file.path(RData_directory, "2) CRISPRa")
@@ -100,67 +99,6 @@ ArePresentInCRISPRDf <- function(entrez_IDs, CRISPR_df) {
   return(results_mat)
 }
 
-
-
-
-
-# Check for genes without sgRNAs from GPP ---------------------------------
-
-are_present_CRISPRko <- collected_entrez_IDs %in% merged_CRISPRko_df[["Entrez_ID"]]
-are_present_CRISPRko_GPP <- collected_entrez_IDs %in% merged_CRISPRko_df[["Entrez_ID"]][grepl("GPP", merged_CRISPRko_df[["Source"]], fixed = TRUE)]
-
-are_present_CRISPRa <- collected_entrez_IDs %in% merged_replaced_CRISPRa_df[["Entrez_ID"]]
-are_present_CRISPRa_GPP <- collected_entrez_IDs %in% merged_replaced_CRISPRa_df[["Entrez_ID"]][grepl("GPP", merged_replaced_CRISPRa_df[["Source"]], fixed = TRUE)]
-
-only_CRISPRa_GPP <- are_present_CRISPRa_GPP & !(are_present_CRISPRko_GPP)
-only_CRISPRko_GPP <- are_present_CRISPRko_GPP & !(are_present_CRISPRa_GPP)
-
-
-
-
-
-# Examine missing genes ---------------------------------------------------
-
-missing_only_for_CRISPRa_df <- collected_entrezs_df[only_CRISPRko_GPP, c("Entrez_ID", "Gene_symbol", "Category")]
-rownames(missing_only_for_CRISPRa_df) <- NULL
-
-missing_only_for_CRISPRko_df <- collected_entrezs_df[only_CRISPRa_GPP, c("Entrez_ID", "Gene_symbol", "Category")]
-rownames(missing_only_for_CRISPRko_df) <- NULL
-
-# All genes that are not in current annotation release, or only annotated on alternate loci, are missing
-table(collected_entrezs_df[["Category"]], !(are_present_CRISPRa_GPP))
-table(collected_entrezs_df[["Category"]], !(are_present_CRISPRko_GPP))
-table(collected_entrezs_df[["Category"]], !(are_present_CRISPRa_GPP & are_present_CRISPRko_GPP))
-
-
-
-
-# Check for targetable genes ----------------------------------------------
-
-not_targetable_categories <- c(
-  "Not in current annotation release",
-  "Only annotated on alternate loci"
-)
-
-are_targetable <- !(collected_entrezs_df[["Category"]] %in% not_targetable_categories)
-
-CRISPRa_redo_entrezs_vec  <- collected_entrez_IDs[are_targetable & !(are_present_CRISPRa_GPP)]
-CRISPRko_redo_entrezs_vec <- collected_entrez_IDs[are_targetable & !(are_present_CRISPRko_GPP)]
-
-
-
-
-
-# Export targetable genes -------------------------------------------------
-
-write.table(CRISPRa_redo_entrezs_vec,
-            file = file.path(annotation_intermediate_files_directory, "Entrez_IDs_not_covered_by_GPP_CRISPRa.txt"),
-            quote = FALSE, row.names = FALSE, col.names = FALSE
-            )
-write.table(CRISPRko_redo_entrezs_vec,
-            file = file.path(annotation_intermediate_files_directory, "Entrez_IDs_not_covered_by_GPP_CRISPRko.txt"),
-            quote = FALSE, row.names = FALSE, col.names = FALSE
-            )
 
 
 
