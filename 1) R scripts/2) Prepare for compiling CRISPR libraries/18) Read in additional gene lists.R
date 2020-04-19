63926993d43252d86cf301a6cce605d843ce2a79
Lukas_1 <lukas.frick@gmail.com>
2020-04-19 13:08:35 +0200

    Map gene symbols to Entrez IDs for our collaborators

---
+++ 1) R scripts/2) Prepare for compiling CRISPR libraries/18) Read in additional gene lists.R
@@ -0,0 +1,86 @@
+### 19th April 2020 ###
+
+
+
+# Import packages and source code -----------------------------------------
+
+library("readxl")
+
+general_functions_directory <- "~/CRISPR/1) R scripts/1) R functions"
+source(file.path(general_functions_directory, "02) Translating between Entrez IDs and gene symbols.R"))
+
+
+
+
+
+# Define folder paths -----------------------------------------------------
+
+CRISPR_root_directory   <- "~/CRISPR"
+general_RData_directory <- file.path(RData_directory, "1) General")
+gene_lists_directory    <- file.path(CRISPR_root_directory, "2) Input data", "Gene lists")
+
+
+
+
+# Load data ---------------------------------------------------------------
+
+load(file.path(general_RData_directory, "02) Map gene symbols to Entrez IDs.RData"))
+load(file.path(general_RData_directory, "06) Collect Entrez IDs from various sources.RData"))
+load(file.path(general_RData_directory, "12) Divide the remaining genes into sublibraries according to hCRISPRa-v2 - sublibrary_df.RData"))
+
+
+
+
+
+# Read in data ------------------------------------------------------------
+
+PD_path <- file.path(gene_lists_directory, "PD_collaborators", "PD-GWAS list.xlsx")
+PD_candidates_df <- data.frame(read_excel(PD_path), stringsAsFactors = FALSE)
+
+
+
+
+# Collect Entrez IDs from our collaborators' PD GWAS study ----------------
+
+## Map to Entrez IDs
+PD_mapped_df <- MapToEntrezs(symbols_vec = PD_candidates_df[[1]])
+
+
+## Identify genes that are unavailable in our library
+are_missing <- is.na(PD_mapped_df[["Entrez_ID"]])
+are_not_protein_coding <- PD_mapped_df[["Entrez_ID"]] %in% collected_entrez_IDs
+are_not_included <- PD_mapped_df[["Entrez_ID"]] %in% unlist(sublibraries_all_entrezs_list)
+
+
+## Examine missing genes
+identical(are_not_protein_coding, are_not_included)
+table(are_missing)
+table(are_not_included)
+
+
+## Define gene lists
+PD_all_entrezs <- PD_mapped_df[["Entrez_ID"]][!(are_missing)]
+PD_4sg_entrezs <- PD_mapped_df[["Entrez_ID"]][!(are_not_included)]
+
+
+
+
+
+
+
+# Save data ---------------------------------------------------------------
+
+save(list = c("PD_all_entrezs", "PD_4sg_entrezs"),
+     file = file.path(general_RData_directory, "18) Read in additional gene lists.RData")
+     )
+
+
+
+
+
+
+
+
+
+
+
