dcb14eaa0b13e40b7f032eff5c4fddbf857fc6af
Lukas_1 <lukas.frick@gmail.com>
2020-04-19 13:12:19 +0200

    Deleted commented-out code

--- 1) R scripts/3) Build CRISPRa library/19) For problematic genes, pick 4 guides without reference to the TSS.R
+++ 1) R scripts/3) Build CRISPRa library/19) For problematic genes, pick 4 guides without reference to the TSS.R
@@ -41,107 +41,6 @@ merged_replaced_CRISPRa_df <- ReplaceUnspacedGuides(merged_replaced_CRISPRa_df)
 
 
 
-
-# # Examine an example gene -------------------------------------------------
-#
-# show_columns <- c("Entrez_ID", "Gene_symbol", "Original_symbol", "sgRNA_sequence",
-#                   "Chromosome", "Strand", "Start", "End", "Cut_location",
-#                   "TSS_number", "Allocated_TSS", "Num_TSSs",
-#                   "TSS_ID", "AltTSS_ID", "Rank", "Best_combination_rank", "Spacing",
-#                   "Overlaps_tolerance", "Num_overlaps", "Original_rank"
-#                   )
-#
-# lax_CRISPRa_df[lax_CRISPRa_df[["Gene_symbol"]] %in% "TRIM74", show_columns][1:10, ]
-# merged_replaced_CRISPRa_df[merged_replaced_CRISPRa_df[["Gene_symbol"]] %in% "TRIM74", show_columns][1:10, ]
-#
-#
-#
-#
-#
-# # Prepare for isolating problematic genes ---------------------------------
-#
-# are_controls <- !(merged_replaced_CRISPRa_df[["Is_control"]] == "No")
-# combined_IDs_vec <- merged_replaced_CRISPRa_df[["Combined_ID"]][!(are_controls)]
-# combined_IDs_fac <- factor(combined_IDs_vec, levels = unique(combined_IDs_vec))
-#
-# CheckThatFactorIsInOrder(combined_IDs_fac)
-#
-#
-#
-#
-# # Identify genes that are to be replaced ----------------------------------
-#
-# are_spaced <- tapply(merged_replaced_CRISPRa_df[["Spacing"]][!(are_controls)],
-#                      combined_IDs_fac,
-#                      function(x) any(x %in% c(12L, 50L))
-#                      )
-#
-# unspaced_gene_IDs <- names(which(!(are_spaced)))
-#
-#
-#
-#
-#
-# # Examine and compare the identified genes --------------------------------
-#
-# length(unspaced_gene_IDs)
-# length(intersect(unspaced_gene_IDs, collected_entrez_IDs))
-#
-# # untargetable_annotations <- c("Not protein-coding", "Only annotated on alternate loci", "Not in current annotation release")
-# # are_targetable <- !(sgRNAs_overview_df[["Gene_annotation_status"]] %in% untargetable_annotations)
-# # are_unspaced_overview <- sgRNAs_overview_df[["Spacing"]] %in% "None"
-# #
-# # unspaced_overview_entrezs <- sgRNAs_overview_df[["Entrez_ID"]][are_targetable & are_unspaced_overview]
-# #
-# # setdiff(intersect(unspaced_gene_IDs, collected_entrez_IDs), unspaced_overview_entrezs)
-#
-#
-#
-#
-#
-# # Replace problematic genes -----------------------------------------------
-#
-# are_to_replace <- merged_replaced_CRISPRa_df[["Combined_ID"]] %in% unspaced_gene_IDs
-#
-# stopifnot(identical(merged_replaced_CRISPRa_df[["Combined_ID"]], lax_CRISPRa_df[["Combined_ID"]]))
-#
-# merged_replaced_CRISPRa_df[["Relaxed_location"]] <- "No"
-# lax_CRISPRa_df[["Relaxed_location"]] <- "Yes"
-#
-# strict_df_splits <- split(merged_replaced_CRISPRa_df[!(are_controls), ],
-#                           combined_IDs_fac[!(are_controls)]
-#                           )
-# lax_df_splits <- split(lax_CRISPRa_df[!(are_controls), !(grepl("_strict$", colnames(lax_CRISPRa_df)))],
-#                        combined_IDs_fac[!(are_controls)]
-#                        )
-#
-# stopifnot(identical(names(strict_df_splits), names(are_spaced)))
-#
-# combined_df_splits <- lapply(seq_along(lax_df_splits), function(x) {
-#   if (are_spaced[[x]]) {
-#     strict_df_splits[[x]]
-#   } else {
-#     lax_df_splits[[x]]
-#   }
-# })
-#
-# combined_df <- do.call(rbind.data.frame,
-#                        c(combined_df_splits,
-#                          list(merged_replaced_CRISPRa_df[are_controls, ]),
-#                          list(stringsAsFactors = FALSE, make.row.names = FALSE)
-#                          )
-#                        )
-#
-# stopifnot(identical(combined_df[["Combined_ID"]], merged_replaced_CRISPRa_df[["Combined_ID"]]))
-#
-# merged_replaced_CRISPRa_df <- combined_df
-
-
-
-
-
-
-
 # Save data ---------------------------------------------------------------
 
 save(list = "merged_replaced_CRISPRa_df",
@@ -149,9 +48,5 @@ save(list = "merged_replaced_CRISPRa_df",
                       "19) For problematic genes, pick 4 guides without reference to the TSS.RData"
                       )
      )
-
-
-
-
 
 
