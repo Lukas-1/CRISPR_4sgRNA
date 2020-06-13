087833822e774b54cf798d1e0d13897375905b1e
Lukas_1 <lukas.frick@gmail.com>
2020-06-13 12:26:12 +0200

    New file number

--- 1) R scripts/2) Prepare for compiling CRISPR libraries/19) Compile all relevant TSSs for each gene.R
+++ 1) R scripts/2) Prepare for compiling CRISPR libraries/19) Compile all relevant TSSs for each gene.R
@@ -21,7 +21,7 @@ general_RData_directory <- file.path(RData_directory, "1) General")
 
 load(file.path(general_RData_directory, "02) Map gene symbols to Entrez IDs.RData"))
 load(file.path(general_RData_directory, "07) Compile TSS (transcription start site) data.RData"))
-load(file.path(general_RData_directory, "09) Divide the entire set of protein-coding genes into chunks - entrez_chunks_list.RData"))
+load(file.path(general_RData_directory, "17) Compile the information on gene type.RData"))
 
 
 
@@ -30,8 +30,23 @@ load(file.path(general_RData_directory, "09) Divide the entire set of protein-co
 
 # Do stuff ----------------------------------------------------------------
 
-
 head(combined_TSS_df)
+head(FANTOM5_df)
+head(BioMart_tidied_df)
+
+grep(",", FANTOM5_df[["Entrez_ID"]], value = TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
