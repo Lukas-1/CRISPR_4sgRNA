cf0589fa737310ca7ca576bc33d6a1152e1b89d2
Lukas_1 <lukas.frick@gmail.com>
2020-06-30 14:42:18 +0200

    Create and export the whole-genome 4sg library for CRISPRi also

--- 1) R scripts/5) Build CRISPRi library/27) Distribute sgRNAs for the whole genome onto plates.R
+++ 1) R scripts/5) Build CRISPRi library/27) Distribute sgRNAs for the whole genome onto plates.R
@@ -33,7 +33,45 @@ load(file.path(general_RData_directory, "06) Collect Entrez IDs from various sou
 load(file.path(general_RData_directory, "12) Divide the remaining genes into sublibraries according to hCRISPRa-v2 - sublibrary_df.RData"))
 load(file.path(CRISPRi_RData_directory, "19) For problematic genes, pick 4 guides without reference to the TSS.RData"))
 
-load(file.path(CRISPRi_RData_directory, "24) Export libraries to .tsv files - vacuolation_entrezs.RData"))
+load(file.path(CRISPRi_RData_directory, "20) Create a gene-based summary of the human genome - vacuolation_entrezs.RData"))
+
+
+
+
+
+
+# Assign all guides to plates ---------------------------------------------
+
+sg4_df <- AllocateAllGuidesToPlates(merged_replaced_CRISPRi_df,
+                                    sublibraries_entrezs_list = sublibraries_all_entrezs_list,
+                                    num_control_wells = 96,
+                                    reorder_df = FALSE
+                                    )
+sg4_df[["Old_order"]] <- seq_len(nrow(sg4_df))
+
+sg4_reordered_df <- ReorderPlates(sg4_df)
+sg4_reordered_df <- RenumberPlatesContinuously(sg4_reordered_df)
+sg4_reordered_df <- AssignPlateStrings(sg4_reordered_df)
+
+sg4_df <- sg4_reordered_df[order(sg4_reordered_df[["Old_order"]]), ]
+
+sg4_df <- sg4_df[, colnames(sg4_df) != "Old_order"]
+sg4_reordered_df <- sg4_reordered_df[, colnames(sg4_reordered_df) != "Old_order"]
+
+
+
+
+# Examine the sub-library allocation --------------------------------------
+
+are_selected <- (sg4_df[["Is_control"]] == "No") &
+                 !(duplicated(sg4_df[["Combined_ID"]]))
+
+table(sg4_df[["Sublibrary_4sg"]][are_selected])
+
+are_selected_TSSs <- (sg4_df[["Is_control"]] == "No") &
+                     !(duplicated(sg4_df[["AltTSS_ID"]]))
+
+sum(are_selected_TSSs) # Number of TSSs targeted by the library
 
 
 
@@ -83,19 +121,37 @@ vac_4sg_reordered_df[["Plate_number"]][are_controls] <- 2L
 
 
 
-# Export the plate layouts ------------------------------------------------
+# Export the vacuolation plate layouts ------------------------------------
 
 vac_4sg_df <- vac_4sg_df[, colnames(vac_4sg_df) != "Sublibrary_4sg"]
 vac_4sg_reordered_df <- vac_4sg_reordered_df[, colnames(vac_4sg_reordered_df) != "Sublibrary_4sg"]
 
-ExportPlates(vac_4sg_df, "Vacuolation_4sg_original_order", sub_folder = "Plate layouts")
-ExportPlates(vac_4sg_reordered_df, "Vacuolation_4sg_reordered", sub_folder = "Plate layouts")
+ExportPlates(vac_4sg_df, "Vacuolation_4sg_original_order", sub_folder = "Plate layout - vacuolation")
+ExportPlates(vac_4sg_reordered_df, "Vacuolation_4sg_reordered", sub_folder = "Plate layout - vacuolation")
 
 for (i in 1:4) {
   use_df <- vac_4sg_reordered_df[vac_4sg_reordered_df[["Rank"]] %in% i, ]
   ExportPlates(use_df,
                paste0("Vacuolation_4sg_reordered_sg", i),
-               sub_folder = "Plate layouts",
+               sub_folder = "Plate layout - vacuolation",
+               add_padding_between_plates = TRUE
+               )
+}
+
+
+
+# Export the whole-genome plate layouts -----------------------------------
+
+use_sub_folder <- "Plate layout - all genes"
+
+ExportPlates(sg4_df, "All_sublibraries_original_order", sub_folder = use_sub_folder)
+ExportPlates(sg4_reordered_df, "All_sublibraries_reordered", sub_folder = use_sub_folder)
+
+for (i in 1:4) {
+  use_df <- sg4_reordered_df[sg4_reordered_df[["Rank"]] %in% i, ]
+  ExportPlates(use_df,
+               paste0("4sg_reordered_sg", i),
+               sub_folder = use_sub_folder,
                add_padding_between_plates = TRUE
                )
 }
@@ -104,11 +160,11 @@ for (i in 1:4) {
 
 
 
-
-
 # Save data ---------------------------------------------------------------
 
-save(list = c("vac_4sg_df", "vac_4sg_reordered_df"),
+save(list = c("sg4_reordered_df", "sg4_df",
+              "vac_4sg_df", "vac_4sg_reordered_df"
+              ),
      file = file.path(CRISPRi_RData_directory, "27) Distribute sgRNAs for the whole genome onto plates.RData")
      )
 
