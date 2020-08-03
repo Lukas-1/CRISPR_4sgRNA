b4d2ac783df8bc48c420d8431b98beac00fa8e0c
Lukas_1 <lukas.frick@gmail.com>
2020-08-03 11:07:46 +0200

    Restructure and expand the code related to the data exported from the BioMart website, so that non-protein-coding genes are better represented

--- 1) R scripts/2) Prepare for compiling CRISPR libraries/17) Compile the information on gene type.R
+++ 1) R scripts/2) Prepare for compiling CRISPR libraries/17) Compile the information on gene type.R
@@ -8,6 +8,7 @@
 general_functions_directory <- "~/CRISPR/1) R scripts/1) R functions"
 source(file.path(general_functions_directory, "01) Retrieving annotation data for a gene.R"))
 source(file.path(general_functions_directory, "02) Translating between Entrez IDs and gene symbols.R"))
+source(file.path(general_functions_directory, "28) Merging the FANTOM5 and BioMart TSS data.R")) # For TidyBioMartChromosomes
 
 
 
@@ -41,50 +42,17 @@ BioMart_gene_type_df <- read.table(file.path(human_genome_directory, "Ensembl", 
 
 
 
-# Explore NCBI's Homo_sapiens.gene_info data ------------------------------
 
-anyNA(NCBI_Hs_info_df[["GeneID"]])
-table(NCBI_Hs_info_df[["type_of_gene"]])
+# Define functions --------------------------------------------------------
 
-any(duplicated(NCBI_Hs_info_df[["GeneID"]]))
-
-
-
-
-
-# Explore the BioMart export (containing gene/transcript type) ------------
-
-table(BioMart_gene_type_df[["Gene type"]])
-table(BioMart_gene_type_df[["Transcript type"]])
-
-
-entrez_column <- "NCBI gene (formerly Entrezgene) ID"
-are_NA_entrezs <- is.na(BioMart_gene_type_df[[entrez_column]])
-
-BioMart_gene_type_df[are_NA_entrezs, ]
-
-table(are_NA_entrezs)
-table(duplicated(BioMart_gene_type_df[[entrez_column]]))
-
-BioMart_type_only_df <- unique(BioMart_gene_type_df[!(are_NA_entrezs), c(entrez_column, "Gene type") ])
-
-colnames(BioMart_type_only_df) <- c("NCBI_gene_ID", "Gene_type")
-rownames(BioMart_type_only_df) <- NULL
-
-any(duplicated(BioMart_type_only_df[["NCBI_gene_ID"]]))
-
-NCBI_IDs_char_vec <- as.character(BioMart_type_only_df[["NCBI_gene_ID"]])
-
-num_occurrences <- table(NCBI_IDs_char_vec)[NCBI_IDs_char_vec]
-duplicates_df <- BioMart_type_only_df[num_occurrences > 1, ]
-duplicates_df <- duplicates_df[order(duplicates_df[["NCBI_gene_ID"]]), ]
-
-
-
-
-
-
-
+CollapseFunction <- function(char_vec) {
+  char_vec <- unique(char_vec)
+  if (all(is.na(char_vec))) {
+    return(NA_character_)
+  } else {
+    return(paste0(char_vec, collapse = ", "))
+  }
+}
 
 
 
@@ -97,7 +65,7 @@ NCBI_gene_type_map <- c(
   "snoRNA"            = "snoRNA",
   "snRNA"             = "snRNA",
   "pseudo"            = "pseudogene",
-  "ncRNA"             = "other",
+  "ncRNA"             = "ncRNA",
   "scRNA"             = "other",
   "biological-region" = "other",
   "other"             = "other",
@@ -165,13 +133,126 @@ BioMart_gene_type_map <- c(
 
 
 
+# Explore NCBI's Homo_sapiens.gene_info data ------------------------------
+
+anyNA(NCBI_Hs_info_df[["GeneID"]])
+table(NCBI_Hs_info_df[["type_of_gene"]])
+
+any(duplicated(NCBI_Hs_info_df[["GeneID"]]))
+
+
+
+
+
+# Explore the BioMart export (containing gene/transcript type) ------------
+
+table(BioMart_gene_type_df[["Gene type"]])
+table(BioMart_gene_type_df[["Transcript type"]])
+
+entrez_column <- "NCBI gene (formerly Entrezgene) ID"
+are_NA_entrezs <- is.na(BioMart_gene_type_df[[entrez_column]])
+
+BioMart_gene_type_df[are_NA_entrezs, ]
+
+table(BioMart_gene_type_df[["Gene type"]][are_NA_entrezs])
+
+anyNA(BioMart_gene_type_df[["Gene stable ID"]])
+
+table(are_NA_entrezs)
+table(duplicated(BioMart_gene_type_df[[entrez_column]]))
+
+
+
+
+
+# Reduce the BioMart table to Entrez ID and gene type ---------------------
+
+BioMart_Entrez_type_only_df <- unique(BioMart_gene_type_df[!(are_NA_entrezs), c(entrez_column, "Gene type") ])
+
+colnames(BioMart_Entrez_type_only_df) <- c("NCBI_gene_ID", "Gene_type")
+rownames(BioMart_Entrez_type_only_df) <- NULL
+
+any(duplicated(BioMart_Entrez_type_only_df[["NCBI_gene_ID"]]))
+
+NCBI_IDs_char_vec <- as.character(BioMart_Entrez_type_only_df[["NCBI_gene_ID"]])
+
+## Explore duplicates (Entrez IDs with multiple gene types)
+num_occurrences <- table(NCBI_IDs_char_vec)[NCBI_IDs_char_vec]
+duplicates_df <- BioMart_Entrez_type_only_df[num_occurrences > 1, ]
+duplicates_df <- duplicates_df[order(duplicates_df[["NCBI_gene_ID"]]), ]
+
+
+
+
 
 # Collect all Entrez IDs --------------------------------------------------
 
 collected_entrezs_vec <- unique(c(NCBI_Hs_info_df[["GeneID"]]),
-                                BioMart_type_only_df[["NCBI_gene_ID"]]
+                                BioMart_Entrez_type_only_df[["NCBI_gene_ID"]]
                                 )
 collected_entrezs_vec <- sort(collected_entrezs_vec)
+
+
+
+
+
+
+
+# Ascertain that ENSG IDs are only for genes that lack Entrez IDs ---------
+
+NA_entrez_ENSGs_vec <- unique(BioMart_gene_type_df[["Gene stable ID"]][are_NA_entrezs])
+are_NA_entrez_ENSG <- BioMart_gene_type_df[["Gene stable ID"]] %in% NA_entrez_ENSGs_vec
+stopifnot(all(is.na(BioMart_gene_type_df[[entrez_column]][are_NA_entrez_ENSG])))
+
+
+
+
+
+# Reduce the BioMart table to Ensembl gene ID and gene type ---------------
+
+BioMart_ENSG_gene_type_df <- unique(BioMart_gene_type_df[are_NA_entrezs, c("Gene stable ID", "Gene type") ])
+
+BioMart_ENSG_gene_type_df <- BioMart_ENSG_gene_type_df[order(BioMart_ENSG_gene_type_df[["Gene stable ID"]]), ]
+
+colnames(BioMart_ENSG_gene_type_df) <- c("Ensembl_gene_ID", "Original_type")
+rownames(BioMart_ENSG_gene_type_df) <- NULL
+
+stopifnot(!(any(duplicated(BioMart_ENSG_gene_type_df[["Ensembl_gene_ID"]]))))
+
+
+
+
+# Collect all Ensembl gene IDs --------------------------------------------
+
+collected_ENSG_vec <- sort(unique(BioMart_ENSG_gene_type_df[["Ensembl_gene_ID"]]))
+
+
+
+
+
+# Complete the data frame for Ensembl gene IDs ----------------------------
+
+BioMart_ENSG_matches <- match(BioMart_ENSG_gene_type_df[["Original_type"]],
+                              names(BioMart_gene_type_map)
+                              )
+BioMart_ENSG_gene_type_df[["Recoded_type"]] <- BioMart_gene_type_map[BioMart_ENSG_matches]
+
+ENSG_ID_matches <- match(BioMart_ENSG_gene_type_df[["Ensembl_gene_ID"]],
+                         BioMart_gene_type_df[["Gene stable ID"]]
+                         )
+
+BioMart_ENSG_gene_type_df[["BioMart_gene_name"]] <- BioMart_gene_type_df[["Gene name"]][ENSG_ID_matches]
+
+BioMart_ENSG_gene_type_df[["TSS"]] <- BioMart_gene_type_df[["Transcription start site (TSS)"]][ENSG_ID_matches]
+BioMart_ENSG_gene_type_df[["Chromosome"]] <- TidyBioMartChromosomes(BioMart_gene_type_df[["Chromosome/scaffold name"]][ENSG_ID_matches])
+BioMart_ENSG_gene_type_df[["Strand"]] <- ifelse(BioMart_gene_type_df[["Strand"]][ENSG_ID_matches] == -1, "-", "+")
+
+BioMart_ENSG_columns <- c("Ensembl_gene_ID", "BioMart_gene_name",
+                          "Recoded_type", "Original_type",
+                          "Chromosome", "Strand", "TSS"
+                          )
+BioMart_ENSG_gene_type_df <- BioMart_ENSG_gene_type_df[, BioMart_ENSG_columns]
+
 
 
 
@@ -187,76 +268,69 @@ NCBI_types_recoded_vec <- NCBI_gene_type_map[NCBI_types_matches]
 
 
 
-# Process BioMart's gene type information ---------------------------------
+# Process BioMart's gene type information for Entrez IDs ------------------
 
 BioMart_types_list <- lapply(collected_entrezs_vec, function(x) {
-  are_this_entrez <- which(BioMart_type_only_df[["NCBI_gene_ID"]] == x)
+  are_this_entrez <- which(BioMart_Entrez_type_only_df[["NCBI_gene_ID"]] == x)
   if (any(are_this_entrez)) {
-    return(unique(BioMart_type_only_df[["Gene_type"]][are_this_entrez]))
+    return(unique(BioMart_Entrez_type_only_df[["Gene_type"]][are_this_entrez]))
   } else {
     return(NA_integer_)
   }
 })
 
-BioMart_expanded_df <- data.frame(
-  "Entrez_ID" = rep(collected_entrezs_vec, lengths(BioMart_types_list)),
-  "Original_type" = unlist(BioMart_types_list, use.names = FALSE),
+BioMart_entrez_expanded_df <- data.frame(
+  "Entrez_ID"      = rep(collected_entrezs_vec, lengths(BioMart_types_list)),
+  "Original_type"  = unlist(BioMart_types_list, use.names = FALSE),
   stringsAsFactors = FALSE
 )
 
 new_order <- order(
-  BioMart_expanded_df[["Entrez_ID"]],
-  match(BioMart_expanded_df[["Original_type"]],
+  BioMart_entrez_expanded_df[["Entrez_ID"]],
+  match(BioMart_entrez_expanded_df[["Original_type"]],
         names(BioMart_gene_type_map)
         )
 )
-BioMart_expanded_df <- BioMart_expanded_df[new_order, ]
-rownames(BioMart_expanded_df) <- NULL
+BioMart_entrez_expanded_df <- BioMart_entrez_expanded_df[new_order, ]
+rownames(BioMart_entrez_expanded_df) <- NULL
 
 
-BioMart_types_matches <- match(BioMart_expanded_df[["Original_type"]],
-                               names(BioMart_gene_type_map)
-                               )
-BioMart_expanded_df[["Type_recoded"]] <- BioMart_gene_type_map[BioMart_types_matches]
+BioMart_entrez_matches <- match(BioMart_entrez_expanded_df[["Original_type"]],
+                                names(BioMart_gene_type_map)
+                                )
+BioMart_entrez_expanded_df[["Recoded_type"]] <- BioMart_gene_type_map[BioMart_entrez_matches]
 
 
 
 
 
-# Summarize BioMart's gene type information -------------------------------
+# Summarize BioMart's gene type information for Entrez IDs ----------------
 
-entrezs_fac <- factor(BioMart_expanded_df[["Entrez_ID"]],
-                      levels = unique(BioMart_expanded_df[["Entrez_ID"]])
+entrezs_fac <- factor(BioMart_entrez_expanded_df[["Entrez_ID"]],
+                      levels = unique(BioMart_entrez_expanded_df[["Entrez_ID"]])
                       )
+stopifnot(identical(collected_entrezs_vec, as.integer(levels(entrezs_fac))))
 
-CollapseFunction <- function(char_vec) {
-  char_vec <- unique(char_vec)
-  if (all(is.na(char_vec))) {
-    return(NA_character_)
-  } else {
-    return(paste0(char_vec, collapse = ", "))
-  }
-}
-original_types_collapsed <- tapply(BioMart_expanded_df[["Original_type"]],
+original_types_collapsed <- tapply(BioMart_entrez_expanded_df[["Original_type"]],
                                    entrezs_fac,
                                    CollapseFunction
                                    )
-recoded_types_collapsed <- tapply(BioMart_expanded_df[["Type_recoded"]],
+recoded_types_collapsed <- tapply(BioMart_entrez_expanded_df[["Recoded_type"]],
                                   entrezs_fac,
                                   CollapseFunction
                                   )
-single_type_vec <- tapply(BioMart_expanded_df[["Type_recoded"]],
-                          entrezs_fac,
-                          function(x) x[[1]]
-                          )
+entrez_single_type_vec <- tapply(BioMart_entrez_expanded_df[["Recoded_type"]],
+                                 entrezs_fac,
+                                 function(x) x[[1]]
+                                 )
 
 
 
 
 # Check for discrepancies between NCBI and BioMart ------------------------
 
-are_identical <- mapply(identical, NCBI_types_recoded_vec, single_type_vec)
-have_NA <- is.na(NCBI_types_recoded_vec) | is.na(single_type_vec)
+are_identical <- mapply(identical, NCBI_types_recoded_vec, entrez_single_type_vec)
+have_NA <- is.na(NCBI_types_recoded_vec) | is.na(entrez_single_type_vec)
 are_identical <- are_identical & !(have_NA)
 are_conflicting <- !(are_identical) & !(have_NA)
 
@@ -266,17 +340,17 @@ table(are_conflicting)
 
 
 
-# Collate the data --------------------------------------------------------
+# Collate the data for Entrez IDs -----------------------------------------
 
-gene_type_df <- data.frame(
+entrez_to_gene_type_df <- data.frame(
   "Entrez_ID"              = collected_entrezs_vec,
   "Gene_symbol"            = MapToEntrezs(as.character(collected_entrezs_vec))[["Gene_symbol"]],
   "Consensus_type"         = ifelse(is.na(NCBI_types_recoded_vec),
-                                    single_type_vec,
+                                    entrez_single_type_vec,
                                     NCBI_types_recoded_vec
                                     ),
   "Recoded_type_NCBI"      = NCBI_types_recoded_vec,
-  "Recoded_type_BioMart"   = single_type_vec,
+  "Recoded_type_BioMart"   = entrez_single_type_vec,
   "Is_conflicting"         = ifelse(have_NA, NA, are_conflicting),
   "Original_type_NCBI"     = NCBI_types_vec,
   "Original_types_BioMart" = original_types_collapsed,
@@ -290,8 +364,9 @@ gene_type_df <- data.frame(
 
 # Perform some checks -----------------------------------------------------
 
-table(gene_type_df[["Is_consistent"]], useNA = "ifany")
-gene_type_df[gene_type_df[["Is_conflicting"]] %in% TRUE, ]
+table(entrez_to_gene_type_df[["Is_consistent"]], useNA = "ifany")
+entrez_to_gene_type_df[entrez_to_gene_type_df[["Is_conflicting"]] %in% TRUE, ]
+
 
 
 
@@ -299,7 +374,7 @@ gene_type_df[gene_type_df[["Is_conflicting"]] %in% TRUE, ]
 
 # Save data ---------------------------------------------------------------
 
-save("gene_type_df",
+save(list = c("entrez_to_gene_type_df", "BioMart_ENSG_gene_type_df"),
      file = file.path(general_RData_directory, "17) Compile the information on gene type.RData")
      )
 
