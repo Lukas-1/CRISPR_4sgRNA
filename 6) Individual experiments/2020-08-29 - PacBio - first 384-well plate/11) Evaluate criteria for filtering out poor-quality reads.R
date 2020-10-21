8203c605b2b3f4877e2b38b39b796ec71e8dd04e
Lukas_1 <lukas.frick@gmail.com>
2020-10-21 17:02:19 +0200

    General update

--- 6) Individual experiments/2020-08-29 - PacBio - first 384-well plate/11) Evaluate criteria for filtering out poor-quality reads.R
+++ 6) Individual experiments/2020-08-29 - PacBio - first 384-well plate/11) Evaluate criteria for filtering out poor-quality reads.R
@@ -42,12 +42,17 @@ row.names(use_df) <- NULL
 
 # Check various cutoffs ---------------------------------------------------
 
+table(use_df[["BC_combined_score"]])
+table(use_df[["BC_score_lead"]])
+
 
 combined_score_cutoff <- 80
 score_lead_cutoff <- 40
-
 mean_quality_cutoff <- 85
+close_well_range <- 3L
 
+combined_score_low_cutoff <- 60
+score_lead_low_cutoff <- 30
 
 table(use_df[["BC_combined_score"]] < 100, useNA = "ifany")
 
@@ -55,11 +60,98 @@ table(use_df[["BC_combined_score"]] < 80, useNA = "ifany")
 table(use_df[["BC_score_lead"]] < 40, useNA = "ifany")
 table(use_df[["Mean_quality"]] < 85, useNA = "ifany")
 
-
 are_poor_barcodes <- (use_df[["BC_combined_score"]] < combined_score_cutoff) |
                      (use_df[["BC_score_lead"]] < score_lead_cutoff)
 
+are_very_poor_barcodes <- (use_df[["BC_combined_score"]] < combined_score_low_cutoff) |
+                          (use_df[["BC_score_lead"]] < score_lead_low_cutoff)
+
 are_poor_quality <- use_df[["Mean_quality"]] < mean_quality_cutoff
+
+
+
+
+# Check for associations with contaminations ------------------------------
+
+are_close_contams <- use_df[["Random_distance"]] <= close_well_range
+
+are_standard_lengths <- use_df[["Length"]] %in% 2223:2229
+
+are_contaminated <- use_df[["Contam_guides"]] >= 1
+
+
+fisher.test(table("are_contam" = are_contaminated,
+                  "bad_bc"     = are_poor_barcodes
+                  ))
+
+fisher.test(table("are_close" = are_close_contams,
+                  "bad_bc"    = are_poor_barcodes
+                  ))
+wilcox.test(use_df[["Random_distance"]] ~ are_poor_barcodes)
+
+
+
+fisher.test(table("are_contam"      = are_contaminated,
+                  "standard_length" = are_standard_lengths
+                  ))
+
+fisher.test(table("standard_length" = are_standard_lengths,
+                  "bad_bc"          = are_poor_barcodes
+                  ))
+
+
+
+
+
+# Evaluate my own barcode criteria ----------------------------------------
+
+table(use_df[["Correct_barcodes"]])
+table(use_df[["Starts_with_row_barcode"]] & use_df[["Ends_with_column_barcode"]])
+
+table(use_df[["Starts_with_row_barcode"]])
+table(use_df[["Ends_with_column_barcode"]])
+
+table(use_df[["Correct_row_flank"]])
+table(use_df[["Correct_column_flank"]])
+
+table(use_df[["Row_bc_length"]])
+table(use_df[["Column_bc_length"]])
+
+table(use_df[["Row_bc_length"]] >= 8)
+table(use_df[["Column_bc_length"]] >= 8)
+
+have_correct_row <- use_df[["Starts_with_row_barcode"]] |
+                    ((use_df[["Row_bc_length"]] >= 8) &
+                      use_df[["Correct_row_flank"]]
+                    )
+have_correct_column <- use_df[["Ends_with_column_barcode"]] |
+                       ((use_df[["Column_bc_length"]] >= 8) &
+                         use_df[["Correct_column_flank"]]
+                        )
+
+
+are_correct <- have_correct_row & have_correct_column
+
+table(are_correct)
+table(are_correct & !(are_very_poor_barcodes))
+table(are_correct & !(are_poor_barcodes))
+
+only_8_bp <- (use_df[["Column_bc_length"]] <= 8) | (use_df[["Row_bc_length"]] <= 8)
+
+use_df[are_correct & are_poor_barcodes, ]
+use_df[are_correct & are_poor_barcodes & only_8_bp, ]
+
+
+fisher.test(table("are_contam" = are_contaminated[are_correct],
+                  "bad_bc"     = are_poor_barcodes[are_correct]
+                  ))
+
+fisher.test(table("are_contam" = are_contaminated[are_correct & !(are_very_poor_barcodes)],
+                  "bad_bc"     = are_poor_barcodes[are_correct & !(are_very_poor_barcodes)]
+                  ))
+
+
+
 
 
 
@@ -82,47 +174,8 @@ table(use_df[["BC_combined_score"]][are_short_bc])
 
 use_df[are_short_bc & !(are_poor_barcodes), ]
 
-goo
 
 
-
-# Check for associations with contaminations ------------------------------
-
-use_reads_df <- sl7_ccs3_df_list[["individual_reads_df"]]
-
-bc_comb_cutoff <- 80
-bc_lead_cutoff <- 40
-close_well_range <- 3L
-are_poor_barcodes <- (use_reads_df[["BC_combined_score"]] < bc_comb_cutoff) &
-                     (use_reads_df[["BC_score_lead"]] < bc_lead_cutoff)
-
-are_close_contams <- use_reads_df[["Random_distance"]] <= close_well_range
-
-are_standard_lengths <- use_reads_df[["Length"]] %in% 2223:2229
-
-are_contaminated <- use_reads_df[["Contam_guides"]] >= 1
-
-
-fisher.test(table("are_contam" = are_contaminated,
-                  "bad_bc"     = are_poor_barcodes
-                  ))
-
-fisher.test(table("are_close" = are_close_contams,
-                  "bad_bc"    = are_poor_barcodes
-                  ))
-wilcox.test(use_reads_df[["Random_distance"]] ~
-              are_poor_barcodes
-            )
-
-
-
-fisher.test(table("are_contam"      = are_contaminated,
-                  "standard_length" = are_standard_lengths
-                  ))
-
-fisher.test(table("standard_length" = are_standard_lengths,
-                  "bad_bc"          = are_poor_barcodes
-                  ))
 
 
 
