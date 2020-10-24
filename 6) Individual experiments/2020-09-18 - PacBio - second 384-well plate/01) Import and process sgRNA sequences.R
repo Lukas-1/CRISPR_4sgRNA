fbe0e7bc04c8937beba3b432bbf6c928aa3208b7
Lukas_1 <lukas.frick@gmail.com>
2020-10-24 11:41:48 +0200

    Export the sgRNA sequences data frame to a text file (to simplify the lookup of sgRNAs for a specific well number)

--- 6) Individual experiments/2020-09-18 - PacBio - second 384-well plate/01) Import and process sgRNA sequences.R
+++ 6) Individual experiments/2020-09-18 - PacBio - second 384-well plate/01) Import and process sgRNA sequences.R
@@ -19,6 +19,7 @@ CRISPR_root_directory  <- "~/CRISPR"
 plate2_directory       <- file.path(CRISPR_root_directory, "6) Individual experiments/2020-09-18 - PacBio - second 384-well plate")
 file_input_directory   <- file.path(plate2_directory, "1) Input")
 p2_R_objects_directory <- file.path(plate2_directory, "2) R objects")
+file_output_directory  <- file.path(plate2_directory, "3) Output")
 
 metadata_directory     <- file.path(file_input_directory, "Metadata")
 sg_sequences_file      <- file.path(metadata_directory, "sgRNA reference.xlsx")
@@ -91,11 +92,11 @@ colnames(guide_seq_mat) <- paste0("Sequence_sg", 1:4)
 sg_sequences_df <- data.frame(
   "Well_number"         = wells_map[gRNAs_excel_df[["Wells"]]],
   "Well_code"           = gRNAs_excel_df[["Wells"]],
+  "Block"               = block_vec,
   "Target_gene"         = plasmid_names,
   "Modality"            = modalities_map[raw_modalities],
   guide_seq_mat,
   "Longest_subsequence" = apply(guide_seq_mat, 1, LongestSharedSubsequence),
-  "Block"               = block_vec,
   stringsAsFactors      = FALSE,
   row.names             = NULL
 )
@@ -109,6 +110,17 @@ t(apply(guide_seq_mat[have_short_guides, ], 1, nchar))
 sg_sequences_df[have_short_guides, ]
 
 
+
+
+# Export the sgRNA sequences data frame (including well number) -----------
+
+write.table(sg_sequences_df,
+            file      = file.path(file_output_directory, paste0("sgRNAs_and_wells.tsv")),
+            sep       = "\t",
+            col.names = TRUE,
+            row.names = FALSE,
+            quote     = FALSE
+            )
 
 
 
