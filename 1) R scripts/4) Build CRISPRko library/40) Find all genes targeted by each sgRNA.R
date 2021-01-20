6f72c5c6f85826b4c1c9f90ba499172b78b72779
Lukas_1 <lukas.frick@gmail.com>
2021-01-20 15:25:45 +0100

    Implement 4sg combinations

--- 1) R scripts/4) Build CRISPRko library/40) Find all genes targeted by each sgRNA.R
+++ 1) R scripts/4) Build CRISPRko library/40) Find all genes targeted by each sgRNA.R
@@ -7,6 +7,8 @@
 
 general_functions_directory <- "~/CRISPR/1) R scripts/1) R functions"
 source(file.path(general_functions_directory, "30) Finding overlapping genes and nearby TSSs.R"))
+source(file.path(general_functions_directory, "20) Randomly allocating sgRNAs to plate layouts.R")) # For Are4sg
+source(file.path(general_functions_directory, "15) Finding non-overlapping sgRNAs.R")) # For CheckThatFactorIsInOrder
 
 
 
@@ -23,6 +25,7 @@ CRISPRko_RData_directory <- file.path(RData_directory, "3) CRISPRko")
 
 # Load data ---------------------------------------------------------------
 
+load(file.path(general_RData_directory, "06) Collect Entrez IDs from various sources.RData"))
 load(file.path(general_RData_directory, "12) Divide the remaining genes into sublibraries according to hCRISPRa-v2 - sublibrary_df.RData"))
 load(file.path(general_RData_directory, "21) Assemble data frames of gene, transcript, exon and CDS coordinates.RData"))
 load(file.path(CRISPRko_RData_directory, "11) Pick 4 guides per gene.RData"))
@@ -31,82 +34,208 @@ load(file.path(CRISPRko_RData_directory, "11) Pick 4 guides per gene.RData"))
 
 
 
-# Define functions --------------------------------------------------------
+# Find overlapping genes for 4sg combinations (predicted deletions) -------
+
+are_4sg <- Are4sg(merged_CRISPRko_df, sublibraries_all_entrezs_list)
+
+deletions_CDS_list <- FindOverlapsWithDeletions(merged_CRISPRko_df[are_4sg, ],
+                                                CDS_or_exon_locations_df
+                                                )
+deletions_exons_list <- FindOverlapsWithDeletions(merged_CRISPRko_df[are_4sg, ],
+                                                  exon_locations_df
+                                                  )
 
 
 
 
-# Do stuff ----------------------------------------------------------------
+# Find overlapping genes for sgRNAs ---------------------------------------
 
-example_entrezs <- unique(merged_CRISPRko_df[["Entrez_ID"]])[1:1000]
-# example_entrezs <- unlist(sublibraries_all_entrezs_list, use.names = FALSE)
-are_example_genes <- merged_CRISPRko_df[["Entrez_ID"]] %in% example_entrezs
+guides_CDS_list <- FindOverlappingGenes(merged_CRISPRko_df[are_example_genes, ],
+                                        CDS_or_exon_locations_df
+                                        )
 
+guides_exons_list <- FindOverlappingGenes(merged_CRISPRko_df[are_example_genes, ],
+                                          exon_locations_df
+                                          )
 
-CRISPR_df <- merged_CRISPRko_df[are_example_genes, ]
+table(overlap_list[["summary_df"]][["Affects_intended_gene"]])
+table(overlap_list_2[["summary_df"]][["Affects_intended_gene"]])
 
+summary_df <- overlap_list[["summary_df"]]
 
-stopifnot(!(anyNA(CRISPR_df[["Entrez_ID"]])))
-stopifnot(!(any(grepl(",", CRISPR_df[["Entrez_ID"]], fixed = TRUE))))
-
-
-genes_df <- PrepareGenesDf(exon_locations_df)
-
-
-# are_same <- mapply(identical,
-#                    lengths(strsplit(genes_df[["Entrez_IDs"]], ", ", fixed = TRUE)),
-#                    lengths(strsplit(genes_df[["Gene_symbols"]], ", ", fixed = TRUE))
-#                    )
+all_entrezs <- merged_CRISPRko_df[["Entrez_ID"]][are_4sg]
 
 
-rename_gene_columns <- c(
-  "Gene_IDs" = "Affected_gene_IDs",
-  "Source"   = "Gene_source"
-)
 
-for (column_name in names(rename_gene_columns)) {
-  names(genes_df)[names(genes_df) == column_name] <- rename_gene_columns[[column_name]]
+
+
+
+comb_blocks <- Get4sgProjectedDeletions(merged_CRISPRko_df[are_4sg, ])
+comb_no_blocks <- Get4sgProjectedDeletions(merged_CRISPRko_df[are_4sg, ],
+                                           split_large_deletions = FALSE
+                                           )
+comb_only_primary_blocks <- Get4sgProjectedDeletions(merged_CRISPRko_df[are_4sg, ],
+                                                     only_primary_location = TRUE
+                                                     )
+comb_only_primary_no_blocks <- Get4sgProjectedDeletions(merged_CRISPRko_df[are_4sg, ],
+                                                        only_primary_location = TRUE,
+                                                        split_large_deletions = FALSE
+                                                        )
+
+reduced_df <- comb_blocks[["reduced_df"]]
+
+reduced_df[["Factor"]] <- reduced_df[["Span"]] / 10^6
+
+reduced_df <- reduced_df[order(reduced_df[["Span"]], decreasing = TRUE), ]
+
+goo
+
+
+
+
+
+
+reduced_df <- comb_no_blocks[["reduced_df"]]
+
+reduced_df[["Factor"]] <- reduced_df[["Span"]] / 10^6
+
+reduced_df <- reduced_df[order(reduced_df[["Span"]], decreasing = TRUE), ]
+head(reduced_df)
+
+
+reduced_df[reduced_df[["Span"]] > 10^6, ]
+
+
+gaps <- reduced_df[["End"]] - reduced_df[["Start"]]
+
+
+reduced_df[reduced_df[["Span"]] > 10^6, ]
+
+
+
+expanded_df <- comb_blocks[["expanded_df"]]
+
+expanded_df[expanded_df[["List_index"]] %in% 19471, ]
+
+
+reduced_df[((gaps > 0) %in% TRUE) & ((gaps < 3) %in% TRUE), ]
+
+are_chosen <- are_first | are_last
+
+expanded_df <- expanded_df[are_chosen, ]
+
+index_chromosomes <- index_chromosomes[are_chosen]
+
+expanded_df[expanded_df[["List_index"]] %in% 1763, ]
+
+
+
+
+
+ex_sub_df <- ex_CRISPR_df[ex_CRISPR_df[["Entrez_ID"]] %in% "276", ]
+
+
+
+# summary_df <- overlap_list[["summary_df"]]
+#
+#
+#
+# shared_columns <- intersect(names(summary_df),
+#                             names(new_summary_df)
+#                             )
+# shared_columns <- setdiff(shared_columns, c("Affected_gene_IDs", "Affected_gene_symbols"))
+#
+#
+# identical(summary_df[, shared_columns], new_summary_df[, shared_columns])
+#
+# are_same <- lengths(affected_symbols_results[["list"]]) == lengths(affected_genes_results[["list"]])
+#
+# lengths(affected_symbols_results[["list"]])[!(are_same)]
+# lengths(affected_genes_results[["list"]])[!(are_same)]
+#
+# summary_df[!(are_same), ]
+#
+#
+# stopifnot(identical(lengths(affected_symbols_results[["list"]]),
+#                     lengths(affected_genes_results[["list"]])
+#                     ))
+#
+#
+#
+# try_df <- full_df[full_df[["Index"]] %in% c(469005, 469008, 469014, 469029 ), ]
+#
+#
+# try_df <- full_df[full_df[["Index"]] %in% c(469005), ]
+#
+# try_df <- unique(try_df[, c("Index", "Affected_gene_symbols", "Affected_Entrez_IDs", "Affected_gene_IDs")])
+# num_occurrences_vec <- table(try_df[["Affected_gene_symbols"]])[try_df[["Affected_gene_symbols"]]]
+#
+# try_df[num_occurrences_vec > 1, ]
+#
+#
+#
+#
+# CDS_or_exon_locations_df[CDS_or_exon_locations_df[["Gene_symbols"]] %in% "LINC00904", ]
+#
+#
+
+#
+# symbol_df <- delete_symbol_df
+# gene_ID_df <- delete_gene_ID_df
+#
+#
+# ## DELETE THIS::
+# #
+shared_columns <- intersect(names(symbol_df), names(gene_ID_df ))
+
+identical(symbol_df[, shared_columns], gene_ID_df[, shared_columns])
+
+for (column in shared_columns) {
+  print(paste0(column, ":", identical(symbol_df[[column]], gene_ID_df[[column]])))
 }
 
-split_results <- Process0MMLoci(CRISPR_df)
-unique_loci_df <- split_results[["unique_df"]]
-expanded_0MM_df <- split_results[["expanded_df"]]
-rm(split_results)
+are_same <- mapply(identical, symbol_df[["Num_affected_genes"]], gene_ID_df[["Num_affected_genes"]])
+symbol_df[!(are_same), ]
+gene_ID_df[!(are_same), ]
 
-combined_df <- FindOverlappingHits(unique_loci_df, genes_df)
-full_combined_df <- AlignHits(expanded_0MM_df, combined_df)
+symbol_df[!(are_same), "Num_affected_genes"]
+gene_ID_df[!(are_same), "Num_affected_genes"]
 
-
-
-identical(lengths(strsplit(full_combined_df[["Affected_Entrez_IDs"]], ", ", fixed = TRUE)),
-          lengths(strsplit(full_combined_df[["Affected_gene_symbols"]], ", ", fixed = TRUE))
-          )
-
-
-combined_columns <- c(
-  "Locus_0MM",
-  "Index", "Is_primary_location", "Intended_Entrez_ID", "Affected_Entrez_IDs",
-  "Number_of_gene_IDs", "Number_of_Entrez_IDs",
-  "Intended_gene_symbol", "Affected_gene_symbols", "Num_loci",
-  "Guide_locus", "Gene_locus", "Chromosome",
-  "Affected_gene_IDs", "Ensembl_gene_IDs", "Ensembl_transcript_ID", "Exon_ID", "Strand",
-  "Gene_types", "Gene_source"
-)
-
-full_combined_df <- full_combined_df[, combined_columns]
-
-stopifnot(identical(unique(full_combined_df[["Index"]]),
-                    seq_len(nrow(CRISPR_df))
-                    )
-          )
+# symbol_df <- symbol_df[, shared_columns]
+# gene_ID_df <- gene_ID_df[, shared_columns]
+# identical(symbol_df, gene_ID_df)
+#
+#
+# are_same <- mapply(identical,
+#                    gene_ID_df[["Loci_targeting_intended_gene"]],
+#                    symbol_df[["Loci_targeting_intended_gene"]]
+#                    )
+#
+# table(are_same)
+#
+# head(gene_ID_df[!(are_same), ])
+# head(symbol_df[!(are_same), ])
 
 
-summary_df <- SummarizeFullDf(full_combined_df)
 
-stopifnot(nrow(summary_df) == nrow(CRISPR_df))
+are_4sg <- Are4sg(merged_CRISPRko_df, sublibraries_all_entrezs_list)
+
+CRISPR_df <- merged_CRISPRko_df[are_4sg, ]
+input_genes_df <- CDS_or_exon_locations_df
+
+only_protein_coding  <- FALSE
+exclude_pseudogenes  <- TRUE
+only_known_gene_type <- TRUE
+require_Entrez_ID    <- FALSE
 
 
-summary_df <- SummarizeSummaryDf(summary_df)
+
+shared_columns <- intersect(names(symbol_df), names(gene_ID_df))
+symbol_df[!(are_same), "Num_affected_genes"]
+gene_ID_df[!(are_same), "Num_affected_genes"]
+
+
+
 
 
 
