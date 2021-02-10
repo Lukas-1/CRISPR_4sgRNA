346c9dd99af2599246dd3eccda6290fbd462dbde
Lukas_1 <lukas.frick@gmail.com>
2021-02-10 19:53:18 +0100

    Incorrect variable name fix

--- 1) R scripts/3) Build CRISPRa library/29) Create 4sg plasmid sequences.R
+++ 1) R scripts/3) Build CRISPRa library/29) Create 4sg plasmid sequences.R
@@ -17,7 +17,7 @@ source(file.path(general_functions_directory, "27) Creating 4sg plasmid sequence
 CRISPR_root_directory    <- "~/CRISPR"
 RData_directory          <- file.path(CRISPR_root_directory, "3) RData files")
 general_RData_directory  <- file.path(RData_directory, "1) General")
-CRISPRi_RData_directory  <- file.path(RData_directory, "2) CRISPRa")
+CRISPRa_RData_directory  <- file.path(RData_directory, "2) CRISPRa")
 file_output_directory    <- file.path(CRISPR_root_directory, "5) Output", "CRISPRa")
 plasmid_output_directory <- file.path(file_output_directory, "Plasmids")
 
@@ -26,7 +26,7 @@ plasmid_output_directory <- file.path(file_output_directory, "Plasmids")
 
 # Load data ---------------------------------------------------------------
 
-load(file.path(CRISPRi_RData_directory, "28) Distribute sgRNAs for the whole genome onto plates.RData"))
+load(file.path(CRISPRa_RData_directory, "28) Distribute sgRNAs for the whole genome onto plates.RData"))
 
 
 
