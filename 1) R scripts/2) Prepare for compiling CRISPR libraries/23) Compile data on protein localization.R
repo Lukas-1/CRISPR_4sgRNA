a51d98f238634f2f24686bbe25a4cd557af445c9
Lukas_1 <lukas.frick@gmail.com>
2021-04-22 11:36:27 +0200

    Completed compilation of the surfaceome

--- 1) R scripts/2) Prepare for compiling CRISPR libraries/23) Compile data on protein localization.R
+++ 1) R scripts/2) Prepare for compiling CRISPR libraries/23) Compile data on protein localization.R
@@ -113,10 +113,10 @@ WriteUniProtIDs(silico_HeLa_df[["UniProt ID"]],
 mass_spec_entrezs_df  <- read.delim(file.path(UniProt_output_directory, "2015 - A Mass Spectrometric-Derived Cell Surface Protein Atlas - UniProt accessions to Entrez IDs.tab"),
                                     stringsAsFactors = FALSE, check.names = FALSE
                                     )
-SURFY_name_entrezs_df  <- read.delim(file.path(UniProt_output_directory, "2018 - The in silico human surfaceome - SURFY - UniProt names to Entrez IDs.tab"),
+SURFY_name_entrezs_df <- read.delim(file.path(UniProt_output_directory, "2018 - The in silico human surfaceome - SURFY - UniProt names to Entrez IDs.tab"),
                                     stringsAsFactors = FALSE, check.names = FALSE
                                     )
-SURFY_acc_entrezs_df <- read.delim(file.path(UniProt_output_directory, "2018 - The in silico human surfaceome - SURFY - UniProt accessions to Entrez IDs.tab"),
+SURFY_acc_entrezs_df  <- read.delim(file.path(UniProt_output_directory, "2018 - The in silico human surfaceome - SURFY - UniProt accessions to Entrez IDs.tab"),
                                     stringsAsFactors = FALSE, check.names = FALSE
                                     )
 HeLa_entrezs_df       <- read.delim(file.path(UniProt_output_directory, "2018 - The in silico human surfaceome - HeLa - UniProt accessions to Entrez IDs.tab"),
@@ -139,44 +139,6 @@ CategorizeHPAReliability <- function(char_vec) {
   return(results_fac)
 }
 
-#
-# ProcessSurfaceEntrezs <- function(mappings_df, UniProt_IDs) {
-#   entrez_IDs <- unique(mappings_df[["To"]])
-#   results_list <- lapply(entrez_IDs, function(x) {
-#     are_this_ID <- mappings_df[["To"]] == x
-#     if (!(any(are_this_ID))) {
-#       results_vec <- c("Num_IDs" = 0L, "Num_present" = 0L, "Num_absent" = 0L)
-#     } else {
-#       these_IDs <- mappings_df[["From"]][are_this_ID]
-#       are_present <- these_IDs %in% UniProt_IDs
-#       results_list <- list(
-#         "Entrez_ID"   = x,
-#         "Num_IDs"     = sum(are_this_ID),
-#         "Num_present" = sum(are_present),
-#         "Num_absent"  = sum(!(are_present)),
-#         "Present_IDs" = paste0(these_IDs[are_present], collapse = ", "),
-#         "Absent_IDs"  = paste0(these_IDs[!(are_present)], collapse = ", ")
-#       )
-#     }
-#   })
-#   results_df <- do.call(rbind.data.frame,
-#                         c(results_list, list(stringsAsFactors = FALSE,
-#                                              make.row.names = FALSE
-#                                              )
-#                           )
-#                         )
-#   return(results_df)
-# }
-#
-#
-#
-# GetSurfaceEntrezs <- function(mappings_df, UniProt_IDs) {
-#   entrezs_df <- ProcessSurfaceEntrezs(mappings_df, UniProt_IDs)
-#   entrezs_vec <- entrezs_df[["Entrez_ID"]][entrezs_df[["Num_present"]] >= 1]
-#   return(entrezs_vec)
-# }
-
-
 
 MapUniProtToEntrezs <- function(mappings_df, UniProt_IDs) {
   vapply(UniProt_IDs, function(x) {
@@ -190,6 +152,87 @@ MapUniProtToEntrezs <- function(mappings_df, UniProt_IDs) {
 }
 
 
+SplitEntrezs <- function(entrezs_vec) {
+  results_vec <- unlist(strsplit(entrezs_vec, ", ", fixed = TRUE), use.names = FALSE)
+  results_vec <- setdiff(unique(as.integer(results_vec)), NA_integer_)
+  return(results_vec)
+}
+
+
+
+GetMSEntrezs <- function(UniProt_IDs) {
+  matches_vec <- match(UniProt_IDs, mass_spec_AB_df[["ID_link"]])
+  mapped_entrezs <- SplitEntrezs(mass_spec_AB_df[["Consensus_Entrez_ID"]][matches_vec])
+  all_MS_entrezs <- ifelse(mass_spec_AB_df[["ENTREZ ac"]] == 0,
+                           mass_spec_AB_df[["ENTREZ geneID"]],
+                           mass_spec_AB_df[["ENTREZ ac"]]
+                           )
+  original_entrezs <- setdiff(all_MS_entrezs[matches_vec], 0L)
+  results_list <- list(
+    "only_original" = setdiff(original_entrezs, mapped_entrezs),
+    "only_mapped"   = setdiff(mapped_entrezs, original_entrezs),
+    "all_mapped"    = mapped_entrezs
+  )
+  return(results_list)
+}
+
+
+
+AddTranslatedSymbols <- function(input_df, symbol_column) {
+  mapped_df <- MapToEntrezs(symbols_vec = input_df[[symbol_column]])
+  mapped_df <- mapped_df[, c("Entrez_ID", "Gene_symbol", "Original_symbol")]
+  names(mapped_df) <- c("Symbol_to_Entrez_IDs",
+                        "Backtranslated_symbol",
+                        "Original_symbol"
+                        )
+  results_df <- data.frame(
+    input_df,
+    mapped_df,
+    stringsAsFactors = FALSE,
+    check.names = FALSE
+  )
+  return(results_df)
+}
+
+
+
+AreConflictingEntrezs <- function(input_df) {
+  are_identical <- mapply(identical,
+                          input_df[["UniProt_acc_to_Entrez_IDs"]],
+                          input_df[["Symbol_to_Entrez_IDs"]]
+                          )
+  are_conflicting <- !(are_identical) &
+                     !(is.na(input_df[["UniProt_acc_to_Entrez_IDs"]])) &
+                     !(is.na(input_df[["Symbol_to_Entrez_IDs"]]))
+  return(are_conflicting)
+}
+
+
+
+
+AddConsensusEntrezs <- function(input_df) {
+  are_conflicting <- AreConflictingEntrezs(input_df)
+  uni_splits <- strsplit(input_df[["UniProt_acc_to_Entrez_IDs"]], ", ", fixed = TRUE)
+  sym_splits <- strsplit(input_df[["Symbol_to_Entrez_IDs"]], ", ", fixed = TRUE)
+
+  consensus_list <- lapply(which(are_conflicting),
+                           function(x) {
+                             if (any(uni_splits[[x]] %in% sym_splits[[x]])) {
+                               intersect(uni_splits[[x]], sym_splits[[x]])
+                             } else {
+                               uni_splits[[x]]
+                             }
+                           })
+
+  consensus_vec <- vapply(consensus_list, function(x) paste0(x, collapse = ", "), "")
+  results_vec <- input_df[["UniProt_acc_to_Entrez_IDs"]]
+  results_vec[are_conflicting] <- consensus_vec
+  are_NA <- is.na(results_vec)
+  results_vec[are_NA] <- input_df[["Symbol_to_Entrez_IDs"]][are_NA]
+
+  input_df[["Consensus_Entrez_ID"]] <- results_vec
+  return(input_df)
+}
 
 
 
@@ -200,7 +243,6 @@ MapUniProtToEntrezs <- function(mappings_df, UniProt_IDs) {
 table(mass_spec_B_df[["CSPA category"]], useNA = "ifany")
 
 stopifnot(all(mass_spec_B_df[["Organisme"]] == "Human"))
-
 
 
 
@@ -219,12 +261,12 @@ table(silico_SURFY_df[["MachineLearning FPR class (1=1%, 2=5%, 3=15%)"]], useNA 
 boxplot(silico_SURFY_df[["MachineLearning score"]] ~ silico_SURFY_df[["Surfaceome Label"]])
 
 table(silico_surfaceome_df[["Surfaceome Label Source"]])
+table(silico_surfaceome_df[["MachineLearning FPR class (1=1%, 2=5%, 3=15%)"]])
 
 table(silico_groups_df[["Almen category"]])
 table(silico_groups_df[["Almen subclass"]])
 
 hist(silico_HeLa_df[["group probability"]])
-
 
 
 
@@ -261,8 +303,10 @@ mass_spec_AB_df <- mass_spec_AB_df[, !(are_duplicated)]
 
 stopifnot(identical(1L, unique(table(mass_spec_AB_df[["ID_link"]]))))
 
-mass_spec_AB_df[["Mapped_Entrez_IDs"]] <- MapUniProtToEntrezs(mass_spec_entrezs_df, mass_spec_AB_df[["ID_link"]])
+mass_spec_AB_df[["UniProt_acc_to_Entrez_IDs"]] <- MapUniProtToEntrezs(mass_spec_entrezs_df, mass_spec_AB_df[["ID_link"]])
 
+mass_spec_AB_df <- AddTranslatedSymbols(mass_spec_AB_df, "ENTREZ gene symbol")
+mass_spec_AB_df <- AddConsensusEntrezs(mass_spec_AB_df)
 
 
 
@@ -285,12 +329,14 @@ mass_spec_AB_df[!(are_identical_CD), ]
 
 
 
-
 # Define Entrez IDs for the in silico surfaceome --------------------------
 
-silico_HeLa_df[["Mapped_Entrez_IDs"]] <- MapUniProtToEntrezs(HeLa_entrezs_df, silico_HeLa_df[["UniProt ID"]])
-silico_surfaceome_df[["Mapped_Entrez_IDs"]] <- MapUniProtToEntrezs(SURFY_acc_entrezs_df, silico_surfaceome_df[["UniProt accession"]])
-silico_surfaceome_df[["Mapped_UniProt_name_to_Entrez_IDs"]] <- MapUniProtToEntrezs(SURFY_name_entrezs_df, silico_surfaceome_df[["UniProt name"]])
+silico_HeLa_df[["UniProt_acc_to_Entrez_IDs"]]        <- MapUniProtToEntrezs(HeLa_entrezs_df, silico_HeLa_df[["UniProt ID"]])
+silico_surfaceome_df[["UniProt_acc_to_Entrez_IDs"]]  <- MapUniProtToEntrezs(SURFY_acc_entrezs_df, silico_surfaceome_df[["UniProt accession"]])
+silico_surfaceome_df[["UniProt_name_to_Entrez_IDs"]] <- MapUniProtToEntrezs(SURFY_name_entrezs_df, silico_surfaceome_df[["UniProt name"]])
+
+silico_surfaceome_df <- AddTranslatedSymbols(silico_surfaceome_df, "UniProt gene")
+silico_surfaceome_df <- AddConsensusEntrezs(silico_surfaceome_df)
 
 
 
@@ -305,10 +351,10 @@ table(mass_spec_B_df[["ENTREZ geneID"]] == 0)
 stopifnot(!(anyNA(mass_spec_B_df[["ENTREZ geneID"]])))
 table(table(mass_spec_B_df[["ENTREZ geneID"]]))
 
-table(!(is.na(mass_spec_AB_df[["Mapped_Entrez_IDs"]])))
-were_found <- mass_spec_B_df[["ID_link"]] %in% mass_spec_entrezs_df[["From"]]
-table(were_found)
-identical(were_found, !(is.na(mass_spec_AB_df[["Mapped_Entrez_IDs"]])))
+no_mapped_entrez <- is.na(mass_spec_AB_df[["UniProt_acc_to_Entrez_IDs"]])
+table(!(no_mapped_entrez))
+mass_spec_AB_df[no_mapped_entrez, ]
+mass_spec_AB_df[AreConflictingEntrezs(mass_spec_AB_df), ]
 
 
 
@@ -320,48 +366,38 @@ table(table(silico_surfaceome_df[["GeneID"]]))
 # The following Entrez IDs seem to have been rounded by Excel...
 silico_surfaceome_df[silico_surfaceome_df[["GeneID"]] %in% "100000000", ]
 silico_surfaceome_df[silico_surfaceome_df[["GeneID"]] %in% "101000000", ]
-
-name_found <- silico_surfaceome_df[["UniProt name"]] %in% SURFY_name_entrezs_df[["From"]]
-accession_found <- silico_surfaceome_df[["UniProt accession"]] %in% SURFY_acc_entrezs_df[["From"]]
-table(name_found)
-table(accession_found)
-head(silico_surfaceome_df[!(accession_found), ])
-
-identical(name_found, !(is.na(silico_surfaceome_df[["Mapped_UniProt_name_to_Entrez_IDs"]])))
-identical(accession_found, !(is.na(silico_surfaceome_df[["Mapped_Entrez_IDs"]])))
-
-table(name_found, !(is.na(silico_surfaceome_df[["Mapped_Entrez_IDs"]])))
-table(accession_found, !(is.na(silico_surfaceome_df[["Mapped_UniProt_name_to_Entrez_IDs"]])))
+silico_surfaceome_df[silico_surfaceome_df[["GeneID"]] %in% "103000000", ]
 
 
+no_mapped_entrez <- is.na(silico_surfaceome_df[["UniProt_acc_to_Entrez_IDs"]])
+table(!(no_mapped_entrez))
+head(silico_surfaceome_df[no_mapped_entrez, ])
+table(!(is.na(silico_surfaceome_df[["UniProt_name_to_Entrez_IDs"]])))
 
-HeLa_found <- silico_HeLa_df[["UniProt ID"]] %in% HeLa_entrezs_df[["From"]]
-table(HeLa_found)
-silico_HeLa_df[!(HeLa_found), ]
+silico_surfaceome_df[AreConflictingEntrezs(silico_surfaceome_df), ]
 
-identical(HeLa_found, !(is.na(silico_HeLa_df[["Mapped_Entrez_IDs"]])))
 
 
 
 # Examine Entrez IDs for the MS-based surfaceome --------------------------
 
 are_certain <- mass_spec_AB_df[["CSPA category"]] == "1 - high confidence"
-mass_spec_original_entrezs <- setdiff(mass_spec_AB_df[["ENTREZ geneID"]][are_certain], 0)
 
-mass_spec_mapped_df <- ProcessSurfaceEntrezs(mass_spec_entrezs_df,
-                                             mass_spec_AB_df[["ID_link"]][are_certain]
-                                             )
-mass_spec_mapped_entrezs <- mass_spec_mapped_df[["Entrez_ID"]][mass_spec_mapped_df[["Num_present"]] >= 1]
+mass_spec_original_entrezs <- setdiff(mass_spec_AB_df[["ENTREZ geneID"]][are_certain], 0)
+mass_spec_mapped_entrezs <- SplitEntrezs(mass_spec_AB_df[["Consensus_Entrez_ID"]][are_certain])
 
 only_original_entrezs <- setdiff(mass_spec_original_entrezs, mass_spec_mapped_entrezs)
+only_mapped_entrezs   <- setdiff(mass_spec_mapped_entrezs, mass_spec_original_entrezs)
+
 are_only_original <- mass_spec_AB_df[["ENTREZ geneID"]] %in% only_original_entrezs
-mass_spec_AB_df[mass_spec_AB_df[["ENTREZ geneID"]] %in% only_original_entrezs, ]
+mass_spec_AB_df[are_only_original, ]
 
-only_mapped_entrezs <- setdiff(mass_spec_mapped_entrezs, mass_spec_original_entrezs)
-only_mapped_IDs <- mass_spec_mapped_df[["Present_IDs"]][mass_spec_mapped_df[["Entrez_ID"]] %in% only_mapped_entrezs]
-are_only_mapped <- mass_spec_AB_df[["ID_link"]] %in% only_mapped_IDs
+
+are_only_mapped <- vapply(strsplit(mass_spec_AB_df[["Consensus_Entrez_ID"]], ", ", fixed = TRUE),
+                          function(x) any(as.integer(x) %in% only_mapped_entrezs),
+                          logical(1)
+                          )
 mass_spec_AB_df[are_only_mapped, ]
-
 
 
 
@@ -370,70 +406,58 @@ mass_spec_AB_df[are_only_mapped, ]
 
 MS_surfaceome_entrezs <- union(mass_spec_original_entrezs, mass_spec_mapped_entrezs)
 
-mass_spec_mapped_entrezs_2 <- setdiff(as.integer(unlist(strsplit(mass_spec_AB_df[["Mapped_Entrez_IDs"]][are_certain], ", ", fixed = TRUE))), NA_integer_)
-identical(sort(mass_spec_mapped_entrezs), sort(mass_spec_mapped_entrezs_2))
-
-
-
-GetMSEntrezs <- function(UniProt_IDs) {
-  mapped_entrezs <- GetSurfaceEntrezs(mass_spec_entrezs_df, UniProt_IDs)
-  all_MS_entrezs <- ifelse(mass_spec_AB_df[["ENTREZ ac"]] == 0,
-                           mass_spec_AB_df[["ENTREZ geneID"]],
-                           mass_spec_AB_df[["ENTREZ ac"]]
-                           )
-  matches_vec <- match(UniProt_IDs, mass_spec_AB_df[["ID_link"]])
-  original_entrezs <- setdiff(all_MS_entrezs[matches_vec], 0L)
-  results_list <- list(
-    "union"         = union(mapped_entrezs, original_entrezs),
-    "only_original" = setdiff(original_entrezs, mapped_entrezs),
-    "only_mapped"   = setdiff(mapped_entrezs, original_entrezs)
-  )
-  return(results_list)
-}
-
-stopifnot(identical(sort(GetMSEntrezs(mass_spec_AB_df[["ID_link"]][are_certain])[["union"]]),
-                    sort(MS_surfaceome_entrezs)
-                    ))
-
 are_HeLa  <- mass_spec_AB_df[["HeLa"]] %in% 1
 are_HEK   <- mass_spec_AB_df[["HEK"]] %in% 1
 are_LN229 <- mass_spec_AB_df[["LN229"]] %in% 1
 
+are_putative <- mass_spec_AB_df[["CSPA category"]] %in% c("1 - high confidence", "2 - putative")
+
 fisher.test(are_certain, are_HeLa)
 fisher.test(are_certain, are_HEK)
 fisher.test(are_certain, are_LN229)
-
-are_putative <- mass_spec_AB_df[["CSPA category"]] %in% c("1 - high confidence", "2 - putative")
 
 MS_HeLa_list  <- GetMSEntrezs(mass_spec_AB_df[["ID_link"]][are_putative & are_HeLa])
 MS_HEK_list   <- GetMSEntrezs(mass_spec_AB_df[["ID_link"]][are_putative & are_HEK])
 MS_LN229_list <- GetMSEntrezs(mass_spec_AB_df[["ID_link"]][are_putative & are_LN229])
 
 
-HeLa_entrezs_2 <- setdiff(as.integer(unlist(strsplit(mass_spec_AB_df[["Mapped_Entrez_IDs"]][are_putative & are_HeLa], ", ", fixed = TRUE))), NA_integer_)
-identical(sort(HeLa_entrezs_2), as.integer(sort(setdiff(MS_HeLa_list[["union"]], MS_HeLa_list[["only_original"]]))))
 
-HEK_entrezs_2 <- setdiff(as.integer(unlist(strsplit(mass_spec_AB_df[["Mapped_Entrez_IDs"]][are_putative & are_HEK], ", ", fixed = TRUE))), NA_integer_)
-identical(sort(HEK_entrezs_2), as.integer(sort(setdiff(MS_HEK_list[["union"]], MS_HEK_list[["only_original"]]))))
 
-LN229_entrezs_2 <- setdiff(as.integer(unlist(strsplit(mass_spec_AB_df[["Mapped_Entrez_IDs"]][are_putative & are_LN229], ", ", fixed = TRUE))), NA_integer_)
-identical(sort(LN229_entrezs_2), as.integer(sort(setdiff(MS_LN229_list[["union"]], MS_LN229_list[["only_original"]]))))
+# Define sets of genes for the in silico surfaceome -----------------------
+
+silico_HeLa_entrezs       <- SplitEntrezs(silico_HeLa_df[["UniProt_acc_to_Entrez_IDs"]])
+silico_surfaceome_entrezs <- SplitEntrezs(silico_surfaceome_df[["Consensus_Entrez_ID"]])
 
 
 
 
+# Define the final list of surfaceome gene sets ---------------------------
 
-# Define Entrez IDs for the in silico surfaceome --------------------------
+surfaceome_list <- list(
+  "CSC_surfaceome_2015"   = as.integer(MS_surfaceome_entrezs),
+  "SURFY_surfaceome_2018" = silico_surfaceome_entrezs,
+  "CSC_HeLa_2015"         = MS_HeLa_list[["all_mapped"]],
+  "CSC_HEK_2015"          = MS_HEK_list[["all_mapped"]],
+  "CSC_LN229_2015"        = MS_LN229_list[["all_mapped"]]
+)
 
-silico_HeLa_df[["Mapped_Entrez_IDs"]] <- MapUniProtToEntrezs(HeLa_entrezs_df, silico_HeLa_df[["UniProt ID"]])
-silico_surfaceome_df[["Mapped_Entrez_IDs"]] <- MapUniProtToEntrezs(SURFY_acc_entrezs_df, silico_surfaceome_df[["UniProt accession"]])
+all_entrezs <- sort(unique(unlist(surfaceome_list)))
 
-silico_HeLa_entrezs <- GetSurfaceEntrezs(HeLa_entrezs_df, silico_HeLa_df[["UniProt ID"]])
-silico_surfaceome_UniProt_names_to_entrezs <- GetSurfaceEntrezs(SURFY_name_entrezs_df, silico_surfaceome_df[["UniProt name"]])
-silico_surfaceome_entrezs <- GetSurfaceEntrezs(SURFY_acc_entrezs_df, silico_surfaceome_df[["UniProt accession"]])
+are_present_list <- lapply(surfaceome_list,
+                           function(x) ifelse(all_entrezs %in% x,
+                                              "Yes",
+                                              ""
+                                              )
+                           )
+surfaceome_mat <- do.call(cbind, are_present_list)
 
-silico_surfaceome_entrezs_2 <- setdiff(as.integer(unlist(strsplit(silico_surfaceome_df[["Mapped_Entrez_IDs"]], ", ", fixed = TRUE))), NA_integer_)
-identical(sort(silico_surfaceome_entrezs_2), as.integer(sort(silico_surfaceome_entrezs)))
+surfaceome_df <- data.frame(
+  "Entrez_ID"   = all_entrezs,
+  "Gene_symbol" = MapToEntrezs(all_entrezs)[["Gene_symbol"]],
+  surfaceome_mat,
+  stringsAsFactors = FALSE
+)
+
 
 
 
@@ -462,10 +486,9 @@ HPA_df[["Entrez_ID"]] <- HPA_mappings_df[["Consensus_entrez"]]
 
 
 
-
 # Save data ---------------------------------------------------------------
 
-save(list = "HPA_df",
+save(list = c("HPA_df", "surfaceome_df"),
      file = file.path(general_RData_directory, "23) Compile data on protein localization.RData")
      )
 
