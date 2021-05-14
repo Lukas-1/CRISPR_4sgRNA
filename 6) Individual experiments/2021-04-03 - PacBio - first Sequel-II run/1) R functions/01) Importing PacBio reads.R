15561496785f568f05f3decf8cf396a5bd1dbc86
Lukas_1 <lukas.frick@gmail.com>
2021-05-14 13:18:00 +0200

    Important fix

--- 6) Individual experiments/2021-04-03 - PacBio - first Sequel-II run/1) R functions/01) Importing PacBio reads.R
+++ 6) Individual experiments/2021-04-03 - PacBio - first Sequel-II run/1) R functions/01) Importing PacBio reads.R
@@ -14,9 +14,23 @@ IntegrateSingleReportDf <- function(sam_df, tidied_report_df, check_zmws = TRUE)
   if (check_zmws) {
     stopifnot(identical(sam_df[["ZMW"]], matched_report_df[["ZMW"]]))
   }
+
+  sequence_lengths <- nchar(sam_df[["Sequence"]])
+  stopifnot(identical(sequence_lengths, nchar(sam_df[["Quality"]])))
+  if ("Pre_clip_start" %in% names(matched_report_df)) {
+    start_vec <- matched_report_df[["Pre_clip_start"]]
+  } else {
+    start_vec <- 1
+  }
+  if ("Pre_clip_end" %in% names(tidied_report_df)) {
+    end_vec <- matched_report_df[["Pre_clip_end"]]
+  } else {
+    end_vec <- sequence_lengths
+  }
+
   for (column_name in c("Sequence", "Quality")) {
-    vec_1 <- substr(sam_df[[column_name]], 1, matched_report_df[["Clip_start"]] - 1)
-    vec_2 <- substr(sam_df[[column_name]], matched_report_df[["Clip_end"]] + 1, nchar(sam_df[[column_name]]))
+    vec_1 <- substr(sam_df[[column_name]], start_vec, matched_report_df[["Clip_start"]] - 1)
+    vec_2 <- substr(sam_df[[column_name]], matched_report_df[["Clip_end"]] + 1, end_vec)
     new_column_names <- paste0("Barcode_", 1:2, "_", tolower(column_name))
     sam_df[[new_column_names[[1]]]] <- vec_1
     sam_df[[new_column_names[[2]]]] <- vec_2
@@ -45,8 +59,15 @@ IntegrateReportDfs <- function(sam_df, plates_report_df, wells_report_df) {
   barcodes_to_plates_map <- plates_df[["Plate_number"]]
   names(barcodes_to_plates_map) <- plate_combos
   tidied_plates_df <- TidyReportDf(plates_report_df, barcodes_to_plates_map)
-
   tidied_wells_df <- TidyReportDf(wells_report_df, barcodes_to_wells_map)
+
+  zmw_matches <- match(tidied_wells_df[["ZMW"]], tidied_plates_df[["ZMW"]])
+
+  add_vec <- tidied_plates_df[["Clip_start"]][zmw_matches] - 1L
+  tidied_wells_df[["Clip_start"]]     <- tidied_wells_df[["Clip_start"]] + add_vec
+  tidied_wells_df[["Clip_end"]]       <- tidied_wells_df[["Clip_end"]] + add_vec
+  tidied_wells_df[["Pre_clip_start"]] <- tidied_plates_df[["Clip_start"]][zmw_matches]
+  tidied_wells_df[["Pre_clip_end"]]   <- tidied_plates_df[["Clip_end"]][zmw_matches]
 
   plate_df <- IntegrateSingleReportDf(sam_df, tidied_plates_df)
   well_df <- IntegrateSingleReportDf(sam_df, tidied_wells_df, check_zmws = FALSE)
@@ -86,9 +107,9 @@ IntegrateReportDfs <- function(sam_df, plates_report_df, wells_report_df) {
     "Passed_filters",
     "Barcode_combined_score", "Barcode_score_lead",
     "Clip_start", "Clip_end", "Barcodes_orientation_fwd",
-    "Barcode_1_length", "Barcode_2_length",
+    "Barcode_1_length",   "Barcode_2_length",
     "Barcode_1_sequence", "Barcode_2_sequence",
-    "Barcode_1_quality", "Barcode_2_quality"
+    "Barcode_1_quality",  "Barcode_2_quality"
   )
   all_columns <- c(
     "ZMW", "Combined_ID", "Plate_number", "Well_number",
@@ -100,9 +121,6 @@ IntegrateReportDfs <- function(sam_df, plates_report_df, wells_report_df) {
   all_columns <- setdiff(all_columns, "Plate_barcodes_orientation_fwd")
 
   results_df <- results_df[, all_columns]
-
-  results_df[["Well_clip_start"]] <- results_df[["Well_clip_start"]] + results_df[["Plate_clip_start"]] - 1L
-  results_df[["Well_clip_end"]]   <- results_df[["Well_clip_end"]]   + results_df[["Plate_clip_start"]] - 1L
 
   are_valid <- is.na(results_df[["Well_clip_end"]]) |
                (results_df[["Well_clip_end"]] <= results_df[["Plate_clip_end"]])
