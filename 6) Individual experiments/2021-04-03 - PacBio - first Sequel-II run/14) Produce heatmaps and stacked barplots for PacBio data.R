e255334ad69be2c1707d8585970850559d9187ca
Lukas_1 <lukas.frick@gmail.com>
2021-05-16 16:05:57 +0200

    Cosmetic changes

--- 6) Individual experiments/2021-04-03 - PacBio - first Sequel-II run/14) Produce heatmaps and stacked barplots for PacBio data.R
+++ 6) Individual experiments/2021-04-03 - PacBio - first Sequel-II run/14) Produce heatmaps and stacked barplots for PacBio data.R
@@ -31,57 +31,6 @@ load(file.path(sql2_R_objects_directory, "03) Import and process sgRNA sequences
 load(file.path(sql2_R_objects_directory, "09) Process demultiplexed PacBio reads.RData"))
 
 
-# # try ---------------------------------------------------------------------
-#
-# use_df <- ccs7_df_list[["individual_reads_df"]]
-# pass_filters <- use_df[["Passes_filters"]] == 1
-#
-# use_df <- use_df[pass_filters, ]
-#
-#
-# use_df <- use_df[use_df[["Plate_number"]] %in% 13, ]
-#
-#
-# sub_df <- use_df[use_df[["Well_number"]] %in% 2, ]
-# table(sub_df[["Contam_well"]])
-#
-# sub_df <- use_df[use_df[["Well_number"]] %in% 4, ]
-# table(sub_df[["Contam_well"]])
-#
-# sub_df <- use_df[use_df[["Well_number"]] %in% 6, ]
-# table(sub_df[["Contam_well"]])
-#
-# sub_df <- use_df[use_df[["Well_number"]] %in% 8, ]
-# table(sub_df[["Contam_well"]])
-#
-# sub_df <- use_df[use_df[["Well_number"]] %in% 10, ]
-# table(sub_df[["Contam_well"]])
-#
-# sub_df <- use_df[use_df[["Well_number"]] %in% 25, ]
-# table(sub_df[["Contam_well"]])
-#
-# sub_df <- use_df[use_df[["Well_number"]] %in% 26, ]
-#
-#
-#
-# vapply(seq(2, 382, by = 2), function(x) {
-#   are_this_well <- use_df[["Well_number"]] %in% x
-#   names(sort(table(use_df[["Contam_well"]][are_this_well]), decreasing = TRUE))[[1]]
-# }, "")
-#
-#
-# library_mat <- as.matrix(library_df[, paste0("Sequence_sg", 1:4)])
-#
-# "ACTCTGCGTTGAGAGAAACA" %in% library_mat
-#
-#
-# use_df[use_df[["ZMW"]] %in% 80020138, ]
-#
-#
-# groo_df <- extracted_df[extracted_df[["ZMW"]] %in% 80020138, ]
-#
-# groo_df[groo_df[["Feature"]] %in% paste0("sg", 1:4), ]
-#
 
 
 
@@ -91,7 +40,6 @@ ccs_numbers <- c(3, 5, 7)
 accuracy_percentages <- c(99, 99.9, 99.99)
 
 use_plate_numbers <- plates_df[["Plate_number"]][order(plates_df[["Plate_rank"]])]
-
 
 for (reorder_wells in c(TRUE, FALSE)) {
 
@@ -123,6 +71,7 @@ for (reorder_wells in c(TRUE, FALSE)) {
                           c("i) unfiltered", "ii) filtered", "iii) filtered gRNAs")[[filter_stage]]
                           )
 
+
       pdf(file = file.path(plots_output_directory, heatmaps_folder, paste0("Heatmaps - ", file_name, ".pdf")),
           width  = use_width, height = use_height
           )
@@ -136,6 +85,7 @@ for (reorder_wells in c(TRUE, FALSE)) {
       }
       dev.off()
 
+
       if (reorder_wells) {
         pdf(file = file.path(plots_output_directory, sand_charts_folder, paste0("Sand chart - ", file_name, ".pdf")),
             width = 3.8, height = 6.7
@@ -143,9 +93,7 @@ for (reorder_wells in c(TRUE, FALSE)) {
         for (plate_number in use_plate_numbers) {
           sub_df <- use_summary_df[use_summary_df[["Plate_number"]] %in% plate_number, ]
           sg_sequences_df <- library_df[library_df[["Plate_number"]] %in% plate_number, ]
-          DrawReorderedSandPlots(sub_df,
-                                 main_title = titles_list[[plate_number]]
-                                 )
+          DrawReorderedSandPlots(sub_df, main_title = titles_list[[plate_number]])
         }
         dev.off()
       }
