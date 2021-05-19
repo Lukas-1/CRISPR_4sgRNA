0ae129cbcbbca5e9e5640772812d3fb55f6d7d0b
Lukas_1 <lukas.frick@gmail.com>
2021-05-19 13:15:43 +0200

    Fixed file name, and other tidying up

--- 6) Individual experiments/2020-09-18 - PacBio - second 384-well plate/17) Examine raw subreads.R
+++ 6) Individual experiments/2020-09-18 - PacBio - second 384-well plate/17) Examine raw subreads.R
@@ -30,26 +30,36 @@ subreads_zmws_directory   <- file.path(subreads_output_directory, "Individual ZM
 
 # Load data ---------------------------------------------------------------
 
-load(file.path(p1_R_objects_directory, "01) Process and export barcodes.RData"))
 load(file.path(p2_R_objects_directory, "01) Import and process sgRNA sequences.RData"))
 load(file.path(p2_R_objects_directory, "03) Read in PacBio data.RData"))
-load(file.path(p2_R_objects_directory, "15) Import raw subreads.RData"))
+load(file.path(p2_R_objects_directory, "16) Import raw subreads.RData"))
 
 
 
 
 
-# Export subreads (before consensus calling) ------------------------------
+# Export selected subreads ------------------------------------------------
 
 ExportReadsForZMW(4588372, sl7_ccs_df)
 ExportReadsForZMW(6489079, sl7_ccs_df)
 ExportReadsForZMW(8323170, sl7_ccs_df)
 
-message("Exporting reads for SmrtLink7_CCS3...")
+
+
+
+
+
+# Export a small sample of raw subreads (before consensus calling) --------
+
+sl7_ccs5_lima_zmws <- GetCCS5_ZMWs(sl7_ccs_df, wells_vec = sg_sequences_df[["Well_number"]])
+
+message("Exporting reads for SmrtLink7_CCS5...")
 ExportSubreadsForWells(sl7_ccs_df,
                        fasta_output_dir = file.path(subreads_fasta_directory, "SmrtLink7_CCS5"),
                        fastq_output_dir = file.path(subreads_fastq_directory, "SmrtLink7_CCS5"),
-                       wells_vec = sg_sequences_df[["Well_number"]]
+                       wells_vec = sg_sequences_df[["Well_number"]],
+                       use_zmws = sl7_ccs5_lima_zmws,
+                       max_num_ZMWs = 20L
                        )
 
 
