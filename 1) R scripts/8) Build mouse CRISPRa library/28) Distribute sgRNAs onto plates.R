562e5d80643aaa012bc4dc48ce5a5067677a03f7
Lukas_1 <lukas.frick@gmail.com>
2021-05-30 13:09:20 +0200

    Adapt to mouse

--- 1) R scripts/8) Build mouse CRISPRa library/28) Distribute sgRNAs onto plates.R
+++ 1) R scripts/8) Build mouse CRISPRa library/28) Distribute sgRNAs onto plates.R
@@ -17,11 +17,11 @@ source(file.path(general_functions_directory, "20) Randomly allocating sgRNAs to
 
 # Define folder paths -----------------------------------------------------
 
-CRISPR_root_directory       <- "~/CRISPR"
-RData_directory             <- file.path(CRISPR_root_directory, "3) RData files")
-general_RData_directory     <- file.path(RData_directory, "6) Mouse - General")
-CRISPRa_RData_directory     <- file.path(RData_directory, "7) Mouse - CRISPRa")
-file_output_directory       <- file.path(CRISPR_root_directory, "5) Output", "Mouse - CRISPRa")
+CRISPR_root_directory   <- "~/CRISPR"
+RData_directory         <- file.path(CRISPR_root_directory, "3) RData files")
+general_RData_directory <- file.path(RData_directory, "6) Mouse - General")
+CRISPRa_RData_directory <- file.path(RData_directory, "7) Mouse - CRISPRa")
+file_output_directory   <- file.path(CRISPR_root_directory, "5) Output", "Mouse - CRISPRa")
 
 
 
@@ -47,59 +47,94 @@ merged_replaced_CRISPRa_df[[preferred_AF_max_column]] <- NA_real_
 
 
 
+# Assign all guides to plates ---------------------------------------------
+
+sg4_by_gene_df <- AllocateAllGuidesToPlates(merged_replaced_CRISPRa_df,
+                                            sublibraries_entrezs_list = list("All" = collected_entrez_IDs),
+                                            num_control_wells = 96,
+                                            reorder_df = FALSE
+                                            )
+sg4_by_gene_df[["Old_order"]] <- seq_len(nrow(sg4_by_gene_df))
+
+sg4_by_well_df <- ReorderPlates(sg4_by_gene_df)
+sg4_by_well_df <- RenumberPlatesContinuously(sg4_by_well_df)
+sg4_by_well_df <- AssignPlateStrings(sg4_by_well_df)
+
+sg4_by_gene_df <- sg4_by_well_df[order(sg4_by_well_df[["Old_order"]]), ]
+
+sg4_by_gene_df <- sg4_by_gene_df[, names(sg4_by_gene_df) != "Old_order"]
+sg4_by_well_df <- sg4_by_well_df[, names(sg4_by_well_df) != "Old_order"]
+
+
+
+
+
+
 # Assign membrane sgRNAs to plates ----------------------------------------
 
 are_membrane <- (merged_replaced_CRISPRa_df[["Combined_ID"]] %in% membrane_het_df[["Entrez_ID"]]) |
                 (merged_replaced_CRISPRa_df[["Is_control"]] %in% "Yes")
 membrane_CRISPRa_df <- merged_replaced_CRISPRa_df[are_membrane, ]
 
+membrane_4sg_by_gene_df <- AllocateAllGuidesToPlates(membrane_CRISPRa_df,
+                                                     list("Membrane" = membrane_het_df[["Entrez_ID"]]),
+                                                     num_control_wells = 6,
+                                                     reorder_df = FALSE
+                                                     )
 
 
 
 
-# Add the control guides to the membrane plates ---------------------------
+# Re-order the membrane sgRNAs according to the plate layout --------------
 
-membrane_4sg_df <- AllocateAllGuidesToPlates(membrane_CRISPRa_df,
-                                             list("Membrane" = membrane_het_df[["Entrez_ID"]]),
-                                             num_control_wells = 6,
-                                             reorder_df = FALSE
-                                             )
-
-
-
-
-# Re-order the data frame according to the plate layout -------------------
-
-membrane_4sg_df[["Old_order"]] <- seq_len(nrow(membrane_4sg_df))
-membrane_4sg_reordered_df <- ReorderPlates(membrane_4sg_df)
+membrane_4sg_by_gene_df[["Old_order"]] <- seq_len(nrow(membrane_4sg_by_gene_df))
+membrane_4sg_by_well_df <- ReorderPlates(membrane_4sg_by_gene_df)
 
 
 
 
 
-# Move the control guides to the last plate -------------------------------
+# Move the membrane control guides to the last plate ----------------------
 
-are_controls <- membrane_4sg_reordered_df[["Is_control"]] == "Yes"
-last_plate_number <- max(membrane_4sg_reordered_df[["Plate_number"]])
-max_well_last_plate <- max(membrane_4sg_reordered_df[["Well_number"]][(membrane_4sg_reordered_df[["Plate_number"]] == last_plate_number) & !(are_controls)])
+are_controls <- membrane_4sg_by_well_df[["Is_control"]] == "Yes"
+last_plate_number <- max(membrane_4sg_by_well_df[["Plate_number"]])
+are_non_control_last <- (membrane_4sg_by_well_df[["Plate_number"]] == last_plate_number) & !(are_controls)
+max_well_last_plate <-
+max(membrane_4sg_by_well_df[["Well_number"]][are_non_control_last])
 control_wells_seq <- rep(seq(from = max_well_last_plate + 1L,
                              to = max_well_last_plate + sum(are_controls) / 4,
                              ),
                          each = 4
                          )
-membrane_4sg_reordered_df[["Well_number"]][are_controls] <- control_wells_seq
-membrane_4sg_reordered_df[["Plate_number"]][are_controls] <- last_plate_number
-membrane_4sg_reordered_df <- AssignPlateStrings(membrane_4sg_reordered_df, use_prefix = "mmb_")
+membrane_4sg_by_well_df[["Well_number"]][are_controls] <- control_wells_seq
+membrane_4sg_by_well_df[["Plate_number"]][are_controls] <- last_plate_number
+membrane_4sg_by_well_df <- AssignPlateStrings(membrane_4sg_by_well_df, use_prefix = "mmb_")
 
 
 
 
 # Restore the original plate order ----------------------------------------
 
-membrane_4sg_df <- membrane_4sg_reordered_df[order(membrane_4sg_reordered_df[["Old_order"]]), ]
-row.names(membrane_4sg_df) <- NULL
-membrane_4sg_reordered_df <- membrane_4sg_reordered_df[, names(membrane_4sg_reordered_df) != "Old_order"]
-membrane_4sg_df <- membrane_4sg_df[, names(membrane_4sg_df) != "Old_order"]
+membrane_4sg_by_gene_df <- membrane_4sg_by_gene_df[order(membrane_4sg_by_gene_df[["Old_order"]]), ]
+row.names(membrane_4sg_by_gene_df) <- NULL
+membrane_4sg_by_well_df <- membrane_4sg_by_well_df[, names(membrane_4sg_by_well_df) != "Old_order"]
+membrane_4sg_by_gene_df <- membrane_4sg_by_gene_df[, names(membrane_4sg_by_gene_df) != "Old_order"]
+
+
+
+
+
+# Export all shared / duplicated sgRNAs -----------------------------------
+
+shared_sgRNAs_df <- SharedsgRNAsDf(sg4_by_gene_df)
+
+use_sub_folder <- "Plate layout - all genes"
+
+ExportSharedDf(shared_sgRNAs_df,
+               file.path(use_sub_folder,
+                         "Duplicated mouse CRISPRa sgRNAs (shared between genes)"
+                         )
+               )
 
 
 
@@ -107,19 +142,44 @@ membrane_4sg_df <- membrane_4sg_df[, names(membrane_4sg_df) != "Old_order"]
 
 # Export the membrane heterogeneity plate layouts -------------------------
 
-membrane_4sg_df <- membrane_4sg_df[, names(membrane_4sg_df) != "Sublibrary_4sg"]
-membrane_4sg_reordered_df <- membrane_4sg_reordered_df[, names(membrane_4sg_reordered_df) != "Sublibrary_4sg"]
+membrane_4sg_by_gene_df <- membrane_4sg_by_gene_df[, names(membrane_4sg_by_gene_df) != "Sublibrary_4sg"]
+membrane_4sg_by_well_df <- membrane_4sg_by_well_df[, names(membrane_4sg_by_well_df) != "Sublibrary_4sg"]
 
 export_columns <- setdiff(export_columns, preferred_AF_max_column)
 
-ExportPlates(membrane_4sg_df, "Membrane_4sg_original_order", sub_folder = "Plate layout - membrane")
-ExportPlates(membrane_4sg_reordered_df, "Membrane_4sg_reordered", sub_folder = "Plate layout - membrane")
+ExportPlates(membrane_4sg_by_gene_df, "Membrane_4sg_by_gene", sub_folder = "Plate layout - membrane")
+ExportPlates(membrane_4sg_by_well_df, "Membrane_4sg_by_well", sub_folder = "Plate layout - membrane")
 
 for (i in 1:4) {
-  use_df <- membrane_4sg_reordered_df[membrane_4sg_reordered_df[["Rank"]] %in% i, ]
+  use_df <- membrane_4sg_by_gene_df[membrane_4sg_by_gene_df[["Rank"]] %in% i, ]
   ExportPlates(use_df,
-               paste0("Membrane_4sg_reordered_sg", i),
+               paste0("PD_sg", i),
                sub_folder = "Plate layout - membrane",
+               add_padding_between_plates = TRUE
+               )
+}
+
+
+
+
+# Export the whole-genome plate layouts -----------------------------------
+
+ExportPlates(sg4_by_gene_df,
+             "All_sublibraries_ordered_by_gene",
+             use_sub_folder,
+             add_primers = FALSE
+             )
+ExportPlates(sg4_by_well_df,
+             "All_sublibraries_ordered_by_well",
+             use_sub_folder,
+             add_primers = FALSE
+             )
+
+for (i in 1:4) {
+  use_df <- sg4_by_well_df[sg4_by_well_df[["Rank"]] %in% i, ]
+  ExportPlates(use_df,
+               paste0("4sg_ordered_by_well_sg", i),
+               sub_folder = use_sub_folder,
                add_padding_between_plates = TRUE
                )
 }
@@ -129,16 +189,18 @@ for (i in 1:4) {
 
 # Remove the empty SNP column ---------------------------------------------
 
-membrane_4sg_df <- membrane_4sg_df[, names(membrane_4sg_df) != preferred_AF_max_column]
-membrane_4sg_reordered_df <- membrane_4sg_reordered_df[, names(membrane_4sg_reordered_df) != preferred_AF_max_column]
+membrane_4sg_by_gene_df <- membrane_4sg_by_gene_df[, names(membrane_4sg_by_gene_df) != preferred_AF_max_column]
+membrane_4sg_by_well_df <- membrane_4sg_by_well_df[, names(membrane_4sg_by_well_df) != preferred_AF_max_column]
 
 
 
 
 # Save data ---------------------------------------------------------------
 
-save(list = c("membrane_4sg_df", "membrane_4sg_reordered_df"),
-     file = file.path(CRISPRa_RData_directory, "27) Distribute sgRNAs for the whole genome onto plates.RData")
+save(list = c("sg4_by_gene_df", "sg4_by_well_df",
+              "membrane_4sg_by_well_df", "membrane_4sg_by_gene_df"
+              ),
+     file = file.path(CRISPRa_RData_directory, "28) Distribute sgRNAs for the whole genome onto plates.RData")
      )
 
 
