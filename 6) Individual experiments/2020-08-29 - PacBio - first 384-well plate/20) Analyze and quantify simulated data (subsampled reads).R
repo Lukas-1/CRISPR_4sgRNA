6e7e74c66fe7ee846c50bc23392bd541d35c393f
Lukas_1 <lukas.frick@gmail.com>
2021-06-01 12:31:53 +0200

    Cosmetic change

--- 6) Individual experiments/2020-08-29 - PacBio - first 384-well plate/20) Analyze and quantify simulated data (subsampled reads).R
+++ 6) Individual experiments/2020-08-29 - PacBio - first 384-well plate/20) Analyze and quantify simulated data (subsampled reads).R
@@ -3,9 +3,9 @@
 
 # Import packages and source code -----------------------------------------
 
-CRISPR_root_directory  <- "~/CRISPR"
-file_directory         <- file.path(CRISPR_root_directory, "6) Individual experiments/2020-08-29 - PacBio - first 384-well plate")
-R_functions_directory  <- file.path(file_directory, "1) R functions")
+CRISPR_root_directory <- "~/CRISPR"
+file_directory        <- file.path(CRISPR_root_directory, "6) Individual experiments/2020-08-29 - PacBio - first 384-well plate")
+R_functions_directory <- file.path(file_directory, "1) R functions")
 
 source(file.path(R_functions_directory, "14) Drawing sigmoid curves (acceptable wells vs. percentage sampled).R"))
 
@@ -13,7 +13,6 @@ source(file.path(R_functions_directory, "14) Drawing sigmoid curves (acceptable 
 
 # Define folder paths -----------------------------------------------------
 
-file_input_directory   <- file.path(file_directory, "2) Input")
 R_objects_directory    <- file.path(file_directory, "3) R objects")
 file_output_directory  <- file.path(file_directory, "5) Output")
 plots_output_directory <- file.path(file_output_directory, "Figures")
