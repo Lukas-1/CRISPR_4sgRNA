1976c9208639bd81b9a934a78911eb29a7e83e48
Lukas_1 <lukas.frick@gmail.com>
2021-06-06 09:05:39 +0200

    Increase the granularity of subsampling fractions

--- 6) Individual experiments/2021-04-03 - PacBio - first Sequel-II run/11) Process demultiplexed reads - with subsampling.R
+++ 6) Individual experiments/2021-04-03 - PacBio - first Sequel-II run/11) Process demultiplexed reads - with subsampling.R
@@ -44,7 +44,7 @@ manhattan_dist_list <- MakeDistanceList(manhattan_distance = TRUE)
 
 # Process reads, with subsampling -----------------------------------------
 
-sampling_fractions <- c(1, 0.5, 0.4, 0.3, 0.25, 0.2, 0.1, 0.05, 0.01)
+sampling_fractions <- c(1, 0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05, 0.01)
 
 subsampled_list <- ProcessWithSubsampling(ccs_df,
                                           barcodes_df,
