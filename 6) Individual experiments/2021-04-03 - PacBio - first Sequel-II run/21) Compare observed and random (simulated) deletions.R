afd019893a599de0e1a24f568c4fbe3562023e31
Lukas_1 <lukas.frick@gmail.com>
2021-06-08 12:51:14 +0200

    Remove unnecessary code

--- 6) Individual experiments/2021-04-03 - PacBio - first Sequel-II run/21) Compare observed and random (simulated) deletions.R
+++ 6) Individual experiments/2021-04-03 - PacBio - first Sequel-II run/21) Compare observed and random (simulated) deletions.R
@@ -7,19 +7,12 @@
 
 library("RColorBrewer")
 
-CRISPR_root_directory <- "~/CRISPR"
-plate1_directory      <- file.path(CRISPR_root_directory, "6) Individual experiments/2020-08-29 - PacBio - first 384-well plate")
-R_functions_directory <- file.path(plate1_directory, "1) R functions")
-
-source(file.path(R_functions_directory, "07) Categorizing subsequences of reads aligned to the reference.R"))
-source(file.path(R_functions_directory, "17) Characterizing deletions.R"))
-
-
 
 
 
 # Define folder paths -----------------------------------------------------
 
+CRISPR_root_directory    <- "~/CRISPR"
 sql2_directory           <- file.path(CRISPR_root_directory, "6) Individual experiments/2021-04-03 - PacBio - first Sequel-II run")
 sql2_R_objects_directory <- file.path(sql2_directory, "3) R objects")
 file_output_directory    <- file.path(sql2_directory, "5) Output", "Figures", "Compare simulated and observed deletions")
