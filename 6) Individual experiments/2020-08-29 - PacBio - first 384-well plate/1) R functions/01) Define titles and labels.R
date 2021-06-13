fc3af4902bd574fb9d49c486217e7e71bb61d2c7
Lukas_1 <lukas.frick@gmail.com>
2021-06-13 14:01:54 +0200

    Fix spelling mistake (tracRNA ==> tracrRNA)

--- 6) Individual experiments/2020-08-29 - PacBio - first 384-well plate/1) R functions/01) Define titles and labels.R
+++ 6) Individual experiments/2020-08-29 - PacBio - first 384-well plate/1) R functions/01) Define titles and labels.R
@@ -6,31 +6,31 @@
 
 titles_list <- list(
 
-  "Num_contaminated_reads" = "Percentage of reads that match gRNAs from other wells (with 100% accuracy)",
+  "Num_contaminated_reads"        = "Percentage of reads that match gRNAs from other wells (with 100% accuracy)",
 
-  "Num_under_2kb"          = "Percentage of truncated reads (shorter than 2000 base pairs)",
+  "Num_under_2kb"                 = "Percentage of truncated reads (shorter than 2000 base pairs)",
 
-  "Num_low_barcode_scores" = "Percentage of reads with low barcode scores",
-  "Num_low_quality_scores" = "Percentage of reads with low average read quality scores",
-  "Num_low_bc_or_qual"     = "Percentage of low-quality reads (barcode or read quality below the threshold)",
-  "Mean_read_quality"      = "Mean read quality scores",
+  "Num_low_barcode_scores"        = "Percentage of reads with low barcode scores",
+  "Num_low_quality_scores"        = "Percentage of reads with low average read quality scores",
+  "Num_low_bc_or_qual"            = "Percentage of low-quality reads (barcode or read quality below the threshold)",
+  "Mean_read_quality"             = "Mean read quality scores",
 
-  "Longest_subsequence"    = "Length of the longest subsequence shared between any two gRNAs in that well",
+  "Longest_subsequence"           = "Length of the longest subsequence shared between any two gRNAs in that well",
 
-  "Count_sg1_cr1"          = "Percentage of reads where sg1 (and its tracRNA) are 100% correct",
-  "Count_sg2_cr2"          = "Percentage of reads where sg2 (and its tracRNA) are 100% correct",
-  "Count_sg3_cr3"          = "Percentage of reads where sg3 (and its tracRNA) are 100% correct",
-  "Count_sg4_cr4"          = "Percentage of reads where sg4 (and its tracRNA) are 100% correct",
+  "Count_sg1_cr1"                 = "Percentage of reads where sg1 (and its tracrRNA) are 100% correct",
+  "Count_sg2_cr2"                 = "Percentage of reads where sg2 (and its tracrRNA) are 100% correct",
+  "Count_sg3_cr3"                 = "Percentage of reads where sg3 (and its tracrRNA) are 100% correct",
+  "Count_sg4_cr4"                 = "Percentage of reads where sg4 (and its tracrRNA) are 100% correct",
 
-  "Count_mean_sg1to4"      = "Percentage of individual gRNAs (+ tracRNA) that are 100% correct (mean of sg1-4)",
+  "Count_mean_sg1to4"             = "Percentage of individual gRNAs (+ tracrRNA) that are 100% correct (mean of sg1-4)",
 
-  "Count_at_least_1"       = "Percentage of reads with at least 1 gRNA that is 100% correct",
-  "Count_at_least_2"       = "Percentage of reads with at least 2 gRNAs that are 100% correct",
-  "Count_at_least_3"       = "Percentage of reads with at least 3 gRNAs that are 100% correct",
-  "Count_all_4"            = "Percentage of reads for which all 4 gRNAs are 100% correct",
+  "Count_at_least_1"              = "Percentage of reads with at least 1 gRNA that is 100% correct",
+  "Count_at_least_2"              = "Percentage of reads with at least 2 gRNAs that are 100% correct",
+  "Count_at_least_3"              = "Percentage of reads with at least 3 gRNAs that are 100% correct",
+  "Count_all_4"                   = "Percentage of reads for which all 4 gRNAs are 100% correct",
 
-  "Count_all_4_promoters"  = "Percentage of reads for which all 4 gRNAs (including the promoters) are 100% correct",
-  "Count_whole_plasmid"    = "Percentage of reads where the entire plasmid sequence is 100% correct",
+  "Count_all_4_promoters"         = "Percentage of reads for which all 4 gRNAs (including the promoters) are 100% correct",
+  "Count_whole_plasmid"           = "Percentage of reads where the entire plasmid sequence is 100% correct",
 
   "Binary_num_contaminated_reads" = "20% or more of the reads match gRNAs from other wells (with 100% accuracy)",
 
@@ -45,10 +45,10 @@ titles_list <- list(
   "Binary_count_all_4_promoters"  = "In the majority of reads, all 4 gRNAs (including the promoters) are 100% correct",
   "Binary_count_whole_plasmid"    = "In the majority of reads, the entire plasmid sequence is 100% correct",
 
-  "Binary_count_sg1_cr1"          = "In the majority of reads, sg1 (and its tracRNA) are 100% correct",
-  "Binary_count_sg2_cr2"          = "In the majority of reads, sg2 (and its tracRNA) are 100% correct",
-  "Binary_count_sg3_cr3"          = "In the majority of reads, sg3 (and its tracRNA) are 100% correct",
-  "Binary_count_sg4_cr4"          = "In the majority of reads, sg4 (and its tracRNA) are 100% correct"
+  "Binary_count_sg1_cr1"          = "In the majority of reads, sg1 (and its tracrRNA) are 100% correct",
+  "Binary_count_sg2_cr2"          = "In the majority of reads, sg2 (and its tracrRNA) are 100% correct",
+  "Binary_count_sg3_cr3"          = "In the majority of reads, sg3 (and its tracrRNA) are 100% correct",
+  "Binary_count_sg4_cr4"          = "In the majority of reads, sg4 (and its tracrRNA) are 100% correct"
 
 
 )
