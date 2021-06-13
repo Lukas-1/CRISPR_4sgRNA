095e38fbe2f3bed67e0a42ec18bacf1c8a62c5aa
Lukas_1 <lukas.frick@gmail.com>
2021-06-13 18:30:05 +0200

    First commit

---
+++ 6) Individual experiments/2021-04-03 - PacBio - first Sequel-II run/1) R functions/03) Summarizing data across wells.R
@@ -0,0 +1,31 @@
+### 13th June 2021 ###
+
+
+
+
+
+# Define functions --------------------------------------------------------
+
+DrawGridlines <- function(y_limits, extra_grid_lines = TRUE) {
+  y_range <- y_limits[[2]] - y_limits[[1]]
+  divide_by <- 20
+  grid_seq <- seq(from = y_limits[[1]], to = y_limits[[2]], by = y_range / divide_by)
+  are_in_between <- rep(c(TRUE, FALSE), divide_by)[seq_along(grid_seq)]
+  segments(x0   = par("usr")[[1]],
+           x1   = par("usr")[[2]],
+           y0   = grid_seq[!(are_in_between)],
+           col  = "gray88",
+           lend = "butt",
+           xpd  = NA
+           )
+  if (extra_grid_lines) {
+    segments(x0   = par("usr")[[1]],
+             x1   = par("usr")[[2]],
+             y0   = grid_seq[are_in_between],
+             col  = "gray95",
+             lend = "butt",
+             xpd  = NA
+             )
+  }
+  return(invisible(NULL))
+}
