0a3743b23137d8ce92cff72cc3892878cdbe1520
Lukas_1 <lukas.frick@gmail.com>
2021-06-14 08:05:00 +0200

    Bugfix

--- 6) Individual experiments/2021-04-03 - PacBio - first Sequel-II run/1) R functions/03) Summarizing data across wells.R
+++ 6) Individual experiments/2021-04-03 - PacBio - first Sequel-II run/1) R functions/03) Summarizing data across wells.R
@@ -10,10 +10,10 @@ DrawGridlines <- function(y_limits, extra_grid_lines = TRUE) {
   y_range <- y_limits[[2]] - y_limits[[1]]
   divide_by <- 20
   grid_seq <- seq(from = y_limits[[1]], to = y_limits[[2]], by = y_range / divide_by)
-  are_in_between <- rep(c(TRUE, FALSE), divide_by)[seq_along(grid_seq)]
+  are_main <- rep(c(TRUE, FALSE), divide_by)[seq_along(grid_seq)]
   segments(x0   = par("usr")[[1]],
            x1   = par("usr")[[2]],
-           y0   = grid_seq[!(are_in_between)],
+           y0   = grid_seq[are_main],
            col  = "gray88",
            lend = "butt",
            xpd  = NA
@@ -21,7 +21,7 @@ DrawGridlines <- function(y_limits, extra_grid_lines = TRUE) {
   if (extra_grid_lines) {
     segments(x0   = par("usr")[[1]],
              x1   = par("usr")[[2]],
-             y0   = grid_seq[are_in_between],
+             y0   = grid_seq[!(are_main)],
              col  = "gray95",
              lend = "butt",
              xpd  = NA
