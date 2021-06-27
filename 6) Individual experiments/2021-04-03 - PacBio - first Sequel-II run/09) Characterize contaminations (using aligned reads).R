7bdb74fa1a6a9f09a93b2a320bc3a7769fbecaf9
Lukas_1 <lukas.frick@gmail.com>
2021-06-27 11:01:50 +0200

    Re-organized code

---
+++ 6) Individual experiments/2021-04-03 - PacBio - first Sequel-II run/09) Characterize contaminations (using aligned reads).R
@@ -0,0 +1,54 @@
+### 14th June 2021 ###
+
+
+
+
+# Import packages and source code -----------------------------------------
+
+CRISPR_root_directory <- "~/CRISPR"
+plate1_directory      <- file.path(CRISPR_root_directory, "6) Individual experiments/2020-08-29 - PacBio - first 384-well plate")
+R_functions_directory <- file.path(plate1_directory, "1) R functions")
+
+source(file.path(R_functions_directory, "17) Characterizing contaminations (using aligned reads).R"))
+
+
+
+
+
+# Define folder paths -----------------------------------------------------
+
+sql2_directory           <- file.path(CRISPR_root_directory, "6) Individual experiments/2021-04-03 - PacBio - first Sequel-II run")
+sql2_R_objects_directory <- file.path(sql2_directory, "3) R objects")
+output_directory         <- file.path(sql2_directory, "5) Output", "Tables", "Aligned contaminations")
+
+
+
+
+
+# Load data ---------------------------------------------------------------
+
+load(file.path(sql2_R_objects_directory, "04) Create reference sequences for each well - sg_sequences_df.RData"))
+load(file.path(sql2_R_objects_directory, "08) Categorize subsequences of reads aligned to the reference.RData"))
+
+
+
+
+
+# Characterize contaminations (using aligned reads) -----------------------
+
+contam_df <- CharacterizeContaminations(extracted_df, sg_sequences_df)
+
+
+
+
+
+# Save data ---------------------------------------------------------------
+
+save(list = "contam_df",
+     file = file.path(sql2_R_objects_directory, "09) Characterize contaminations (using aligned reads).RData")
+     )
+
+
+
+
+
