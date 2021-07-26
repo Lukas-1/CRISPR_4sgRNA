af2e9b32f1237e871636fc377d85a8b99597461f
Lukas_1 <lukas.frick@gmail.com>
2021-07-26 19:14:41 +0200

    Re-wrote the function to make it much more efficient

--- 6) Individual experiments/2020-08-29 - PacBio - first 384-well plate/1) R functions/19) Annotating duplicated gRNAs.R
+++ 6) Individual experiments/2020-08-29 - PacBio - first 384-well plate/1) R functions/19) Annotating duplicated gRNAs.R
@@ -5,14 +5,13 @@
 # Define functions --------------------------------------------------------
 
 AddNumOccurrences <- function(sg_df) {
-
   all_sequences <- unlist(sg_df[, paste0("Sequence_sg", 1:4)])
-  num_occurrences_mat <- do.call(cbind,
-                                 lapply(sg_df[, paste0("Sequence_sg", 1:4)],
-                                        function(x) vapply(x, function(y) sum(y == all_sequences), integer(1), USE.NAMES = FALSE)
-                                 ))
+  all_sequences_table <- table(all_sequences)
+  num_occurrences_mat <- apply(sg_df[, paste0("Sequence_sg", 1:4)], 2, function(x) {
+    matches_vec <- match(x, names(all_sequences_table))
+    as.integer(all_sequences_table[matches_vec])
+  })
   colnames(num_occurrences_mat) <- paste0("Num_occurrences_sg", 1:4)
-
   results_df <- data.frame(sg_df, num_occurrences_mat, stringsAsFactors = FALSE)
   return(results_df)
 }
