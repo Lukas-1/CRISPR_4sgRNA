bf7261164ae32a899104c1e9dde9026807419477
Lukas_1 <lukas.frick@gmail.com>
2021-09-10 15:15:48 +0200

    Correct column names

--- 6) Individual experiments/2021-08-18 - find non-essential hit genes/02) Add data on the fitness effect with control treatment.R
+++ 6) Individual experiments/2021-08-18 - find non-essential hit genes/02) Add data on the fitness effect with control treatment.R
@@ -56,13 +56,13 @@ baseline_df[["Pass_threshold"]] <- (baseline_df[, "log2 Ratio"] < -1) &
 genes_fac <- factor(baseline_df[["Gene_symbol"]])
 
 baseline_genes_df <- data.frame(
-  "Gene_symbol"         = levels(genes_fac),
-  "Mean_FC_essential"   = tapply(baseline_df[["log2 Ratio"]], genes_fac, mean),
-  "Num_essential_gRNAs" = tapply(baseline_df[["Pass_threshold"]], genes_fac, sum),
-  "Min_FC_essential"    = tapply(baseline_df[["log2 Ratio"]], genes_fac, min),
-  "Min_FDR_essential"   = tapply(baseline_df[["fdr"]], genes_fac, min),
-  stringsAsFactors      = FALSE,
-  row.names             = NULL
+  "Gene_symbol"           = levels(genes_fac),
+  "Mean_log2FC_essential" = tapply(baseline_df[["log2 Ratio"]], genes_fac, mean),
+  "Num_essential_gRNAs"   = tapply(baseline_df[["Pass_threshold"]], genes_fac, sum),
+  "Min_log2FC_essential"  = tapply(baseline_df[["log2 Ratio"]], genes_fac, min),
+  "Min_FDR_essential"     = tapply(baseline_df[["fdr"]], genes_fac, min),
+  stringsAsFactors        = FALSE,
+  row.names               = NULL
 )
 
 matches_vec <- match(combined_df[, "Mouse_symbol"],
@@ -87,15 +87,15 @@ combined_df <- combined_df[, column_indices]
 # Prepare for export ------------------------------------------------------
 
 export_df <- combined_df
-NA_empty_columns <- c("CRISPR_mean_probability",   "CRISPR_num_essential",   "CRISPR_num_cell_lines",
-                      "CRISPR_mean_effect",
+NA_empty_columns <- c("CRISPR_mean_probability", "CRISPR_num_essential",
+                      "CRISPR_num_cell_lines", "CRISPR_mean_effect",
                       "Achilles_mean_probability", "Achilles_num_essential", "Achilles_num_cell_lines",
                       "DEMETER2_mean_probability", "DEMETER2_num_essential", "DEMETER2_num_cell_lines",
                       "Human_Entrez_ID", "Human_symbol",
                       "Mouse_symbol", "Mouse_Entrez_ID", "Mapping",
                       "Expressed_in_GT17",
-                      "Num_essential_gRNAs", "Mean_FC_essential",
-                      "Min_FC_essential", "Min_FDR_essential"
+                      "Num_essential_gRNAs", "Mean_log2FC_essential",
+                      "Min_log2FC_essential", "Min_FDR_essential"
                       )
 for (column_name in NA_empty_columns) {
   export_df[, column_name] <- ifelse(is.na(export_df[, column_name]),
