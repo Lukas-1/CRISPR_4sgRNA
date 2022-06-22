83c8ee6dae23debdc3dabf75cce0099d16d29c4a
Lukas_1 <lukas.frick@gmail.com>
2022-06-22 23:02:09 +0200

    Moved functions to an external script

--- 6) Individual experiments/2022-04-21 - Illumina paired-end 2sg - first trial/01_R_scripts/06_assign_sgRNAs_to_plasmids.R
+++ 6) Individual experiments/2022-04-21 - Illumina paired-end 2sg - first trial/01_R_scripts/06_assign_sgRNAs_to_plasmids.R
@@ -1,19 +1,19 @@
 ### 2022-04-09
 
 
-
 # Load packages and source code -------------------------------------------
 
 CRISPR_root_directory <- "~/CRISPR"
 experiments_directory <- file.path(CRISPR_root_directory, "6) Individual experiments")
-first_nanopore_dir <- file.path(experiments_directory, "2022-01-05 - first nanopore sequencing run")
+first_nanopore_dir    <- file.path(experiments_directory, "2022-01-05 - first nanopore sequencing run")
+project_dir           <- file.path(experiments_directory, "2022-04-21 - Illumina paired-end 2sg - first trial")
+source(file.path(project_dir, "01_R_scripts", "R_functions", "04_obtaining_counts_for_2sg_plasmids.R"))
 source(file.path(first_nanopore_dir, "01_R_scripts", "1_R_functions", "06_assigning_sgRNAs_to_plasmids.R"))
 
 
 
 # Define paths ------------------------------------------------------------
 
-project_dir  <- file.path(experiments_directory, "2022-04-21 - Illumina paired-end 2sg - first trial")
 rdata_dir <- file.path(project_dir, "03_R_objects")
 
 
@@ -24,66 +24,6 @@ load(file.path(rdata_dir, "03_disambiguate_CRISPRoff_library.RData"))
 load(file.path(rdata_dir, "04_look_up_sgRNAs_run1.RData"))
 load(file.path(rdata_dir, "04_look_up_sgRNAs_run2_chunk1.RData"))
 load(file.path(rdata_dir, "04_look_up_sgRNAs_run2_chunk2.RData"))
-
-
-
-# Define functions --------------------------------------------------------
-
-GetCounts2sg <- function(mapped_df,
-                         only_0MM = FALSE,
-                         no_template_switch = FALSE,
-                         no_template_switch_requires_both_sgRNAs = TRUE,
-                         choose_sample = NULL
-                         ) {
-
-  stopifnot("sg_sequences_df" %in% ls(envir = globalenv()))
-
-  are_selected <- rep(TRUE, nrow(mapped_df))
-
-  if (only_0MM) {
-    have_0MM <- (mapped_df[, "Num_MM_sg1"] %in% 0) &
-                (mapped_df[, "Num_MM_sg2"] %in% 0)
-    are_selected <- have_0MM
-  }
-  if (no_template_switch) {
-    have_no_switch <- (!(mapped_df[, "Has_template_switch"]))
-    if (no_template_switch_requires_both_sgRNAs) {
-      have_no_switch <- have_no_switch & (mapped_df[, "Num_matched_sgRNAs"] == 2L)
-    }
-    are_selected <- are_selected & have_no_switch
-  }
-  if (!(is.null(choose_sample))) {
-    are_this_sample <- mapped_df[, "Sample_number"] == choose_sample
-    are_selected <- are_selected & are_this_sample
-  }
-
-  sel_mapped_df <- mapped_df[are_selected, c("Plasmid_sg1", "Plasmid_sg2")]
-  row.names(sel_mapped_df) <- NULL
-  counts_vec <- GetCounts(sg_sequences_df[, "Plasmid_ID"], sel_mapped_df,
-                          sg_numbers = 1:2
-                          )
-  return(counts_vec)
-}
-
-
-AllSamplesCounts <- function(mapped_df, only_0MM = FALSE, no_template_switch = FALSE) {
-
-  sample_numbers <- unique(mapped_df[, "Sample_number"])
-  sample_names <- mapped_df[, "Sample_name"][match(sample_numbers, mapped_df[, "Sample_number"])]
-  short_names <- sub("before_off", "before", sample_names, fixed = TRUE)
-  short_names <- sub("2sg_", "", short_names, fixed = TRUE)
-
-  sample_counts_list <- lapply(seq_along(sample_numbers), function(x) {
-    message(paste0("Calculating counts for the sample: ", short_names[[x]], "..."))
-    GetCounts2sg(mapped_df, choose_sample = sample_numbers[[x]],
-                 only_0MM = only_0MM, no_template_switch = no_template_switch
-                 )
-  })
-  results_mat <- do.call(cbind, sample_counts_list)
-  colnames(results_mat) <- short_names
-  rownames(results_mat) <- NULL
-  return(results_mat)
-}
 
 
 
---
+++ 6) Individual experiments/2022-04-21 - Illumina paired-end 2sg - first trial/01_R_scripts/R_functions/04_obtaining_counts_for_2sg_plasmids.R
@@ -0,0 +1,65 @@
+### 2022-04-09
+
+
+
+# Define functions --------------------------------------------------------
+
+GetCounts2sg <- function(mapped_df,
+                         only_0MM = FALSE,
+                         no_template_switch = FALSE,
+                         no_template_switch_requires_both_sgRNAs = TRUE,
+                         choose_sample = NULL
+                         ) {
+
+  stopifnot("sg_sequences_df" %in% ls(envir = globalenv()))
+
+  are_selected <- rep(TRUE, nrow(mapped_df))
+
+  if (only_0MM) {
+    have_0MM <- (mapped_df[, "Num_MM_sg1"] %in% 0) &
+                (mapped_df[, "Num_MM_sg2"] %in% 0)
+    are_selected <- have_0MM
+  }
+  if (no_template_switch) {
+    have_no_switch <- (!(mapped_df[, "Has_template_switch"]))
+    if (no_template_switch_requires_both_sgRNAs) {
+      have_no_switch <- have_no_switch & (mapped_df[, "Num_matched_sgRNAs"] == 2L)
+    }
+    are_selected <- are_selected & have_no_switch
+  }
+  if (!(is.null(choose_sample))) {
+    are_this_sample <- mapped_df[, "Sample_number"] == choose_sample
+    are_selected <- are_selected & are_this_sample
+  }
+
+  sel_mapped_df <- mapped_df[are_selected, c("Plasmid_sg1", "Plasmid_sg2")]
+  row.names(sel_mapped_df) <- NULL
+  counts_vec <- GetCounts(sg_sequences_df[, "Plasmid_ID"], sel_mapped_df,
+                          sg_numbers = 1:2
+                          )
+  return(counts_vec)
+}
+
+
+
+AllSamplesCounts <- function(mapped_df, only_0MM = FALSE, no_template_switch = FALSE) {
+
+  sample_numbers <- unique(mapped_df[, "Sample_number"])
+  sample_names <- mapped_df[, "Sample_name"][match(sample_numbers, mapped_df[, "Sample_number"])]
+  short_names <- sub("before_off", "before", sample_names, fixed = TRUE)
+  short_names <- sub("2sg_", "", short_names, fixed = TRUE)
+
+  sample_counts_list <- lapply(seq_along(sample_numbers), function(x) {
+    message(paste0("Calculating counts for the sample: ", short_names[[x]], "..."))
+    GetCounts2sg(mapped_df, choose_sample = sample_numbers[[x]],
+                 only_0MM = only_0MM, no_template_switch = no_template_switch
+                 )
+  })
+  results_mat <- do.call(cbind, sample_counts_list)
+  colnames(results_mat) <- short_names
+  rownames(results_mat) <- NULL
+  return(results_mat)
+}
+
+
+
