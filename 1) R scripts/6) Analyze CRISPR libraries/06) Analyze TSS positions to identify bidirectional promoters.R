6d3ec243d53de61ef01b088b2069107efed92538
Lukas_1 <lukas.frick@gmail.com>
2022-06-22 14:30:57 +0200

    First commit

---
+++ 1) R scripts/6) Analyze CRISPR libraries/06) Analyze TSS positions to identify bidirectional promoters.R
@@ -0,0 +1,52 @@
+### 9th March 2021 ###
+
+
+
+
+# Import packages and source code -----------------------------------------
+
+general_functions_directory <- "~/CRISPR/1) R scripts/1) R functions"
+source(file.path(general_functions_directory, "30) Finding overlapping genes and nearby TSSs.R"))
+
+
+
+
+
+# Define folder paths -----------------------------------------------------
+
+CRISPR_root_directory   <- "~/CRISPR"
+RData_directory         <- file.path(CRISPR_root_directory, "3) RData files")
+general_RData_directory <- file.path(RData_directory, "1) General")
+CRISPRa_RData_directory <- file.path(RData_directory, "2) CRISPRa")
+
+
+
+
+# Load data ---------------------------------------------------------------
+
+load(file.path(general_RData_directory, "20) Compile all relevant TSSs for each gene.RData"))
+load(file.path(CRISPRa_RData_directory, "20) Create a gene-based summary of the human genome.RData"))
+
+
+
+
+
+
+# Identify bidirectional promoters ----------------------------------------
+
+all_genes_TSS_df <- PrepareTSSDf(all_TSS_df)
+
+CRISPRa_genes <- sgRNAs_overview_df[["Entrez_ID"]][sgRNAs_overview_df[["In_4sg_library"]] %in% "Yes"]
+CRISPRa_genes_TSS_df <- all_genes_TSS_df[all_genes_TSS_df[["Entrez_ID"]] %in% CRISPRa_genes, ]
+row.names(CRISPRa_genes_TSS_df) <- NULL
+
+
+
+
+
+
+
+
+
+
+
---
+++ 6) Individual experiments/2022-06-21 - Illumina paired-end 2sg - correct reference/01_R_scripts/02_annotate_dJR072_CRISPRoff_library.R
@@ -0,0 +1,86 @@
+## 2022-06-21
+
+
+# Load packages and source code -------------------------------------------
+
+CRISPR_root_directory <- "~/CRISPR"
+general_functions_directory <- file.path(CRISPR_root_directory, "1) R scripts", "1) R functions")
+source(file.path(general_functions_directory, "23) Translating between Ensembl IDs, gene symbols and Entrez IDs.R"))
+
+
+
+# Define folder paths -----------------------------------------------------
+
+experiments_directory <- file.path(CRISPR_root_directory, "6) Individual experiments")
+project_dir  <- file.path(experiments_directory, "2022-06-21 - Illumina paired-end 2sg - correct reference")
+rdata_dir    <- file.path(project_dir, "03_R_objects")
+input_dir    <- file.path(project_dir, "02_input_data")
+library_path <- file.path(input_dir, "20200513_library_1_2_unbalanced.csv")
+
+
+
+
+# Read in the CRISPR-off library ------------------------------------------
+
+CRISPRoff_df <- read.csv(library_path, stringsAsFactors = FALSE)[, -1]
+
+
+
+
+# Process CRISPRoff 2sg library genes -------------------------------------
+
+symbols_vec <- sapply(strsplit(CRISPRoff_df[, "sgID_AB"], "_", fixed = TRUE), "[[", 1)
+all_entrezs_df <- SymbolToEntrezDf(symbols_vec)
+
+CRISPRoff_df[, "Entrez_IDs"] <- all_entrezs_df[, "Entrez_ID"]
+CRISPRoff_df[, "Gene_symbols"] <- all_entrezs_df[, "Gene_symbol"]
+CRISPRoff_df[, "Combined_ID"] <- ifelse(is.na(CRISPRoff_df[, "Entrez_IDs"]),
+                                        CRISPRoff_df[, "gene"],
+                                        CRISPRoff_df[, "Entrez_IDs"]
+                                        )
+
+
+
+
+# Process plasmid IDs -----------------------------------------------------
+
+are_NT <- CRISPRoff_df[, "gene"] == "negative_control"
+NT_IDs <- paste0("NT_", gsub("non-targeting_", "", CRISPRoff_df[, "sgID_AB"][are_NT], fixed = TRUE))
+
+num_occurrences <- table(symbols_vec)[symbols_vec]
+
+plasmid_IDs_vec <- ifelse(are_NT,
+                          NT_IDs,
+                          ifelse(num_occurrences == 1,
+                                 symbols_vec,
+                                 paste0(symbols_vec, "_", CRISPRoff_df[, "transcript"])
+                                 )
+                          )
+
+num_plasmid_occurrences <- table(plasmid_IDs_vec)[plasmid_IDs_vec]
+plasmid_IDs_vec <- ifelse(num_plasmid_occurrences == 2,
+                          CRISPRoff_df[, "gene"],
+                          plasmid_IDs_vec
+                          )
+
+stopifnot(!(any(duplicated(plasmid_IDs_vec))))
+
+CRISPRoff_df[, "Plasmid_ID"] <- gsub(",", "/", plasmid_IDs_vec, fixed = TRUE)
+
+
+
+
+# Explore the annotated CRISPRoff library ---------------------------------
+
+CRISPRoff_df[!(are_NT) & is.na(CRISPRoff_df[, "Entrez_IDs"]), ]
+CRISPRoff_df[grepl("," , CRISPRoff_df[, "Entrez_IDs"], fixed = TRUE), ]
+
+
+
+
+# Save data ---------------------------------------------------------------
+
+save(list = "CRISPRoff_df",
+     file = file.path(rdata_dir, "02_annotate_dJR072_CRISPRoff_library.RData")
+     )
+
