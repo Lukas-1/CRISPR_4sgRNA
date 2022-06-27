6ae9d58a854453124eded6d94dbacb3177397b74
Lukas_1 <lukas.frick@gmail.com>
2022-06-27 10:20:16 +0200

    Delete commented-out text

--- 1) R scripts/3) Build CRISPRa library/15) Separate sgRNAs for genes with multiple relevant TSSs.R
+++ 1) R scripts/3) Build CRISPRa library/15) Separate sgRNAs for genes with multiple relevant TSSs.R
@@ -33,9 +33,6 @@ load(file.path(CRISPRa_RData_directory, "14) Find overlaps between sgRNA sequenc
 
 # Find combinations of non-overlapping sgRNAs -----------------------------
 
-# CRISPR_sub_df <- merged_replaced_CRISPRa_df[merged_replaced_CRISPRa_df[["Gene_symbol"]] %in% "TRIM74", ]
-# SeparateByTSS(CRISPR_sub_df)[, c("Source", "Gene_symbol", "Entrez_ID", "hCRISPRa_v2_transcript", "hCRISPRa_v2_ID", "TSS_number", "Allocated_TSS")]
-
 merged_replaced_CRISPRa_df <- AllocateTSSforAllGenes(merged_replaced_CRISPRa_df,
                                                      parallel_mode = TRUE,
                                                      tolerate_divergent_chromosomes = TRUE
@@ -50,32 +47,6 @@ merged_replaced_CRISPRa_df <- AllocateTSSforAllGenes(merged_replaced_CRISPRa_df,
 save(list = "merged_replaced_CRISPRa_df",
      file = file.path(CRISPRa_RData_directory, "15) Separate sgRNAs for genes with multiple relevant TSSs.RData")
      )
-
-
-
-# try_CRISPR_sub_df <- merged_replaced_CRISPRa_df[merged_replaced_CRISPRa_df[["Combined_ID"]] %in% "100288966", ]
-#
-#
-# merged_replaced_CRISPRa_df[is.na(merged_replaced_CRISPRa_df[["TSS_number"]]), use_columns]
-#
-# table(is.na(merged_replaced_CRISPRa_df[["TSS_number"]]))
-#
-# table(is.na(merged_replaced_CRISPRa_df[["Allocated_TSS"]]))
-#
-#
-# use_columns <- c("Source", "Gene_symbol", "Entrez_ID", "hCRISPRa_v2_transcript",
-#                  "sgRNA_sequence", "PAM", "Chromosome", "Entrez_chromosome",
-#                  "Best_TSS", "Start", #"Locations_0MM",
-#                  "TSS_number", "Allocated_TSS", "Num_TSSs",
-#                  "TSS_ID", "AltTSS_ID"
-#                  )
-#
-# merged_replaced_CRISPRa_df[merged_replaced_CRISPRa_df[["Gene_symbol"]] %in% "GOLGA6L9", use_columns]
-
-
-
-
-
 
 
 
