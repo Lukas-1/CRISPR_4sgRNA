a53468f2512a01a328e84fe4c32b7d6b54de084a
Lukas_1 <lukas.frick@gmail.com>
2022-10-29 12:38:09 +0200

    First commit

---
+++ 1) R scripts/4) Build CRISPRko library/29) Create 4sg plasmid sequences.R
@@ -0,0 +1,40 @@
+### 29th January 2022 ###
+
+
+
+# Import packages and source code -----------------------------------------
+
+general_functions_directory <- "~/CRISPR/1) R scripts/1) R functions"
+source(file.path(general_functions_directory, "27) Creating 4sg plasmid sequences.R"))
+
+
+
+# Define folder paths -----------------------------------------------------
+
+CRISPR_root_directory    <- "~/CRISPR"
+RData_directory          <- file.path(CRISPR_root_directory, "3) RData files")
+CRISPRko_RData_directory <- file.path(RData_directory, "3) CRISPRko")
+file_output_directory    <- file.path(CRISPR_root_directory, "5) Output", "CRISPRko")
+plasmid_output_directory <- file.path(file_output_directory, "Plasmids")
+
+
+
+# Load data ---------------------------------------------------------------
+
+load(file.path(CRISPRko_RData_directory, "20) Distribute sgRNAs for the whole genome onto plates.RData"))
+
+
+
+
+# Export vectors ----------------------------------------------------------
+
+export_genes <- c(
+  "PRNP", "IKBKG", "Control_33", "Control_64"
+)
+
+for (gene_symbol in export_genes) {
+  ExportVectorsForGene(gene_symbol, full_4sg_by_gene_df)
+}
+
+
+
