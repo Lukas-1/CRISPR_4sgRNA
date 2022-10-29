6c96f25d872631c00c2327c1650068c513bdba5d
Lukas_1 <lukas.frick@gmail.com>
2022-10-29 12:52:25 +0200

    Count the number of reads per sample, and fix a read that was included twice

--- 6) Individual experiments/2022-04-21 - Illumina paired-end 2sg - first trial/01_R_scripts/01_read_in_data.R
+++ 6) Individual experiments/2022-04-21 - Illumina paired-end 2sg - first trial/01_R_scripts/01_read_in_data.R
@@ -5,18 +5,23 @@
 
 CRISPR_root_directory <- "~/CRISPR"
 experiments_directory <- file.path(CRISPR_root_directory, "6) Individual experiments")
-
-project_dir <- file.path(experiments_directory, "2022-04-21 - Illumina paired-end 2sg - first trial")
-first_nanopore_dir <- file.path(experiments_directory, "2022-01-05 - first nanopore sequencing run")
+first_nanopore_dir    <- file.path(experiments_directory, "2022-01-05 - first nanopore sequencing run")
+project_dir           <- file.path(experiments_directory, "2022-04-21 - Illumina paired-end 2sg - first trial")
+R_functions_dir       <- file.path(project_dir, "01_R_scripts", "R_functions")
 source(file.path(first_nanopore_dir, "01_R_scripts", "1_R_functions", "03_extracting_aligned_sgRNAs.R"))
+source(file.path(R_functions_dir, "06_read_level_QC.R"))
 
 
 
 # Define folder paths -----------------------------------------------------
 
-input_dir <- file.path(project_dir, "02_input_data")
-rdata_dir <- file.path(project_dir, "03_R_objects")
-reads_dir <- file.path(input_dir, "Raw reads")
+input_dir   <- file.path(project_dir, "02_input_data")
+rdata_dir   <- file.path(project_dir, "03_R_objects")
+reads_dir   <- file.path(input_dir, "Raw reads")
+
+
+
+# Read in FASTQ files -----------------------------------------------------
 
 all_reads_list_run1 <- sapply(list.files(path.expand(file.path(reads_dir, "20220408")), full.names = TRUE),
                               function(x) ShortRead::readFastq(x),
@@ -28,13 +33,9 @@ all_reads_list_run2 <- sapply(list.files(path.expand(file.path(reads_dir, "20220
                               )
 
 
-
-
 # Define functions --------------------------------------------------------
 
-MakeFastqDf <- function(all_reads_list) {
-
-  ## Prepare sample names
+ProcessSamples <- function(all_reads_list) {
   path_list <- strsplit(names(all_reads_list), "/", fixed = TRUE)
   file_names <- vapply(path_list, function(x) x[[length(x)]], "")
   file_splits <- strsplit(file_names, "_", fixed = TRUE)
@@ -44,18 +45,32 @@ MakeFastqDf <- function(all_reads_list) {
   sample_name_splits <- strsplit(sample_names, "-", fixed = TRUE)
   sample_numbers <- as.integer(sapply(sample_name_splits, "[[", 1))
   sample_names <- sapply(sample_name_splits, "[[", 2)
+  results_df <- data.frame(
+    "Sample_name"    = sample_names,
+    "Sample_number"  = sample_numbers,
+    "Num_reads"      = vapply(all_reads_list, length, integer(1)),
+    stringsAsFactors = FALSE,
+    row.names        = NULL
+  )
+  return(results_df)
+}
 
-  read_lengths <- vapply(all_reads_list, length, integer(1))
+
+MakeFastqDf <- function(all_reads_list) {
+
+  ## Prepare sample names
+  names_df <- ProcessSamples(all_reads_list)
   indices_list <- split(seq_along(all_reads_list),
-                        factor(read_lengths, levels = unique(read_lengths))
+                        factor(names_df[, "Num_reads"],
+                               levels = unique(names_df[, "Num_reads"])
+                               )
                         )
-  print(sample_names)
 
   ## Assemble the data frame
   df_list <- lapply(indices_list, function(x) {
     data.frame(
-      "Sample_number"  = sample_numbers[[x[[1]]]],
-      "Sample_name"    = sample_names[[x[[1]]]],
+      "Sample_number"  = names_df[, "Sample_number"][[x[[1]]]],
+      "Sample_name"    = names_df[, "Sample_name"][[x[[1]]]],
       "Sequence_sg1"   = as.character(ShortRead::sread(all_reads_list[[x[[1]]]])),
       "Sequence_sg2"   = as.character(ShortRead::sread(reverseComplement(all_reads_list[[x[[2]]]]))),
       "Quality_sg1"    = as.character(as(Biostrings::quality(all_reads_list[[x[[1]]]]), "PhredQuality")),
@@ -68,7 +83,6 @@ MakeFastqDf <- function(all_reads_list) {
   fastq_df[, "Mean_quality_sg1"] <- GetMeanQuality(fastq_df[, "Quality_sg1"])
   fastq_df[, "Mean_quality_sg2"] <- GetMeanQuality(fastq_df[, "Quality_sg2"])
 
-
   fastq_df[, "Sequence_sg1"] <- substr(fastq_df[, "Sequence_sg1"], 1, 19)
   fastq_df[, "Sequence_sg2"] <- substr(fastq_df[, "Sequence_sg2"],
                                        nchar(fastq_df[, "Sequence_sg2"]) - 18,
@@ -79,9 +93,24 @@ MakeFastqDf <- function(all_reads_list) {
                                       nchar(fastq_df[, "Quality_sg2"]) - 18,
                                       nchar(fastq_df[, "Quality_sg2"])
                                       )
-
   return(fastq_df)
 }
+
+
+
+# Count the number of reads per sample ------------------------------------
+
+run1_samples_df <- unique(ProcessSamples(all_reads_list_run1), MARGIN = 1)
+run2_samples_df <- unique(ProcessSamples(all_reads_list_run2), MARGIN = 1)
+
+samples_df <- data.frame(
+  run1_samples_df[, 1:2],
+  "Num_reads_run1" = run1_samples_df[, "Num_reads"],
+  "Num_reads_run2" = run2_samples_df[, "Num_reads"],
+  stringsAsFactors = FALSE
+)
+samples_df[, "Num_reads"] <- rowSums(samples_df[, c("Num_reads_run1", "Num_reads_run2")])
+
 
 
 
@@ -90,17 +119,27 @@ MakeFastqDf <- function(all_reads_list) {
 run1_fastq_df <- MakeFastqDf(all_reads_list_run1)
 run2_fastq_df <- MakeFastqDf(all_reads_list_run2)
 
+
+
+
+# Split fastq data frames -------------------------------------------------
+
 run2_chunk1_fastq_df <- run2_fastq_df[1:45000000, ]
-run2_chunk2_fastq_df <- run2_fastq_df[45000000:nrow(run2_fastq_df), ]
+run2_chunk2_fastq_df <- run2_fastq_df[45000001:nrow(run2_fastq_df), ]
 row.names(run2_chunk2_fastq_df) <- NULL
+
 
 
 
 # Save data ---------------------------------------------------------------
 
+save(samples_df, file = file.path(rdata_dir, "01_read_in_data_num_reads.RData"))
 save(run1_fastq_df, file = file.path(rdata_dir, "01_read_in_data_run1.RData"))
 save(run2_chunk1_fastq_df, file = file.path(rdata_dir, "01_read_in_data_run2_chunk1.RData"))
 save(run2_chunk2_fastq_df, file = file.path(rdata_dir, "01_read_in_data_run2_chunk2.RData"))
+
+
+
 
 
 
