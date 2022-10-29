c82777dfbff09e456e1e9a5b7efbd8c6d66c184d
Lukas_1 <lukas.frick@gmail.com>
2022-10-29 13:07:57 +0200

    Export selected plots as .emf files

--- 6) Individual experiments/2020-08-29 - PacBio - first 384-well plate/16) Display metrics in a schematic of a 384-well plate.R
+++ 6) Individual experiments/2020-08-29 - PacBio - first 384-well plate/16) Display metrics in a schematic of a 384-well plate.R
@@ -46,3 +46,51 @@ BarPlotPanel(sl7_ccs3_df_list[["original_summary_df"]],
 
 
 
+
+
+
+
+# Export selected graphics as .emf files ----------------------------------
+
+library("devEMF")
+manuscript_directory <- file.path(plots_output_directory, "For the manuscript")
+
+summary_df <- sl7_ccs3_df_list[["filtered_summary_df"]]
+
+
+
+emf(file.path(manuscript_directory, "Thesis", "Count_at_least_2.emf"),
+    width = 7.5, height = 6, emfPlus = FALSE
+    )
+WellLayoutBarplot(summary_df,
+                  "Count_at_least_2",
+                  sg_sequences_df,
+                  label_rows_with_letters = TRUE
+                  )
+CenterText("Percentage of reads with at least 2 sgRNAs that are entirely correct",
+           y = par("usr")[[4]] + diff(grconvertY(c(0, 1.3), from = "lines", to = "user")),
+           use_cex = par("cex") * 1.15
+           )
+dev.off()
+
+
+
+emf(file.path(manuscript_directory, "Thesis", "Num_contaminated_reads.emf"),
+    width = 7.5, height = 6, emfPlus = FALSE
+    )
+WellLayoutBarplot(summary_df,
+                  "Num_contaminated_reads",
+                  sg_sequences_df,
+                  label_rows_with_letters = TRUE
+                  )
+CenterText("Percentage of reads with contaminations (sgRNAs from other wells)",
+           y = par("usr")[[4]] + diff(grconvertY(c(0, 1.3), from = "lines", to = "user")),
+           use_cex = par("cex") * 1.15
+           )
+dev.off()
+
+
+
+
+
+
