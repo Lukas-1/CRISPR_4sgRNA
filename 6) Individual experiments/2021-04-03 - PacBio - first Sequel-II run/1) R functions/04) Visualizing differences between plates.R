af1e88de753fbf16d42bb5a3829971e131925906
Lukas_1 <lukas.frick@gmail.com>
2022-12-26 14:53:20 +0100

    Add ridgeline plots

--- 6) Individual experiments/2021-04-03 - PacBio - first Sequel-II run/1) R functions/04) Visualizing differences between plates.R
+++ 6) Individual experiments/2021-04-03 - PacBio - first Sequel-II run/1) R functions/04) Visualizing differences between plates.R
@@ -12,7 +12,7 @@ library("RColorBrewer")
 
 
 
-# Define functions --------------------------------------------------------
+# Functions for drawing swarm plots ---------------------------------------
 
 ComparePlates <- function(summary_df,
                           show_column,
@@ -266,4 +266,300 @@ DrawAllPlateComparisons <- function(export_PNGs      = TRUE,
 
 
 
+# Functions for drawing ridgeline plots -----------------------------------
+
+MakeDensityPolygons <- function(x_vec, y_vec) {
+  are_empty <- y_vec == 0
+  group_lengths <- rle(are_empty)[["lengths"]]
+  rle_vec <- rep(seq_along(group_lengths), group_lengths)
+  polygon_df <- data.frame(
+    "x"        = x_vec,
+    "y"        = y_vec,
+    "Is_empty" = are_empty,
+    "Group"    = rle_vec,
+    stringsAsFactors = FALSE
+  )
+  polygon_df <- polygon_df[!(are_empty), ]
+  row.names(polygon_df) <- NULL
+  polygon_df[, "Group"] <- match(polygon_df[, "Group"], unique(polygon_df[, "Group"]))
+  split_df_list <- split(polygon_df, polygon_df[, "Group"])
+  mat_list <- lapply(split_df_list, function(x) {
+    cbind(x = c(x[, "x"][[1]], x[, "x"], x[, "x"][[nrow(x)]]),
+          y = c(0, x[, "y"], 0)
+          )
+  })
+  return(mat_list)
+}
+
+
+
+PlotDensityList <- function(density_list,
+                            use_colors,
+                            y_positions   = rev(seq_along(density_list)),
+                            min_y         = 0,
+                            ridge_height  = 3,
+                            border_colors = "white",
+                            use_alpha     = 0.8
+                            ) {
+  fill_colors <- adjustcolor(use_colors, alpha.f = use_alpha)
+  x_list <- lapply(density_list, "[[", "x")
+  y_list <- lapply(density_list, "[[", "y")
+  y_list <- lapply(y_list, function(x) (x - min(x)) / max(x - min(x)))
+  original_y_list <- y_list
+  if (min_y > 0) {
+    y_list <- lapply(y_list, function(x) ifelse(x >= min_y, x, 0))
+  }
+  y_list <- lapply(y_list, "*", ridge_height)
+  polygon_mat_list_list <- mapply(MakeDensityPolygons, x_list, y_list, SIMPLIFY = FALSE)
+  y_list <- Map("+", y_list, y_positions)
+  polygon_mat_list_list <- lapply(seq_along(density_list), function(x) {
+    mat_list <- polygon_mat_list_list[[x]]
+    mat_list <- lapply(mat_list, function(y) {
+      y[, "y"] <- y[, "y"] + y_positions[[x]]
+      return(y)
+    })
+    return(mat_list)
+  })
+  fills_vec <- rep(fill_colors, length.out = length(density_list))
+  borders_vec <- rep(border_colors, length.out = length(density_list))
+  lapply(seq_along(density_list), function(x) {
+    for (polygon_mat in polygon_mat_list_list[[x]]) {
+      polygon(x      = polygon_mat[, "x"],
+              y      = polygon_mat[, "y"],
+              col    = fills_vec[[x]],
+              border = NA,
+              xpd    = NA
+              )
+    }
+    lines(x      = x_list[[x]],
+          y      = ifelse(original_y_list[[x]] > 0.05, y_list[[x]], NA),
+          col    = borders_vec[[x]],
+          xpd    = NA
+          )
+
+  })
+  return(invisible(NULL))
+}
+
+
+
+PlateRidgelines <- function(summary_df,
+                            num_slots       = NULL,
+                            fill_colors     = c("#452e84", "#baace2"),
+                            border_colors   = c("#ddd5f0", "#795dc6"),
+                            show_title      = NULL,
+                            embed_PNG       = FALSE,
+                            PNG_lwd         = 1,
+                            ridge_height    = 3,
+                            zebra_pattern   = FALSE,
+                            zebra_palify    = 0.4,
+                            plate_prefix    = FALSE,
+                            label_on_right  = FALSE,
+                            plate_label_cex = 0.4,
+                            grid_lwd        = 1,
+                            reorder_plates  = FALSE,
+                            for_powerpoint  = FALSE,
+                            ...
+                            ) {
+
+  stopifnot("plates_df" %in% ls(envir = globalenv()))
+
+  ## Prepare plate labels and re-order plates
+  control_plates <- plates_df[plates_df[, "Colony_picked"], "Plate_number"]
+  summary_df <- summary_df[!(summary_df[, "Plate_number"]) %in% control_plates, ]
+
+  internal_plate_numbers <- unique(summary_df[, "Plate_number"])
+  matches_vec <- match(internal_plate_numbers, plates_df[, "Plate_number"])
+  plate_names <- plates_df[, "Plate_name"][matches_vec]
+  plate_names <- sub("-beads", "", plate_names, fixed = TRUE)
+  plate_names <- sub("plus", "+", plate_names, fixed = TRUE)
+  plate_names <- sub("and", "&", plate_names, fixed = TRUE)
+  first_plates <- sapply(strsplit(plate_names, " ", fixed = TRUE), "[[", 1)
+  plate_name_numbers <- as.integer(gsub("[^0-9]", "", first_plates))
+  new_order <- order(plate_name_numbers)
+  plate_names <- plate_names[new_order]
+  internal_plate_numbers <- internal_plate_numbers[new_order]
+  if (!(plate_prefix)) {
+    plate_names <- paste0("Plate ", gsub("H[AO]_", "", plate_names))
+    plate_names[plate_names == "Plate 5 & 53"] <- "Plates 5 & 53"
+  }
+  if (for_powerpoint) {
+    plate_names[plate_names == "Plate 5+"] <- "Plate 05"
+    plate_names <- gsub(" ", "_", plate_names, fixed = TRUE)
+    plate_list <- strsplit(plate_names, "_", fixed = TRUE)
+    plate_names <- vapply(plate_list, function(x) paste0(rev(x), collapse = "_"), "")
+  }
+
+  if (reorder_plates) {
+    plates_fac <- factor(summary_df[, "Plate_number"], levels = unique(summary_df[, "Plate_number"]))
+    mean_percentages <- tapply(summary_df[, "Count_all_4"] / summary_df[, "Count_total"],
+                               plates_fac,
+                               mean,
+                               na.rm = TRUE
+                               )
+    mean_percentages <- sort(mean_percentages, decreasing = TRUE)
+    new_order <- order(match(summary_df[, "Plate_number"], names(mean_percentages)))
+    plate_names <- plate_names[order(match(internal_plate_numbers, names(mean_percentages)))]
+  } else {
+    new_order <- order(match(summary_df[, "Plate_number"], internal_plate_numbers))
+  }
+
+  summary_df <- summary_df[new_order, ]
+  plates_fac <- factor(summary_df[, "Plate_number"], levels = unique(summary_df[, "Plate_number"]))
+
+  ## Compute densities
+  DensitiesForColumn <- function(use_column, omit_NA = TRUE) {
+    integer_vec <- summary_df[, use_column]
+    if (omit_NA) {
+      fraction_vec <- (integer_vec / summary_df[, "Count_total"])
+      are_NA <- is.na(fraction_vec)
+      fraction_vec <- fraction_vec[!(are_NA)]
+      use_fac <- plates_fac[!(are_NA)]
+    } else {
+      use_fac <- plates_fac
+      are_NA <- is.na(integer_vec)
+      integer_vec[are_NA] <- 0
+      fraction_vec <- integer_vec / summary_df[, "Count_total"]
+      fraction_vec[is.nan(fraction_vec)] <- 0
+    }
+    tapply(fraction_vec, use_fac, density, from = 0, to = 1)
+  }
+
+  # summary_df[, "Count_fewer_than_two"] <- summary_df[, "Count_total"] - summary_df[, "Count_at_least_2"]
+  # less_two_density_list <- DensitiesForColumn("Count_fewer_than_two", omit_NA = TRUE)
+  # deletion_density_list <- DensitiesForColumn("Num_reads_with_sgRNA_deletion", omit_NA = TRUE)
+  # atleast3_density_list <- DensitiesForColumn("Count_at_least_3", omit_NA = FALSE)
+
+  all4_density_list     <- DensitiesForColumn("Count_all_4", omit_NA = FALSE)
+  atleast1_density_list <- DensitiesForColumn("Count_at_least_1", omit_NA = FALSE)
+
+  ## Prepare x and y axes
+  if (is.null(num_slots)) {
+    num_slots <- nlevels(plates_fac)
+  }
+  y_positions <- rev(seq_len(nlevels(plates_fac))) + (num_slots - nlevels(plates_fac))
+  bottom_gap <- 1
+  y_top <- num_slots + 2
+  y_limits <- c(1 - bottom_gap, num_slots + ridge_height + 0.5)
+
+  ## Set up the plot region
+  if (embed_PNG) {
+    current_device <- StartEmbedPNG(plots_output_directory, add_padding = TRUE,
+                                    use_cairo = TRUE, png_res = 600
+                                    )
+  }
+
+  plot.new()
+  plot.window(xlim = c(0, 1), ylim = y_limits, xaxs = "i", yaxs = "i")
+
+  segments(x0  = seq(0, 1, by = 0.1),
+           y0  = par("usr")[[3]],
+           y1  = y_top,
+           col = "gray80",
+           lwd = par("lwd") * grid_lwd,
+           xpd = NA
+           )
+  segments(x0  = seq(0.05, 0.95, by = 0.1),
+           y0  = par("usr")[[3]],
+           y1  = y_top,
+           col = "gray90",,
+           lwd = par("lwd") * grid_lwd,
+           xpd = NA
+           )
+
+  ## Draw densities
+  PlotDensityList(all4_density_list,
+                  if (zebra_pattern) c(Palify(fill_colors[[1]], 0.2), fill_colors[[1]]) else fill_colors[[1]],
+                  border_colors[[1]],
+                  min_y        = 0.02,
+                  y_positions  = y_positions,
+                  ridge_height = ridge_height,
+                  ...
+                  )
+  PlotDensityList(atleast1_density_list,
+                  if (zebra_pattern) c(Palify(fill_colors[[2]], zebra_palify), fill_colors[[2]]) else fill_colors[[2]],
+                  border_colors[[2]],
+                  min_y        = 0.04,
+                  y_positions  = y_positions,
+                  ridge_height = ridge_height,
+                  ...
+                  )
+
+
+  if (embed_PNG) {
+    current_device <- StopEmbedPNG(current_device, plots_output_directory, add_padding = TRUE)
+  }
+
+  ## Annotate plot
+  mtext(plate_names,
+        side = if (label_on_right) 4 else 2,
+        line = if (label_on_right) 0.5 else 0.3,
+        at   = y_positions,
+        cex  = par("cex") * plate_label_cex,
+        col  = if (zebra_pattern) c("gray67", "gray50") else "gray50",
+        las  = 2,
+        padj = 0
+        )
+
+  x_ticks <- seq(0, 1, by = 0.1)
+  axis(1, at = x_ticks, labels = x_ticks * 100, tcl = -0.325, mgp = c(3, 0.35, 0),
+       gap.axis = 0.25, lwd = par("lwd")
+       )
+  mtext("Percentage of reads", side = 1, line = 1.5, cex = par("cex"))
+
+
+  ## Draw legend
+  original_text_vec <- c(expression(scriptstyle(" ") * "4 correct gRNAs"), expression("" >= "1 correct gRNA"))
+  text_vec <- sapply(original_text_vec, VerticalAdjust)
+  pch1_pos <- 0
+  y_pos <- par("usr")[[3]] - diff(grconvertY(c(0, 3.7), from = "lines", to = "user"))
+  point_y_pos <- y_pos + ((par("cxy")[2] / pi) * par("cex") * 0.2)
+  point_radius <- (par("cxy")[1] / pi) * par("cex")
+  pch_gap <- point_radius * 1.7
+  text1_pos <- pch1_pos + pch_gap
+  pch2_pos <- text1_pos + strwidth(original_text_vec[[1]]) + diff(grconvertX(c(0, 1.25), from = "char", to = "user"))
+  text2_pos <- pch2_pos + pch_gap
+  x_span <- text2_pos + strwidth(original_text_vec[[2]])
+  min_x <- grconvertX(0.5, from = "npc", to = "user") - (x_span / 2)
+  max_x <- grconvertX(0.5, from = "npc", to = "user") + (x_span / 2)
+  points(x   = rescale(pch1_pos, from = c(0, x_span), to = c(min_x, max_x)),
+         y   = point_y_pos,
+         pch = 22,
+         col = if (sum(col2rgb(border_colors[[1]])) < sum(col2rgb(fill_colors[[1]]))) border_colors[[1]] else "black",
+         bg  = fill_colors[[1]],
+         cex = 1.4,
+         xpd = NA
+         )
+  text(x      = rescale(text1_pos - strwidth("gh"), from = c(0, x_span), to = c(min_x, max_x)),
+       y      = y_pos,
+       labels = text_vec[[1]],
+       adj    = c(0, 0.5),
+       xpd    = NA
+       )
+  points(x   = rescale(pch2_pos, from = c(0, x_span), to = c(min_x, max_x)),
+         y   = point_y_pos,
+         pch = 22,
+         col = if (sum(col2rgb(border_colors[[2]])) < sum(col2rgb(fill_colors[[2]]))) border_colors[[2]] else "black",
+         bg  = fill_colors[[2]],
+         cex = 1.4,
+         xpd = NA
+         )
+  text(x      = rescale(text2_pos - strwidth("gh"), from = c(0, x_span), to = c(min_x, max_x)),
+       y      = y_pos,
+       labels = text_vec[[2]],
+       adj    = c(0, 0.5),
+       xpd    = NA
+       )
+
+  if (!(is.null(show_title))) {
+    text(x      = grconvertX(0.5, from = "npc", to = "user"),
+         y      = y_top + diff(grconvertY(c(0, 0.7), from = "lines", to = "user")),
+         labels = show_title,
+         adj    = c(0.5, 0),
+         xpd    = NA
+         )
+  }
+
+  return(invisible(NULL))
+}
 
--- 6) Individual experiments/2021-12-08 - integrate PacBio data/15) Visualize differences between plates.R
+++ 6) Individual experiments/2021-12-08 - integrate PacBio data/15) Visualize differences between plates.R
@@ -4,6 +4,7 @@
 
 # Import packages and source code -----------------------------------------
 
+library("svglite")
 CRISPR_root_directory      <- "~/CRISPR"
 experiments_directory      <- file.path(CRISPR_root_directory, "6) Individual experiments")
 plate1_directory           <- file.path(experiments_directory, "2020-08-29 - PacBio - first 384-well plate")
@@ -12,8 +13,11 @@ p1_R_functions_directory   <- file.path(plate1_directory, "1) R functions")
 s2r1_R_functions_directory <- file.path(s2r1_directory, "1) R functions")
 
 source(file.path(p1_R_functions_directory, "01) Define titles and labels.R"))
+source(file.path(p1_R_functions_directory, "09) Producing heatmaps.R")) # For VerticalAdjust
 source(file.path(s2r1_R_functions_directory, "04) Visualizing differences between plates.R"))
-
+source(file.path(experiments_directory, "2022-04-21 - Illumina paired-end 2sg - first trial",
+                 "01_R_scripts", "R_functions", "01_violin_swarm_plots.R" # For StartEmbedPNG and StopEmbedPNG
+                 ))
 
 
 
@@ -24,7 +28,7 @@ s2rI_R_objects_directory <- file.path(s2rI_directory, "3) R objects")
 file_output_directory    <- file.path(s2rI_directory, "5) Output")
 plots_output_directory   <- file.path(file_output_directory, "Figures", "Compare plates")
 PNGs_output_directory    <- file.path(file_output_directory, "PNGs", "Compare plates")
-
+thesis_plots_directory   <- file.path(file_output_directory, "Figures", "Manuscript", "Thesis")
 
 
 
@@ -54,7 +58,107 @@ ComparePlates(ccs7_df_list[["filtered_summary_df"]], "Count_total",
 
 
 
-# Export graphics ---------------------------------------------------------
+# Draw ridgeline plots ----------------------------------------------------
+
+use_summary_df <- ccs3_df_list[["original_summary_df"]]
+
+CRISPRa_plate_numbers  <- plates_df[grepl("^HA", plates_df[, "Plate_name"]), "Plate_number"]
+CRISPRko_plate_numbers <- plates_df[grepl("^HO", plates_df[, "Plate_name"]), "Plate_number"]
+
+CRISPRa_summary_df  <- use_summary_df[use_summary_df[, "Plate_number"] %in% CRISPRa_plate_numbers, ]
+CRISPRko_summary_df <- use_summary_df[use_summary_df[, "Plate_number"] %in% CRISPRko_plate_numbers, ]
+
+
+
+PlateRidgelines(CRISPRa_summary_df,
+                fill_colors   = c("#a83e76", "#f49aca"),
+                border_colors = c("#621e42", "#892a5c"),
+                show_title    = "T.gonfio library",
+                use_alpha     = 0.75,
+                zebra_pattern = TRUE,
+                zebra_palify  = 0.3
+                )
+
+PlateRidgelines(CRISPRko_summary_df,
+                fill_colors   = c("#225faa", "#9ac2f4"),
+                border_colors = c("#113055", "#1d5395"),
+                num_slots     = length(CRISPRa_plate_numbers),
+                show_title    = "T.spiezzo library",
+                use_alpha     = 0.75,
+                zebra_pattern = TRUE
+                )
+
+
+PlateRidgelines(CRISPRa_summary_df,
+                fill_colors    = c("#dd3c92", "#f49aca"),
+                border_colors  = c("#f7d4e7", "#f8edf2"),
+                show_title     = "T.gonfio library",
+                use_alpha      = 0.75,
+                zebra_pattern  = TRUE,
+                zebra_palify   = 0.3,
+                reorder_plates = TRUE
+                )
+
+PlateRidgelines(CRISPRko_summary_df,
+                fill_colors   = c("#225faa", "#8cbaf2"),
+                border_colors = c("#d4e4f7", "#d1e3fa"),
+                num_slots     = length(CRISPRa_plate_numbers),
+                show_title    = "T.spiezzo library",
+                use_alpha     = 0.75,
+                zebra_pattern = TRUE,
+                reorder_plates = TRUE
+                )
+
+
+
+svg_width <- 2.7
+svg_height <- 5
+use_mai <- c(0.8, 0.3, 0.3, 0.4)
+
+svg_path <- file.path(thesis_plots_directory, "Ridgeline plot - T.gonfio.svg")
+svglite::svglite(svg_path, width = svg_width, height = svg_height, bg = "transparent")
+par(mai = use_mai, cex = 0.6, lwd = 0.6)
+PlateRidgelines(CRISPRa_summary_df,
+                fill_colors      = c("#d74291", "#f49aca"),
+                border_colors    = c("#f7d4e7", "#f8edf2"),
+                show_title       = "T.gonfio library",
+                use_alpha        = 0.75,
+                zebra_pattern    = TRUE,
+                zebra_palify     = 0.3,
+                embed_PNG        = FALSE,
+                PNG_lwd          = 0.8,
+                ridge_height     = 2.5,
+                plate_label_cex  = 0.5,
+                grid_lwd         = 0.7,
+                for_powerpoint   = TRUE
+                )
+dev.off()
+
+
+svglite::svglite(file.path(thesis_plots_directory, "Ridgeline plot - T.spiezzo.svg"),
+    width = svg_width, height = svg_height, bg = "transparent"
+    )
+par(mai = use_mai, cex = 0.6, lwd = 0.6)
+PlateRidgelines(CRISPRko_summary_df,
+                fill_colors     = c("#225faa", "#75acf0"),
+                border_colors   = c("#d4e4f7", "#d1e3fa"),
+                num_slots       = length(CRISPRa_plate_numbers),
+                show_title      = "T.spiezzo library",
+                use_alpha       = 0.75,
+                zebra_pattern   = TRUE,
+                embed_PNG       = FALSE,
+                PNG_lwd         = 0.8,
+                label_on_right  = TRUE,
+                ridge_height    = 2.5,
+                plate_label_cex = 0.5,
+                grid_lwd        = 0.7
+                )
+dev.off()
+
+
+
+
+# Export beeswarm plots ---------------------------------------------------
 
 DrawAllPlateComparisons(use_cex          = 0.175,
                         beeswarm_spacing = 0.3,
@@ -65,7 +169,6 @@ DrawAllPlateComparisons(use_cex          = 0.175,
                         export_PNGs      = TRUE,
                         exclude_controls = TRUE
                         )
-
 
 
 
