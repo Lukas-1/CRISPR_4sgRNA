fb4932c86b70ed3feba2741da179a71c3af9990e
Lukas_1 <lukas.frick@gmail.com>
2023-11-02 20:25:05 +0100

    Adapt the CheckGuides function to the output from minimap2

--- 6) Individual experiments/2022-01-05 - first nanopore sequencing run/01_R_scripts/1_R_functions/05_looking_up_aligned_sgRNAs.R
+++ 6) Individual experiments/2022-01-05 - first nanopore sequencing run/01_R_scripts/1_R_functions/05_looking_up_aligned_sgRNAs.R
@@ -52,10 +52,11 @@ MakeChunks <- function(n_total, n_per_chunk = 1000) {
 
 
 CheckGuides <- function(extract_df,
-                        sg_number = 1,
+                        sg_number        = 1,
                         large_chunk_size = 100000,
                         small_chunk_size = 10000,
-                        distance_method = "lv"
+                        distance_method  = "lv",
+                        num_cores        = NULL
                         ) {
 
   aligned_vec <- extracted_df[, paste0("Aligned_read_sg", sg_number)]
@@ -73,7 +74,7 @@ CheckGuides <- function(extract_df,
 
   ## Check for sequences with two or more gaps in the alignment
   num_gaps <- nchar(aligned_vec) - nchar(gsub("-", "", aligned_vec, fixed = TRUE))
-  too_many_gaps <- num_gaps >= 2
+  too_many_gaps <- (num_gaps >= 2) %in% TRUE
   message(paste0(sum(too_many_gaps), " out of ", nrow(extract_df), " sequences",
                  " (", formatC(sum(too_many_gaps) / nrow(extract_df) * 100,
                                digits = 1, format = "f"
@@ -82,7 +83,7 @@ CheckGuides <- function(extract_df,
                  ))
 
   ## Check for aligned sequences that are two or more base pairs too long
-  are_too_long <- nchar(aligned_vec) > 21
+  are_too_long <- (nchar(aligned_vec) > 21) %in% TRUE
   message(paste0(sum(are_too_long), " out of ", nrow(extract_df), " sequences",
                  " (", formatC(sum(are_too_long) / nrow(extract_df) * 100,
                                digits = 1, format = "f"
@@ -90,8 +91,26 @@ CheckGuides <- function(extract_df,
                  "%) were too long (12 base pairs or longer)."
                  ))
 
+  ## Check for aligned sequences that are empty or NA
+  are_empty <- (!(nzchar(aligned_vec))) | is.na(aligned_vec)
+  message(paste0(sum(are_empty), " out of ", nrow(extract_df), " sequences",
+                 " (", formatC(sum(are_empty) / nrow(extract_df) * 100,
+                               digits = 1, format = "f"
+                               ),
+                 "%) were empty or NA."
+                 ))
+
+  ## Check for aligned sequences that are too short
+  are_too_short <- ((nchar(aligned_vec) < 10) %in% TRUE) & (!(are_empty))
+  message(paste0(sum(are_too_short), " out of ", nrow(extract_df), " sequences",
+                 " (", formatC(sum(are_too_short) / nrow(extract_df) * 100,
+                               digits = 1, format = "f"
+                               ),
+                 "%) were shorter than 10 base pairs."
+                 ))
+
   ## Choose the remaining sequences (to look for hits with one mismatch)
-  are_remaining <- !(are_identical | too_many_gaps | are_too_long)
+  are_remaining <- !(are_identical | too_many_gaps | are_too_long | are_too_short | are_empty)
   remaining_vec <- aligned_vec[are_remaining]
 
   ## Initialize vectors
@@ -108,7 +127,9 @@ CheckGuides <- function(extract_df,
   chunk_numbers <- format(seq_len(num_chunks))
 
   ## Prepare for parallelization
-  num_cores <- parallel::detectCores() - 2
+  if (is.null(num_cores)) {
+    num_cores <- parallel::detectCores() - 2
+  }
   cl <- parallel::makeCluster(num_cores)
   parallel::clusterExport(cl,
                           varlist = c("template_vec", "remaining_vec",
